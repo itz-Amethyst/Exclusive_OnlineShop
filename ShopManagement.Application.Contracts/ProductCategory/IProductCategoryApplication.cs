d7c93777013919f3df66015019b306584f638b66
callme-devil <pransermi@gmail.com>
2022-08-01 21:37:56 +0430

    feat: added IProductCategoryApplication

---
+++ ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public interface IProductCategoryApplication
+    {
+        void Create(CreateProductCategory command);
+
+        void Edit(EditProductCategory command);
+
+        Domain.ProductCategoryAgg.ProductCategory GetDetails(int id);
+
+        List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel);
+    }
+}
