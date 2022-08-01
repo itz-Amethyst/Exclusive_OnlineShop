b1c69617174e4b8ddbaeb1778b59c9948677e256
callme-devil <pransermi@gmail.com>
2022-08-01 21:32:27 +0430

    feat: ProductCategory Create & Edit & Search & ViewModel Created & Filled

---
+++ ShopManagement.Application.Contracts/ProductCategory/ProductCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory;
+
+public class ProductCategoryViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Picture { get; set; }
+
+    public string CreationDate { get; set; }
+
+    public int ProductCount { get; set; }
+}
