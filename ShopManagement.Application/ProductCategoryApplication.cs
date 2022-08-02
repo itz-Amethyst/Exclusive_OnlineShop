fa26f1e61d4f61f8533d7fe29415a7579193d65a
callme-devil <pransermi@gmail.com>
2022-08-02 12:17:44 +0430

    feat: ProductCategoryApplication Implemented

---
+++ ShopManagement.Application/ProductCategoryApplication.cs
@@ -0,0 +1,35 @@
+﻿using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+        public void Create(CreateProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(EditProductCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
