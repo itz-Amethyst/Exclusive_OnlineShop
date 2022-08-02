35ba60f1142f2d1673d08137b41847b42f4a2ccb
callme-devil <pransermi@gmail.com>
2022-08-02 12:27:05 +0430

    feat : Framework OperationResult added

---
+++ 0_Framework/Application/OperationResult.cs
@@ -0,0 +1,28 @@
+﻿namespace _0_Framework.Application
+{
+    public class OperationResult
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccessful { get; set; }
+
+        public OperationResult()
+        {
+            IsSuccessful = false;
+        }
+
+        public OperationResult Succeeded(string message ="عملیات با موفقیت انجام شد")
+        {
+            IsSuccessful = true;
+            Message = message;
+            return this;
+        }
+
+        public OperationResult Failed(string message = "")
+        {
+            IsSuccessful = false;
+            Message = message;
+            return this;
+        }
+    }
+}
