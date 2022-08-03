f0d9a92b7afdc0aca5cbf8637e6fa63c41d335ca
callme-devil <pransermi@gmail.com>
2022-08-03 21:04:30 +0430

    feat: ProductCategoryRepository Implemented

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class ProductCategoryRepository:IProductCategoryRepository
+    {
+        public void Create(ProductCategory entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductCategory GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Expression<Func<ProductCategory, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditProductCategory GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
