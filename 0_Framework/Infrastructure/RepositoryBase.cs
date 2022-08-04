3facd5ede237186f4e90c9d6a545c4f7a99c9a32
callme-devil <pransermi@gmail.com>
2022-08-04 11:29:26 +0430

    feat: Generic RepositoryBase Filled

---
+++ 0_Framework/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using _0_Framework.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace _0_Framework.Infrastructure
+{
+    public class RepositoryBase<TKey , T >: IRepository<TKey , T> where T : class
+    {
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public T GetById(TKey id)
+        {
+            return _context.Find<T>(id);
+        }
+
+        public List<T> Get()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public void Create(T entity)
+        {
+            _context.Add(entity);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public bool Exists(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Any(expression);
+        }
+    }
+}
