0549face45375c295a00b58cc40ea553bebb387c
callme-devil <pransermi@gmail.com>
2022-08-04 17:53:14 +0430

    feat: AdminLayout & Assets Added

---
+++ ServiceHost/Areas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
