c6df27e94aaf5f95d8ca4abe81081d96adf918b4
callme-devil <pransermi@gmail.com>
2022-08-04 11:22:42 +0430

    feat: Generic Repository Implemented

---
+++ 0_Framework/Domain/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace _0_Framework.Domain
+{
+    public interface IRepository<in TKey, T> where T : class
+    {
+        T GetById(TKey id);
+
+        List<T> Get();
+
+        void Create(T entity);
+
+        void SaveChanges();
+
+        bool Exists(Expression<Func<T, bool>> expression);
+    }
+}
