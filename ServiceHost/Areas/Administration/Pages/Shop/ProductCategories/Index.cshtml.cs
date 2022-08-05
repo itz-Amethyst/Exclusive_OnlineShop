233e2bee3854cda4343c5ad5cae46c476964bbb7
callme-devil <pransermi@gmail.com>
2022-08-05 12:27:28 +0430

    feat: ProductCategories OnGet Method Implemented

--- ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
 
-namespace ServiceHost.Areas.Administration.Shop.ProductCategories
+namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly  IProductCategoryApplication _productCategoryApplication;
+
+        public List<ProductCategoryViewModel> ProductCategories;
+
+        public IndexModel(IProductCategoryApplication productCategoryApplication, List<ProductCategoryViewModel> productCategories)
         {
+            _productCategoryApplication = productCategoryApplication;
+            ProductCategories = productCategories;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _productCategoryApplication.Search(searchModel);
         }
     }
 }
