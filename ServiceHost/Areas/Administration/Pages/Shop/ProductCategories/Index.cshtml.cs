1dca9c02e907e4dc8783ae53d9057b0c3f36361e
callme-devil <pransermi@gmail.com>
2022-08-06 18:54:08 +0430

    feat: EditProductCategory Get & Post Method Added

--- ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -31,5 +31,19 @@ namespace ServiceHost.Areas.Administration.Pages.Shop.ProductCategories
             var result = _productCategoryApplication.Create(command);
             return new JsonResult(result);
         }
+
+        public IActionResult OnGetEdit(int id)
+        {
+            var productCategory = _productCategoryApplication.GetDetails(id);
+
+            return Partial("Edit", productCategory);
+        }
+
+        public JsonResult OnPostEdit(EditProductCategory command)
+        {
+            var result = _productCategoryApplication.Edit(command);
+
+            return new JsonResult(result);
+        }
     }
 }
