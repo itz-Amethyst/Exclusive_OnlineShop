d5cab32c765e54ef95b222371b72ad06e0558653
callme-devil <pransermi@gmail.com>
2022-08-06 20:59:33 +0430

    fix: Fixed Slug Create Bug In ProductCategory

--- ShopManagement.Application/ProductCategoryApplication.cs
+++ ShopManagement.Application/ProductCategoryApplication.cs
@@ -24,7 +24,7 @@ namespace ShopManagement.Application
             var slug = command.Slug.Slugify();
 
             var productCategory = new ProductCategory(command.Name, command.Description, command.Picture,
-                command.PictureAlt, command.PictureTitle, command.Keywords, command.Slug, command.MetaDescription);
+                command.PictureAlt, command.PictureTitle, command.Keywords, slug, command.MetaDescription);
 
             _productCategoryRepository.Create(productCategory);
             _productCategoryRepository.SaveChanges();
