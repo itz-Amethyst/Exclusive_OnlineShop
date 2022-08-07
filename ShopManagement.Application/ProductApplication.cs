cff0626c8216d45cdd6ed82b05ec6859c055d734
callme-devil <pransermi@gmail.com>
2022-08-07 18:08:32 +0430

    feat: ProductApplication Create OperationResult Filled

---
+++ ShopManagement.Application/ProductApplication.cs
@@ -0,0 +1,60 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Domain.ProductAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductApplication : IProductApplication
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductApplication(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public OperationResult Create(CreateProduct command)
+        {
+            var operation = new OperationResult();
+
+            if (_productRepository.Exists(x => x.Name == command.Name))
+            {
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            }
+
+            var product = new Product(command.Name, command.Code, command.UnitPrice, command.ShortDescription,
+                command.Description, command.Picture, command.PictureAlt, command.PictureTitle, command.CategoryId,
+                command.Slug, command.Keywords, command.MetaDescription);
+
+            _productRepository.Create(product);
+            _productRepository.SaveChanges();
+
+            return operation.Succeeded();
+        }
+
+        public OperationResult Edit(EditProduct command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditProduct GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductViewModel> Search(ProductSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InStock(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EmptyStock(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
