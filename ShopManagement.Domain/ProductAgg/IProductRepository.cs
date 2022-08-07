468164da56cab1e41f110b046e64504cf89e7d7d
callme-devil <pransermi@gmail.com>
2022-08-07 17:45:22 +0430

    feat: IProductRepository Filled With Methods

--- ShopManagement.Domain/ProductAgg/IProductRepository.cs
+++ ShopManagement.Domain/ProductAgg/IProductRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Product;
 
 namespace ShopManagement.Domain.ProductAgg
 {
     public interface IProductRepository : IRepository<int , Product>
     {
+        EditProduct GetDetails(int id);
 
+        List<ProductViewModel> Search(ProductSearchModel searchModel);
     }
 }
