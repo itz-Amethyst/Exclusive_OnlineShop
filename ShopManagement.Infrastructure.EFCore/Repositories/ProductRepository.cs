2b0630d916da12f62335ca2f79d108f47f71e51c
callme-devil <pransermi@gmail.com>
2022-08-07 19:08:41 +0430

    feat: ProductRepository & GetDetails Method Implemented

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/ProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class ProductRepository : RepositoryBase<int , Product> , IProductRepository
+    {
+        private readonly  ShopContext _context;
+
+        public ProductRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditProduct GetDetails(int id)
+        {
+            return _context.Products.Select(x => new EditProduct
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Code = x.Code,
+                Slug = x.Slug,
+                CategoryId = x.CategoryId,
+                Description = x.Description,
+                Keywords = x.Keywords,
+                MetaDescription = x.MetaDescription,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                ShortDescription = x.ShortDescription,
+                UnitPrice = x.UnitPrice,
+            }).First(x => x.Id == id);
+        }
+
+        public List<ProductViewModel> Search(ProductSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
