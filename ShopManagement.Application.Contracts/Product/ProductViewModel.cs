fedfd578423a01df039a43c56733f26daeaa5bd8
callme-devil <pransermi@gmail.com>
2022-08-07 17:30:15 +0430

    feat: ProductViewModel Implemented

---
+++ ShopManagement.Application.Contracts/Product/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopManagement.Application.Contracts.Product
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
