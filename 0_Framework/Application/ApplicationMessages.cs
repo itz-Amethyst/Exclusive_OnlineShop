d150ef4b3d18621a77f631dd722606a475a14aca
callme-devil <pransermi@gmail.com>
2022-08-07 17:51:51 +0430

    feat: Framework ApplicationMessages Implemented

---
+++ 0_Framework/Application/ApplicationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace _0_Framework.Application
+{
+    public class ApplicationMessages
+    {
+        public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد , لطفا دوباره تلاش کنید";
+
+        public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد, لطفا دوباره تلاش کنید";
+    }
+}
