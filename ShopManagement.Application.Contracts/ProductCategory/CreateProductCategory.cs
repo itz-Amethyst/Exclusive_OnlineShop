e53f50e562f7d31ccfdcaba72893a90d78f1f1b0
callme-devil <pransermi@gmail.com>
2022-08-07 16:59:07 +0430

    feat:  CreateProductCategory Client-Validation Added

--- ShopManagement.Application.Contracts/ProductCategory/CreateProductCategory.cs
+++ ShopManagement.Application.Contracts/ProductCategory/CreateProductCategory.cs
@@ -1,21 +1,32 @@
-﻿namespace ShopManagement.Application.Contracts.ProductCategory
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
 {
     public class CreateProductCategory
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Keywords { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get; set; }
 
     }
