a000a65999804f24617a17c9a3128de18a7080bf
callme-devil <pransermi@gmail.com>
2022-08-07 19:19:01 +0430

    feat: Product ShopManagementBootstrapper Wired Up

--- ShopManagement.Configuration/ShopManagementBootstrapper.cs
+++ ShopManagement.Configuration/ShopManagementBootstrapper.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ShopManagement.Application;
+using ShopManagement.Application.Contracts.Product;
 using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Domain.ProductCategoryAgg;
 using ShopManagement.Infrastructure.EFCore.Context;
 using ShopManagement.Infrastructure.EFCore.Repositories;
@@ -16,6 +18,13 @@ namespace ShopManagement.Configuration
 
             services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
             services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+            #endregion
+
+            #region Product
+
+            services.AddTransient<IProductApplication, ProductApplication>();
+            services.AddTransient<IProductRepository, ProductRepository>();
 
             #endregion
 
