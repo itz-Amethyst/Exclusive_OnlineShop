74b4049c289aa5443157544ce9a3aa624d7e647b
callme-devil <pransermi@gmail.com>
2022-08-07 17:37:39 +0430

    feat: IProductApplication Implemented

---
+++ ShopManagement.Application.Contracts/Product/IProductApplication.cs
@@ -0,0 +1,19 @@
+﻿using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Product
+{
+    public interface IProductApplication
+    {
+        OperationResult Create(CreateProduct command);
+
+        OperationResult Edit(EditProduct command);
+
+        EditProduct GetDetails(int id);
+
+        List<ProductViewModel> Search(ProductSearchModel searchModel);
+
+        void InStock(int id);
+
+        void EmptyStock(int id);
+    }
+}
