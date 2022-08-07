be195aec4c3050d4a562e433caf69960d446da18
callme-devil <pransermi@gmail.com>
2022-08-07 17:33:59 +0430

    feat: ProductSearchModel Implemented

---
+++ ShopManagement.Application.Contracts/Product/ProductSearchModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopManagement.Application.Contracts.Product
+{
+    public class ProductSearchModel
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
