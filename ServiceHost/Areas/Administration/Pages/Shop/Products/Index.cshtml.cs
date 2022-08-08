33d920521ff6f77c64e3af3fe5da7fcb0183d92d
callme-devil <pransermi@gmail.com>
2022-08-08 12:24:11 +0430

    feat: Created Products Pages From Duplicated ProductCategories

---
+++ ServiceHost/Areas/Administration/Pages/Shop/Products/Index.cshtml.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.ProductCategory;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductCategoryApplication _productCategoryApplication;
+
+        public ProductCategorySearchModel SearchModel;
+        public List<ProductCategoryViewModel> ProductCategories;
+
+        public IndexModel(IProductCategoryApplication productCategoryApplication)
+        {
+            _productCategoryApplication = productCategoryApplication;
+        }
+
+        public void OnGet(ProductCategorySearchModel searchModel)
+        {
+            ProductCategories = _productCategoryApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            return Partial("./Create", new CreateProductCategory());
+        }
+
+        public JsonResult OnPostCreate(CreateProductCategory command)
+        {
+            var result = _productCategoryApplication.Create(command);
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(int id)
+        {
+            var productCategory = _productCategoryApplication.GetDetails(id);
+
+            return Partial("Edit", productCategory);
+        }
+
+        public JsonResult OnPostEdit(EditProductCategory command)
+        {
+            var result = _productCategoryApplication.Edit(command);
+
+            return new JsonResult(result);
+        }
+    }
+}
