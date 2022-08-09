c0b98cef03ade2031757eaf82ddb88376cd0b4ab
callme-devil <pransermi@gmail.com>
2022-08-09 22:19:08 +0430

    feat: EditProductPicture Model Implemented

---
+++ ShopManagement.Domain/ProductAgg/EditProductPicture.cs
@@ -0,0 +1,10 @@
+﻿using ShopManagement.Application.Contracts.ProductPicture;
+
+namespace ShopManagement.Domain.ProductAgg
+{
+    public class EditProductPicture : CreateProductPicture
+    {
+        public int Id { get; set; }
+
+    }
+}
