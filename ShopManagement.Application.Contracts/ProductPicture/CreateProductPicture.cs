1b10040419085c55ca031f0c7e745dc3233b8987
callme-devil <pransermi@gmail.com>
2022-08-09 22:18:09 +0430

    feat: CreateProductPicture Model Implemented

---
+++ ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopManagement.Application.Contracts.ProductPicture
+{
+    public class CreateProductPicture
+    {
+        public int ProductId { get; set; }
+
+        public string Picture { get; set; }
+
+        public string PictureAlt { get; set; }
+
+        public string PictureTitle { get; set; }
+    }
+}
