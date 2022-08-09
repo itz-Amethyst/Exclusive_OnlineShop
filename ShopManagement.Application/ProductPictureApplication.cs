fe4695eebe7d1e898a47e5f83a8acf89c1ee1223
callme-devil <pransermi@gmail.com>
2022-08-09 22:31:22 +0430

    feat: ProductPictureApplication Implemented

---
+++ ShopManagement.Application/ProductPictureApplication.cs
@@ -0,0 +1,47 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductPicture;
+using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductPictureAgg;
+
+namespace ShopManagement.Application
+{
+    public class ProductPictureApplication : IProductPictureApplication
+    {
+        private readonly IProductPictureRepository _productPictureRepository;
+
+        public ProductPictureApplication(IProductPictureRepository productPictureRepository)
+        {
+            _productPictureRepository = productPictureRepository;
+        }
+
+        public OperationResult Create(CreateProductPicture command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditProductPicture command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Restore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditProductPicture GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
