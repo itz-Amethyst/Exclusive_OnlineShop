89cf52e6828eae9ad4e9cfaa20c7de818c425067
callme-devil <pransermi@gmail.com>
2022-08-09 22:21:47 +0430

    feat: ProductPictureViewModel Implemented

---
+++ ShopManagement.Domain/ProductAgg/ProductPictureViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopManagement.Domain.ProductAgg
+{
+    public class ProductPictureViewModel
+    {
+        public int Id { get; set; }
+
+        public string Product { get; set; }
+
+        public string Picture { get; set; }
+
+        public string CreationDate { get; set; }
+    }
+}
