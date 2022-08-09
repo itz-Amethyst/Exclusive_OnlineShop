619fa326f65165cf994adcdee2c03f2e00b186e1
callme-devil <pransermi@gmail.com>
2022-08-09 22:11:40 +0430

    feat: ProductPicture added in Domain

---
+++ ShopManagement.Domain/ProductPictureAgg/ProductPicture.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.ProductPictureAgg
+{
+    public class ProductPicture : EntityBase
+    {
+        public int ProductId { get; private set; }
+
+        public string Picture { get; private set; }
+
+        public string PictureAlt { get; private set; }
+
+        public string PictureTitle { get; private set; }
+    }
+}
