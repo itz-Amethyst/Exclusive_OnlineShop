9a8eb3021f611bb901197ce8cea35efae6228c46
callme-devil <pransermi@gmail.com>
2022-08-09 22:29:41 +0430

    feat: IProductPictureRepository Filled

--- ShopManagement.Domain/ProductPictureAgg/IProductPictureRepository.cs
+++ ShopManagement.Domain/ProductPictureAgg/IProductPictureRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Domain.ProductAgg;
 
 namespace ShopManagement.Domain.ProductPictureAgg
 {
     public interface IProductPictureRepository : IRepository<int , ProductPicture>
     {
+        EditProductPicture GetDetails(int id);
 
+        List<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel);
     }
 }
