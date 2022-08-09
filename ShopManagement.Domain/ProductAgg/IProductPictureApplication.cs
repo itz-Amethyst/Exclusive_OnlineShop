29ac64bfe05c722fa3a0e66627c13ad61fdff9ed
callme-devil <pransermi@gmail.com>
2022-08-09 22:27:45 +0430

    feat: ProductPicture Application Interface Implemented & Filled

---
+++ ShopManagement.Domain/ProductAgg/IProductPictureApplication.cs
@@ -0,0 +1,20 @@
+﻿using _0_Framework.Application;
+using ShopManagement.Application.Contracts.ProductPicture;
+
+namespace ShopManagement.Domain.ProductAgg
+{
+    public interface IProductPictureApplication
+    {
+        OperationResult Create(CreateProductPicture command);
+
+        OperationResult Edit(EditProductPicture command);
+
+        OperationResult Remove(int id);
+
+        OperationResult Restore(int id);
+
+        EditProductPicture GetDetails(int id);
+
+        List<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel);
+    }
+}
