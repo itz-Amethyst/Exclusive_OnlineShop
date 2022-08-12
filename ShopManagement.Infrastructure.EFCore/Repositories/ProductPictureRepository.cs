39ceeb7b12f827737bb6feb8caa25942e23fc183
callme-devil <pransermi@gmail.com>
2022-08-12 12:59:14 +0430

    feat: ProductPictureRepository Implemented

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/ProductPictureRepository.cs
@@ -0,0 +1,51 @@
+﻿using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Domain.ProductPictureAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class ProductPictureRepository : RepositoryBase<int, ProductPicture>, IProductPictureRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductPictureRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditProductPicture GetDetails(int id)
+        {
+            return _context.ProductPictures.Select(x => new EditProductPicture
+            {
+                Id =x.Id,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                ProductId = x.ProductId
+            }).First(x => x.Id == id);
+        }
+
+        public List<ProductPictureViewModel> Search(ProductPictureSearchModel searchModel)
+        {
+            var query = _context.ProductPictures.Include(x=>x.Product).Select(x => new ProductPictureViewModel
+            {
+                Id = x.Id,
+                Product = x.Product.Name,
+                CreationDate = x.CreationDate.ToString(),
+                Picture = x.Picture,
+                ProductId = x.ProductId    
+            });
+
+            if (searchModel.ProductId != 0)
+            {
+                query = query.Where(x => x.ProductId == searchModel.ProductId);
+            }
+
+            return query.OrderByDescending(x => x.Id).ToList();
+
+        }
+
+    }
+}
