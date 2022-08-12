b743e890d9df45ea2f45a54acc0dddad6e4f3e9b
callme-devil <pransermi@gmail.com>
2022-08-12 11:59:22 +0430

    feat: Added ProductPicture Reference prop to Product

--- ShopManagement.Domain/ProductAgg/Product.cs
+++ ShopManagement.Domain/ProductAgg/Product.cs
@@ -1,5 +1,6 @@
 ﻿using _0_Framework.Domain;
 using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Domain.ProductPictureAgg;
 
 namespace ShopManagement.Domain.ProductAgg
 {
@@ -32,6 +33,8 @@ namespace ShopManagement.Domain.ProductAgg
         public string MetaDescription { get; private set; }
 
         public ProductCategory Category { get; private set; }
+
+        public List<ProductPicture> ProductPictures { get; private set; }
 
         public Product(string name, string code, double unitPrice, string shortDescription, string description, string picture, string pictureAlt, string pictureTitle, int categoryId, string slug, string keywords, string metaDescription)
         {
