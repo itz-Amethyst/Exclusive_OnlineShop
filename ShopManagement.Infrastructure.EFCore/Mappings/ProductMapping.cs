b58c3db6a8097704cca1d78da60274bc9125e5ac
callme-devil <pransermi@gmail.com>
2022-08-12 12:47:24 +0430

    feat: ProductPicture Mapping inside Product Added

--- ShopManagement.Infrastructure.EFCore/Mappings/ProductMapping.cs
+++ ShopManagement.Infrastructure.EFCore/Mappings/ProductMapping.cs
@@ -26,6 +26,10 @@ namespace ShopManagement.Infrastructure.EFCore.Mappings
             builder.HasOne(x => x.Category)
                 .WithMany(x => x.Products)
                 .HasForeignKey(x => x.CategoryId);
+
+            builder.HasMany(x=>x.ProductPictures)
+                .WithOne(x=>x.Product)
+                .HasForeignKey(x => x.ProductId);
         }
     }
 }
