54ff43e361379fc68341fd454fd09c55bd43e4b8
callme-devil <pransermi@gmail.com>
2022-08-14 23:06:31 +0430

    feat: CreateProductPIcture Validation Added

--- ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
+++ ShopManagement.Application.Contracts/ProductPicture/CreateProductPicture.cs
@@ -1,17 +1,24 @@
-﻿using ShopManagement.Application.Contracts.Product;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Product;
 
 namespace ShopManagement.Application.Contracts.ProductPicture
 {
     public class CreateProductPicture
     {
+        [Range(1,100000 , ErrorMessage = ValidationMessages.IsRequired)]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public List<ProductViewModel> Products { get; set; }
     }
 }
