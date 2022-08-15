b75c6079abe3f991bf7395ed615965aed80f48e1
callme-devil <pransermi@gmail.com>
2022-08-15 15:13:32 +0430

    feat: SlideViewModel Implemented

---
+++ ShopManagement.Application.Contracts/Slide/SlideViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ShopManagement.Application.Contracts.Slide
+{
+    public class SlideViewModel
+    {
+        public int Id { get; set; }
+
+        public string Picture { get; set; }
+
+        public string Heading { get; set; }
+
+        public string Title { get; set; }
+    }
+}
