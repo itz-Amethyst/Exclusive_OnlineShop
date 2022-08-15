90c48f62bc572a6f53a47d3916413d07ec5f53b2
callme-devil <pransermi@gmail.com>
2022-08-15 15:34:12 +0430

    feat: SlideRepository & GetDetails Method Implemented

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/SlideRepository.cs
@@ -0,0 +1,37 @@
+﻿using _0_Framework.Infrastructure;
+using ShopManagement.Application.Contracts.Slide;
+using ShopManagement.Domain.SlideAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class SlideRepository : RepositoryBase<int , Slide> , ISlideRepository
+    {
+        private readonly ShopContext _context;
+
+        public SlideRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditSlide GetDetails(int id)
+        {
+           return _context.Slides.Select(x => new EditSlide
+            {
+                Id = x.Id,
+                BtnText = x.BtnText,
+                Heading = x.Heading,
+                Picture = x.Picture,
+                PictureAlt = x.PictureAlt,
+                PictureTitle = x.PictureTitle,
+                Text = x.Text,
+                Title = x.Title
+            }).First(x => x.Id == id);
+        }
+
+        public List<SlideViewModel> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
