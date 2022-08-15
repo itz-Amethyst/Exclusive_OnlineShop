0c15cb86cbf12c336fba53ceaa843746283fd32f
callme-devil <pransermi@gmail.com>
2022-08-15 15:17:37 +0430

    feat: SlideRepository Filled

--- ShopManagement.Domain/SlideAgg/ISlideRepository.cs
+++ ShopManagement.Domain/SlideAgg/ISlideRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Slide;
 
 namespace ShopManagement.Domain.SlideAgg
 {
     public interface ISlideRepository : IRepository<int , Slide>
     {
+        EditSlide GetDetails(int id);
 
+        List<SlideViewModel> GetList();
     }
 }
