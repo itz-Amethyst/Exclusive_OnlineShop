9dc90a1d2292cf99d01b84de7d851f784c25638b
callme-devil <pransermi@gmail.com>
2022-08-15 15:15:45 +0430

    feat: SlideApplication Created & Filled

---
+++ ShopManagement.Application.Contracts/Slide/ISlideApplication.cs
@@ -0,0 +1,19 @@
+﻿using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Slide
+{
+    public interface ISlideApplication
+    {
+        OperationResult Create(CreateSlide command);
+
+        OperationResult Edit (EditSlide command);
+
+        OperationResult Remove(int id);
+
+        OperationResult Restore(int id);
+
+        EditSlide GetDetails(int id);
+
+        List<SlideViewModel> GetList();
+    }
+}
