b4fd5c427852a7fbb8beb1311ce95fe63ed396ac
callme-devil <pransermi@gmail.com>
2022-08-16 16:04:43 +0430

    feat: Created Slides From ProductPictures Copied

---
+++ ServiceHost/Areas/Administration/Pages/Shop/Slides/Index.cshtml.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contracts.Product;
+using ShopManagement.Application.Contracts.ProductPicture;
+using ShopManagement.Domain.ProductAgg;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.Slides
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductApplication _productApplication;
+        private readonly IProductPictureApplication _productPictureApplication;
+
+        public ProductPictureSearchModel SearchModel;
+        public List<ProductPictureViewModel> ProductPictures;
+        public SelectList Products;
+
+        public IndexModel(IProductApplication productApplication, IProductPictureApplication productPictureApplication)
+        {
+            _productApplication = productApplication;
+            _productPictureApplication = productPictureApplication;
+        }
+
+        public void OnGet(ProductPictureSearchModel searchModel , bool restored = false , bool removed = false)
+        {
+            ViewData["Restored"] = restored;
+            ViewData["Removed"] = removed;
+            Products = new SelectList(_productApplication.GetProducts(),"Id" , "Name");
+            ProductPictures = _productPictureApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new CreateProductPicture
+            {
+                Products = _productApplication.GetProducts()
+            };
+            return Partial("./Create", command);
+        }
+
+        public JsonResult OnPostCreate(CreateProductPicture command)
+        {
+            var result = _productPictureApplication.Create(command);
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(int id)
+        {
+            var ProductPicture = _productPictureApplication.GetDetails(id);
+            ProductPicture.Products = _productApplication.GetProducts();
+            return Partial("Edit", ProductPicture);
+        }
+
+        public JsonResult OnPostEdit(EditProductPicture command)
+        {
+            var result = _productPictureApplication.Edit(command);
+
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetRemove(int id)
+        {
+            var result = _productPictureApplication.Remove(id);
+           
+            return RedirectToPage("./Index" , new {Removed="True"});
+            
+        }
+
+        public IActionResult OnGetRestore(int id)
+        {
+            var result = _productPictureApplication.Restore(id);
+
+            return RedirectToPage("./Index", new { Restored = "True" });
+        }
+    }
+}
