eea103e60d19aefe3cc1a6e3e148a5907acde235
callme-devil <pransermi@gmail.com>
2022-08-18 22:23:58 +0430

    feat: CustomerDiscount Added to DiscountContext

--- DiscountManagement.Infrastructure.EFCore/Context/DiscountContext.cs
+++ DiscountManagement.Infrastructure.EFCore/Context/DiscountContext.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EFCore.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiscountManagement.Infrastructure.EFCore.Context
 {
     public class DiscountContext : DbContext
     {
+        public DbSet<CustomerDiscount> CustomerDiscounts { get; set; }
 
+        public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(CustomerDiscountMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
