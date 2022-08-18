e4feb35f23e7bbcf31ea69a8388bc9a51302f013
callme-devil <pransermi@gmail.com>
2022-08-18 21:43:54 +0430

    feat: DefineCustomerDiscount Filled

--- DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
+++ DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
@@ -2,6 +2,14 @@
 {
     public class DefineCustomerDiscount
     {
+        public int ProductId { get; set; }
 
+        public int DiscountRate { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public string Reason { get; set; }
     }
 }
