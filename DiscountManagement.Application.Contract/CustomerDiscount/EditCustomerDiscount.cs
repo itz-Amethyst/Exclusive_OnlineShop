dbed79f6f204086973bcd541ebe638b71b1ab6b1
callme-devil <pransermi@gmail.com>
2022-08-18 21:44:58 +0430

    feat: EditCustomerDiscount Implemented

---
+++ DiscountManagement.Application.Contract/CustomerDiscount/EditCustomerDiscount.cs
@@ -0,0 +1,7 @@
+﻿namespace DiscountManagement.Application.Contract.CustomerDiscount
+{
+    public class EditCustomerDiscount : DefineCustomerDiscount
+    {
+        public int Id { get; set; }
+    }
+}
