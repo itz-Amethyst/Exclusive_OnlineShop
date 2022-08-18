3fb6ec0334b523e937d98e1e3155a2cc6196530a
callme-devil <pransermi@gmail.com>
2022-08-18 22:19:32 +0430

    feat: CustomerDiscountRepository Basic Implemented

---
+++ DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
@@ -0,0 +1,29 @@
+﻿using _0_Framework.Domain;
+using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class CustomerDiscountRepository : RepositoryBase<int , CustomerDiscount>, ICustomerDiscountRepository
+    {
+        private readonly DiscountContext _context;
+
+        public CustomerDiscountRepository(DiscountContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditCustomerDiscount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
