f267e83c53dd70fb95347e10e2747df72eb640ee
callme-devil <pransermi@gmail.com>
2022-08-18 21:48:11 +0430

    feat: CustomerDiscountViewModel Implemented

---
+++ DiscountManagement.Application.Contract/CustomerDiscount/CustomerDiscountViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DiscountManagement.Application.Contract.CustomerDiscount
+{
+    public class CustomerDiscountViewModel
+    {
+        public int ProductId { get; private set; }
+
+        public string ProductName { get; set; }
+
+        public int DiscountRate { get; private set; }
+
+        public DateTime StartDate { get; private set; }
+
+        public DateTime EndDate { get; private set; }
+
+        public string Reason { get; private set; }
+    }
+}
