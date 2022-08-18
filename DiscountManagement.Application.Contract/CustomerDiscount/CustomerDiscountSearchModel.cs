0b4e43e8d3994e1ab3197de7e6f62b47a9258b7a
callme-devil <pransermi@gmail.com>
2022-08-18 21:48:41 +0430

    feat: CustomerDiscountSearchModel Implemented

---
+++ DiscountManagement.Application.Contract/CustomerDiscount/CustomerDiscountSearchModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscountManagement.Application.Contract.CustomerDiscount
+{
+    public class CustomerDiscountSearchModel
+    {
+        public int ProductId { get; private set; }
+
+        public DateTime StartDate { get; private set; }
+
+        public DateTime EndDate { get; private set; }
+
+    }
+}
