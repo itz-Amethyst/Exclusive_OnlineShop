b61705dae1b180597dc4d90193d62c60e83488d7
callme-devil <pransermi@gmail.com>
2022-08-18 21:52:04 +0430

    feat: ICustomerDiscountRepository Filled

--- DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
+++ DiscountManagement.Domain/CustomerDiscountAgg/ICustomerDiscountRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using DiscountManagement.Application.Contract.CustomerDiscount;
 
 namespace DiscountManagement.Domain.CustomerDiscountAgg
 {
     public interface ICustomerDiscountRepository : IRepository<int, CustomerDiscount>
     {
+        EditCustomerDiscount GetDetails(int id);
 
+        List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
     }
 }
