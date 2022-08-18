3a8a25d625a6e44a74ddcf79d3f491ddf7675e5b
callme-devil <pransermi@gmail.com>
2022-08-18 22:08:16 +0430

    feat: CustomerDiscountManagement Application Basic  Implemented

---
+++ DiscountManagement.Application/CustomerDiscountApplication.cs
@@ -0,0 +1,36 @@
+﻿using _0_Framework.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+
+namespace DiscountManagement.Application
+{
+    public class CustomerDiscountApplication : ICustomerDiscountApplication
+    {
+        private readonly ICustomerDiscountRepository _customerDiscountRepository;
+
+        public CustomerDiscountApplication(ICustomerDiscountRepository customerDiscountRepository)
+        {
+            _customerDiscountRepository = customerDiscountRepository;
+        }
+
+        public OperationResult Define(DefineCustomerDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditCustomerDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditCustomerDiscount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
