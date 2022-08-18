72f6537261d05e0745a0083260cf1d5671ce71c6
callme-devil <pransermi@gmail.com>
2022-08-18 21:51:02 +0430

    feat: ICustomerDiscountApplication Implemented

---
+++ DiscountManagement.Application.Contract/CustomerDiscount/ICustomerDiscountApplication.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Application;
+
+namespace DiscountManagement.Application.Contract.CustomerDiscount
+{
+    public interface ICustomerDiscountApplication
+    {
+        OperationResult Define(DefineCustomerDiscount command);
+
+        OperationResult Edit(EditCustomerDiscount command);
+
+        EditCustomerDiscount GetDetails(int id);
+
+        List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel);
+    }
+}
