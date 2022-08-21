33ebc5b3b5841af5475d69a4263cd33a1b957bbe
callme-devil <pransermi@gmail.com>
2022-08-21 21:30:49 +0430

    feat: LimitReasonProp Migration Added

---
+++ DiscountManagement.Infrastructure.EFCore/Migrations/20220821170016_LimitedReasonProp.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscountManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class LimitedReasonProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "CustomerDiscounts",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Reason",
+                table: "CustomerDiscounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
