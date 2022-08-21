bc4ecf5fe48951d19fe5d97289d8083f0df13600
callme-devil <pransermi@gmail.com>
2022-08-21 21:08:16 +0430

    feat: DiscountManagement.Configuration Layer Added

---
+++ DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscountManagement.Configuration
+{
+    public class DiscountManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection service, string connectionString)
+        {
+
+        }
+    }
+}
