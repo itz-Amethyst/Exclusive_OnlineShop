c0ab7a7db7c3adce079a519be78630938c686277
callme-devil <pransermi@gmail.com>
2022-08-21 20:53:27 +0430

    refactor: refactored ProductPictureRepository ToString ==> ToFarsi Method

--- ShopManagement.Infrastructure.EFCore/Repositories/ProductPictureRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repositories/ProductPictureRepository.cs
@@ -1,4 +1,5 @@
-﻿using _0_Framework.Infrastructure;
+﻿using _0_Framework.Application;
+using _0_Framework.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using ShopManagement.Application.Contracts.ProductPicture;
 using ShopManagement.Domain.ProductAgg;
@@ -34,7 +35,7 @@ namespace ShopManagement.Infrastructure.EFCore.Repositories
             {
                 Id = x.Id,
                 Product = x.Product.Name,
-                CreationDate = x.CreationDate.ToString(),
+                CreationDate = x.CreationDate.ToFarsi(),
                 Picture = x.Picture,
                 ProductId = x.ProductId,
                 IsRemoved = x.IsRemoved
