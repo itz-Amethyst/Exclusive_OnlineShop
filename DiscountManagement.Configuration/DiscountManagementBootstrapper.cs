80a4bc69c645a2ed585efb7dd4f4d2ccbdbc3cb5
callme-devil <pransermi@gmail.com>
2022-08-21 21:15:07 +0430

    feat: CustomerDiscount WiredUp in DiscountManagementBootstrapper

--- DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
+++ DiscountManagement.Configuration/DiscountManagementBootstrapper.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DiscountManagement.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using DiscountManagement.Domain.CustomerDiscountAgg;
+using DiscountManagement.Infrastructure.EFCore.Context;
+using DiscountManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DiscountManagement.Configuration
 {
     public class DiscountManagementBootstrapper
     {
-        public static void Configure(IServiceCollection service, string connectionString)
+        public static void Configure(IServiceCollection services, string connectionString)
         {
+            #region CustomerDiscount
 
+            services.AddTransient<ICustomerDiscountApplication, CustomerDiscountApplication>();
+            services.AddTransient<ICustomerDiscountRepository, CustomerDiscountRepository>();
+
+            #endregion
+
+            services.AddDbContext<DiscountContext>(x => x.UseSqlServer(connectionString));
         }
     }
 }
