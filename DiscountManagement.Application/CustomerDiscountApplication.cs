b52a917bf30a88c65136d31942bf703ee3861411
callme-devil <pransermi@gmail.com>
2022-08-21 20:59:57 +0430

    feat: CustomerDiscountApplication Define Method Implemented

--- DiscountManagement.Application/CustomerDiscountApplication.cs
+++ DiscountManagement.Application/CustomerDiscountApplication.cs
@@ -15,7 +15,21 @@ namespace DiscountManagement.Application
 
         public OperationResult Define(DefineCustomerDiscount command)
         {
-            throw new NotImplementedException();
+            var operation = new OperationResult();
+            if (_customerDiscountRepository.Exists(x => x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate))
+            {
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            }
+
+            var startDate = command.StartDate.ToGeorgianDateTime();
+            var endDate = command.EndDate.ToGeorgianDateTime();
+
+
+            var customerDiscount = new CustomerDiscount(command.ProductId , command.DiscountRate , startDate , endDate , command.Reason);
+
+            _customerDiscountRepository.Create(customerDiscount);
+            _customerDiscountRepository.SaveChanges();
+            return operation.Succeeded();
         }
 
         public OperationResult Edit(EditCustomerDiscount command)
