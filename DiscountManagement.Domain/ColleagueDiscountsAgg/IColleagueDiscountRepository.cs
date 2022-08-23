fbae7ecc66bb63f479043ef6500f8ac3b939bb53
callme-devil <pransermi@gmail.com>
2022-08-23 20:26:03 +0430

    feat: IColleagueDiscountRepository Filled

--- DiscountManagement.Domain/ColleagueDiscountsAgg/IColleagueDiscountRepository.cs
+++ DiscountManagement.Domain/ColleagueDiscountsAgg/IColleagueDiscountRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using DiscountManagement.Application.Contract.ColleagueDiscount;
 
 namespace DiscountManagement.Domain.ColleagueDiscountsAgg
 {
     public interface IColleagueDiscountRepository : IRepository<int , ColleagueDiscount>
     {
+        EditColleagueDiscount GetDetails(int id);
 
+        List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel);
     }
 }
