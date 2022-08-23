2e719555e77ea0d1279f4bba6354c46f02bec521
callme-devil <pransermi@gmail.com>
2022-08-23 20:14:47 +0430

    feat: Colleague Discount domain Implemented

---
+++ DiscountManagement.Domain/ColleagueDiscountsAgg/ColleagueDiscount.cs
@@ -0,0 +1,35 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.ColleagueDiscountsAgg
+{
+    public class ColleagueDiscount : EntityBase
+    {
+        public int ProductId { get; private set; }
+
+        public int DiscountRate { get; private set; }
+
+        public bool IsRemoved { get; private set; }
+
+        public ColleagueDiscount(int productId, int discountRate)
+        {
+            ProductId = productId;
+            DiscountRate = discountRate;
+            IsRemoved = false;
+        }
+
+        public void Edit(int productId, int discountRate)
+        {
+            ProductId = productId;
+        }
+
+        public void Remove()
+        {
+            IsRemoved = true;
+        }
+
+        public void Restore()
+        {
+            IsRemoved = false;
+        }
+    }
+}
