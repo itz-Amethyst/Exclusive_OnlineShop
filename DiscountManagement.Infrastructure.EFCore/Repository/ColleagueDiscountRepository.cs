9c1fccfc2b7e3da1f3c40efa9cd9a4ebd3f93368
callme-devil <pransermi@gmail.com>
2022-08-23 20:58:39 +0430

    feat: ColleagueDiscountRepository Base Implemented

---
+++ DiscountManagement.Infrastructure.EFCore/Repository/ColleagueDiscountRepository.cs
@@ -0,0 +1,28 @@
+﻿using _0_Framework.Infrastructure;
+using DiscountManagement.Application.Contract.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountsAgg;
+using DiscountManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class ColleagueDiscountRepository : RepositoryBase<int , ColleagueDiscount> , IColleagueDiscountRepository
+    {
+        public readonly DiscountContext _context;
+
+        public ColleagueDiscountRepository(DiscountContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditColleagueDiscount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
