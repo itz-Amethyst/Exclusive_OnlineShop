727eec2e6bf8d9dad5f0ef74925fc13463179bf1
callme-devil <pransermi@gmail.com>
2022-08-23 20:24:00 +0430

    feat: DefineColleagueDiscount Application Implemented

---
+++ DiscountManagement.Application.Contract/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -0,0 +1,13 @@
+﻿using ShopManagement.Application.Contracts.Product;
+
+namespace DiscountManagement.Application.Contract.ColleagueDiscount
+{
+    public class DefineColleagueDiscount
+    {
+        public int ProductId { get; set; }
+
+        public int DiscountRate { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
