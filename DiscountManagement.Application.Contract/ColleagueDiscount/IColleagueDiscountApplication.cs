1df1d9d5527c0c96f7c763e006cf3632636d92b5
callme-devil <pransermi@gmail.com>
2022-08-23 20:25:20 +0430

    feat: IColleagueDiscount Application Implemented

---
+++ DiscountManagement.Application.Contract/ColleagueDiscount/IColleagueDiscountApplication.cs
@@ -0,0 +1,19 @@
+﻿using _0_Framework.Application;
+
+namespace DiscountManagement.Application.Contract.ColleagueDiscount
+{
+    public interface IColleagueDiscountApplication
+    {
+        OperationResult Define(DefineColleagueDiscount command);
+
+        OperationResult Edit(EditColleagueDiscount command);
+
+        OperationResult Remove(int id);
+
+        OperationResult Restore(int id);
+
+        EditColleagueDiscount GetDetails(int id);
+
+        List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel);
+    }
+}
