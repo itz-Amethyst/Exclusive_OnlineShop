b135bdbcfc584614963fbb90bfd53a033da02e2d
callme-devil <pransermi@gmail.com>
2022-08-23 20:15:21 +0430

    feat: IColleagueDiscountRepository  base Implemented

---
+++ DiscountManagement.Domain/ColleagueDiscountsAgg/IColleagueDiscountRepository.cs
@@ -0,0 +1,9 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.ColleagueDiscountsAgg
+{
+    public interface IColleagueDiscountRepository : IRepository<int , ColleagueDiscount>
+    {
+
+    }
+}
