da057747dcd64f9cd15c3f861d5e4ced95ce164e
callme-devil <pransermi@gmail.com>
2022-08-25 19:57:21 +0430

    feat: InventoryManagementApplicationContract CreateInventory Implemented

---
+++ InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
@@ -0,0 +1,9 @@
+﻿namespace InventoryManagement.Application.Contract.Inventory
+{
+    public class CreateInventory
+    {
+        public int ProductId { get; set; }
+
+        public double UnitPrice { get; set; }
+    }
+}
