89347d303355c19ef1f54e0426207bccd87ff804
callme-devil <pransermi@gmail.com>
2022-08-25 14:32:32 +0430

    feat: Added Some ApplicationValidation Messages

--- 0_Framework/Application/ApplicationMessages.cs
+++ 0_Framework/Application/ApplicationMessages.cs
@@ -5,5 +5,9 @@
         public const string DuplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد , لطفا دوباره تلاش کنید";
 
         public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد, لطفا دوباره تلاش کنید";
+
+        public const string InputDateNotValid = "تاریخ شروع وارد شده بیشتر از تاریخ پایان میباشد لطفا دوباره تلاش کنید";
+
+        public const string DuplicatedDate = "تاریخ وارد شده نمیتواند تکراری باشد لطفا دوباره تلاش کنید";
     }
 }
