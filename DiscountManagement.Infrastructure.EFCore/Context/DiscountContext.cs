b6228fad4c226d807d4580cf59d9d39f464b6661
callme-devil <pransermi@gmail.com>
2022-08-25 11:39:53 +0430

    feat: ColleagueDiscount Dbset Added to context

--- DiscountManagement.Infrastructure.EFCore/Context/DiscountContext.cs
+++ DiscountManagement.Infrastructure.EFCore/Context/DiscountContext.cs
@@ -1,4 +1,5 @@
-﻿using DiscountManagement.Domain.CustomerDiscountAgg;
+﻿using DiscountManagement.Domain.ColleagueDiscountsAgg;
+using DiscountManagement.Domain.CustomerDiscountAgg;
 using DiscountManagement.Infrastructure.EFCore.Mappings;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,7 @@ namespace DiscountManagement.Infrastructure.EFCore.Context
     public class DiscountContext : DbContext
     {
         public DbSet<CustomerDiscount> CustomerDiscounts { get; set; }
+        public DbSet<ColleagueDiscount> ColleagueDiscounts { get; set; }
 
         public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
         {
