97166a31f111d764763fc9be4e2339678b074998
callme-devil <pransermi@gmail.com>
2022-08-25 14:33:09 +0430

    feat: Added Framework Validation Message For Empty DiscountRate Input

--- 0_Framework/Application/ValidationMessages.cs
+++ 0_Framework/Application/ValidationMessages.cs
@@ -3,5 +3,6 @@
     public class ValidationMessages
     {
         public const string IsRequired = "این مقدار نمیتواند خالی باشد";
+        public const string NotValid = "مقدار وارد شده صحیح نمیباشد";
     }
 }
