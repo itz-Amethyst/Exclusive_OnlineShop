df33574bd646142887a8737d5b00d28b7be54f5f
callme-devil <pransermi@gmail.com>
2022-08-25 14:34:16 +0430

    feat: Configured DefineColleagueDiscount Validation Message

--- DiscountManagement.Application.Contract/ColleagueDiscount/DefineColleagueDiscount.cs
+++ DiscountManagement.Application.Contract/ColleagueDiscount/DefineColleagueDiscount.cs
@@ -1,11 +1,16 @@
-﻿using ShopManagement.Application.Contracts.Product;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Product;
 
 namespace DiscountManagement.Application.Contract.ColleagueDiscount
 {
     public class DefineColleagueDiscount
     {
+        [Range(1,100000 , ErrorMessage = ValidationMessages.IsRequired)]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Range(1,99 , ErrorMessage = ValidationMessages.NotValid)]
         public int DiscountRate { get; set; }
 
         public List<ProductViewModel> Products { get; set; }
