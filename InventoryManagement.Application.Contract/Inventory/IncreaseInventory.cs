7e24d751a29f41ddedc80c827806bb8d8d83f14d
callme-devil <pransermi@gmail.com>
2022-08-25 19:59:15 +0430

    feat: InventoryManagementApplicationContract IncreaseInventory Implemented

---
+++ InventoryManagement.Application.Contract/Inventory/IncreaseInventory.cs
@@ -0,0 +1,11 @@
+﻿namespace InventoryManagement.Application.Contract.Inventory
+{
+    public class IncreaseInventory
+    {
+        public int InventoryId { get; set; }
+
+        public int Count { get; set; }
+
+        public string Description { get; set; }
+    }
+}
