d8c17a070f6e63aa35ace6c079b17022afbef9d0
callme-devil <pransermi@gmail.com>
2022-08-25 14:33:44 +0430

    feat: Configured DefineCustomerDiscount Validation

--- DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
+++ DiscountManagement.Application.Contract/CustomerDiscount/DefineCustomerDiscount.cs
@@ -1,15 +1,22 @@
-﻿using ShopManagement.Application.Contracts.Product;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Product;
 
 namespace DiscountManagement.Application.Contract.CustomerDiscount
 {
     public class DefineCustomerDiscount
     {
+        [Range(1, 100000, ErrorMessage = ValidationMessages.IsRequired)]
         public int ProductId { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [Range(1, 99, ErrorMessage = ValidationMessages.NotValid)]
         public int DiscountRate { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string StartDate { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string EndDate { get; set; }
 
         public string Reason { get; set; }
