3f888cae89e0541488c077dad18fa6857c7850c6
callme-devil <pransermi@gmail.com>
2022-08-25 20:01:02 +0430

    feat: InventoryManagementApplicationContract InventoryApplication Interface Implemented

---
+++ InventoryManagement.Application.Contract/Inventory/IInventoryApplication.cs
@@ -0,0 +1,23 @@
+﻿using _0_Framework.Application;
+
+namespace InventoryManagement.Application.Contract.Inventory
+{
+    public interface IInventoryApplication
+    {
+        OperationResult Create(CreateInventory command);
+
+        OperationResult Edit(EditInventory command);
+
+        OperationResult Increase(IncreaseInventory command);
+
+        //? Customer
+        OperationResult Reduce(List<ReduceInventory> command);
+
+        //! Colleague
+        OperationResult Reduce(ReduceInventory command);
+
+        EditInventory GetDetails(int id);
+
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+    }
+}
