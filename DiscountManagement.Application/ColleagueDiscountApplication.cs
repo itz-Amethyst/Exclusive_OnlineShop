6650bb22821e6678d405013047416cde5628f827
callme-devil <pransermi@gmail.com>
2022-08-25 11:47:20 +0430

    feat: ColleagueDiscountApplication Basic Implemented

---
+++ DiscountManagement.Application/ColleagueDiscountApplication.cs
@@ -0,0 +1,46 @@
+﻿using _0_Framework.Application;
+using DiscountManagement.Application.Contract.ColleagueDiscount;
+using DiscountManagement.Domain.ColleagueDiscountsAgg;
+
+namespace DiscountManagement.Application
+{
+    public class ColleagueDiscountApplication : IColleagueDiscountApplication
+    {
+        private readonly IColleagueDiscountRepository _colleagueDiscountRepository;
+
+        public ColleagueDiscountApplication(IColleagueDiscountRepository colleagueDiscountRepository)
+        {
+            _colleagueDiscountRepository = colleagueDiscountRepository;
+        }
+
+        public OperationResult Define(DefineColleagueDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditColleagueDiscount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Restore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditColleagueDiscount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ColleagueDiscountViewModel> Search(ColleagueDiscountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
