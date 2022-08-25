55d3f5277a6c5088899611ed799b0bb234d3676d
callme-devil <pransermi@gmail.com>
2022-08-25 20:00:00 +0430

    feat: InventoryManagementApplicationContract InventoryViewModel Implemented

---
+++ InventoryManagement.Application.Contract/Inventory/InventoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InventoryManagement.Application.Contract.Inventory
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int ProductId { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public bool InStock { get; set; }
+
+        public int CurrentCount { get; set; }
+    }
+}
