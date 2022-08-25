158b39fa8f9998cf1e0581f4747ca13af90e5319
callme-devil <pransermi@gmail.com>
2022-08-25 18:32:42 +0430

    feat: InventoryManagement Implemented with Inventory Filled Class

---
+++ InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Domain;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class Inventory : EntityBase
+    {
+        public int ProductId { get; private set; }
+
+        public int Count { get; private set; }
+
+        public double UnitPrice { get; private set; }
+
+        public bool InStock { get; private set; }
+    }
+}
