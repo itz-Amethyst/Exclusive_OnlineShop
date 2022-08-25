8d565ca183a7bcdf97883208b7dff22df0ab6719
callme-devil <pransermi@gmail.com>
2022-08-25 11:50:58 +0430

    feat: ColleagueDiscountApplication Define Method Implemented

--- DiscountManagement.Application/ColleagueDiscountApplication.cs
+++ DiscountManagement.Application/ColleagueDiscountApplication.cs
@@ -15,7 +15,17 @@ namespace DiscountManagement.Application
 
         public OperationResult Define(DefineColleagueDiscount command)
         {
-            throw new NotImplementedException();
+            var operation = new OperationResult();
+            if (_colleagueDiscountRepository.Exists(x =>
+                    x.ProductId == command.ProductId && x.DiscountRate == command.DiscountRate))
+            {
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            }
+
+            var colleagueDiscount = new ColleagueDiscount(command.ProductId, command.DiscountRate);
+            _colleagueDiscountRepository.Create(colleagueDiscount);
+            _colleagueDiscountRepository.SaveChanges();
+            return operation.Succeeded();
         }
 
         public OperationResult Edit(EditColleagueDiscount command)
