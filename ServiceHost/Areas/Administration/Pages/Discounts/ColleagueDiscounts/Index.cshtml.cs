3e673385fd64ac984025e7d5a96e84c7a762326f
callme-devil <pransermi@gmail.com>
2022-08-25 13:18:41 +0430

    feat: ColleagueDiscount Index Page Implemented

---
+++ ServiceHost/Areas/Administration/Pages/Discounts/ColleagueDiscounts/Index.cshtml.cs
@@ -0,0 +1,64 @@
+using DiscountManagement.Application.Contract.ColleagueDiscount;
+using DiscountManagement.Application.Contract.CustomerDiscount;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShopManagement.Application.Contracts.Product;
+
+namespace ServiceHost.Areas.Administration.Pages.Discounts.ColleagueDiscounts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IProductApplication _productApplication;
+        private readonly IColleagueDiscountApplication _colleagueDiscountApplication;
+
+
+        public ColleagueDiscountSearchModel SearchModel;
+        public List<ColleagueDiscountViewModel> ColleagueDiscounts;
+        public SelectList Products;
+
+        public IndexModel(IProductApplication productApplication, IColleagueDiscountApplication colleagueDiscountApplication)
+        {
+            _productApplication = productApplication;
+            _colleagueDiscountApplication = colleagueDiscountApplication;
+        }
+
+        public void OnGet(ColleagueDiscountSearchModel searchModel , bool inStock = false , bool emptyStock = false)
+        {
+            ViewData["InStock"] = inStock;
+            ViewData["EmptyStock"] = emptyStock;
+            Products = new SelectList(_productApplication.GetProducts(),"Id" , "Name");
+            ColleagueDiscounts = _colleagueDiscountApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            var command = new DefineColleagueDiscount()
+            {
+                Products = _productApplication.GetProducts()
+            };
+            return Partial("./Create", command);
+        }
+
+        public JsonResult OnPostCreate(DefineColleagueDiscount command)
+        {
+            var result = _colleagueDiscountApplication.Define(command);
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(int id)
+        {
+            var colleagueDiscount  = _colleagueDiscountApplication.GetDetails(id);
+            colleagueDiscount.Products = _productApplication.GetProducts();
+            return Partial("Edit", colleagueDiscount);
+        }
+
+        public JsonResult OnPostEdit(EditColleagueDiscount command)
+        {
+            var result = _colleagueDiscountApplication.Edit(command);
+
+            return new JsonResult(result);
+        }
+
+    }
+}
