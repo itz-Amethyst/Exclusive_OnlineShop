c6369db9e49fdfb268c92f936106290a8f55f5a8
callme-devil <pransermi@gmail.com>
2022-08-26 19:28:01 +0430

    feat: InventoryContext Added

---
+++ InventoryManagement.Infrastructure.EFCore/Context/InventoryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.EFCore.Context
+{
+    public class InventoryContext : DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
+        {
+
+        }
+    }
+}
