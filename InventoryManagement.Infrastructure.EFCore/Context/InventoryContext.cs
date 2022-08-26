b92174ae43e7e7cf6357a5ab1fd2e5390b17911d
callme-devil <pransermi@gmail.com>
2022-08-26 19:41:42 +0430

    feat: Inventory Dbset Added to InventoryContext

--- InventoryManagement.Infrastructure.EFCore/Context/InventoryContext.cs
+++ InventoryManagement.Infrastructure.EFCore/Context/InventoryContext.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Mappings;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Infrastructure.EFCore.Context
 {
     public class InventoryContext : DbContext
     {
+        public DbSet<Inventory> Inventories { get; set; }
+
         public InventoryContext(DbContextOptions<InventoryContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(InventoryMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
