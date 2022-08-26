94ae0b5ad052c4d1701b8db77c5b96fc920239b8
callme-devil <pransermi@gmail.com>
2022-08-26 19:24:59 +0430

    feat: IInventoryRepository Domain Added

---
+++ InventoryManagement.Domain/InventoryAgg/IInventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using _0_Framework.Domain;
+using InventoryManagement.Application.Contract.Inventory;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public interface IInventoryRepository : IRepository <int , Inventory>
+    {
+        EditInventory GetDetails(int id);
+
+        List<InventoryViewModel> Search(InventorySearchModel searchModel);
+
+        Inventory GetBy(int productId);
+    }
+}
