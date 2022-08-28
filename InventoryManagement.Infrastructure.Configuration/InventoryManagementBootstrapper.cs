d180ad204fa77fcc36b11e27e9e80cac822325d6
callme-devil <pransermi@gmail.com>
2022-08-28 20:27:40 +0430

    feat: InventoryManagementBootstrapper Wired Up In Layer

--- InventoryManagement.Infrastructure.Configuration/InventoryManagementBootstrapper.cs
+++ InventoryManagement.Infrastructure.Configuration/InventoryManagementBootstrapper.cs
@@ -1,7 +1,22 @@
-﻿namespace InventoryManagement.Infrastructure.Configuration
+﻿using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Context;
+using InventoryManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InventoryManagement.Infrastructure.Configuration
 {
     public class InventoryManagementBootstrapper
     {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            #region Inventory
 
+            services.AddTransient<IInventoryRepository, InventoryRepository>();
+
+            #endregion
+
+            services.AddDbContext<InventoryContext>(x => x.UseSqlServer(connectionString));
+        }
     }
 }
