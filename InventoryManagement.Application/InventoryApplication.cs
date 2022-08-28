f85547c7fb2fca892d25e130e554b3dabe494f7f
callme-devil <pransermi@gmail.com>
2022-08-28 20:54:15 +0430

    feat: InventoryApplication Basic Implemented

---
+++ InventoryManagement.Application/InventoryApplication.cs
@@ -0,0 +1,51 @@
+﻿using _0_Framework.Application;
+using InventoryManagement.Application.Contract.Inventory;
+using InventoryManagement.Domain.InventoryAgg;
+
+namespace InventoryManagement.Application
+{
+    public class InventoryApplication : IInventoryApplication
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public InventoryApplication(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+        public OperationResult Create(CreateInventory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditInventory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Increase(IncreaseInventory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Reduce(List<ReduceInventory> command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Reduce(ReduceInventory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditInventory GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<InventoryViewModel> Search(InventorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
