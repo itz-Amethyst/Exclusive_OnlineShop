bdcb7da40bcf1ff0925573100ffc7d751d3a2311
callme-devil <pransermi@gmail.com>
2022-08-28 20:04:32 +0430

    feat: InventoryRepository Basic Implemented

---
+++ InventoryManagement.Infrastructure.EFCore/Repository/InventoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using _0_Framework.Infrastructure;
+using InventoryManagement.Application.Contract.Inventory;
+using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.EFCore.Repository
+{
+    public class InventoryRepository : RepositoryBase<int , Inventory> , IInventoryRepository
+    {
+        private readonly InventoryContext _context;
+
+        public InventoryRepository(InventoryContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditInventory GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<InventoryViewModel> Search(InventorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Inventory GetBy(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
