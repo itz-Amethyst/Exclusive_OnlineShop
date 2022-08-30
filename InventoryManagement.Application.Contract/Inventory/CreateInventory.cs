2ffea4e0471dfd5ba8cfe969a1a450c5ea31b382
callme-devil <pransermi@gmail.com>
2022-08-30 17:29:11 +0430

    feat: Product List Prop Added to CreateInventory

--- InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
+++ InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
@@ -1,9 +1,13 @@
-﻿namespace InventoryManagement.Application.Contract.Inventory
+﻿using ShopManagement.Application.Contracts.Product;
+
+namespace InventoryManagement.Application.Contract.Inventory
 {
     public class CreateInventory
     {
         public int ProductId { get; set; }
 
         public double UnitPrice { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
     }
 }
