7d88f852463ec5d239f26126af097718d8ec5c00
callme-devil <pransermi@gmail.com>
2022-08-31 19:06:37 +0430

    feat: CreateInventory Server-Side Validations Completed

--- InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
+++ InventoryManagement.Application.Contract/Inventory/CreateInventory.cs
@@ -1,11 +1,15 @@
-﻿using ShopManagement.Application.Contracts.Product;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using ShopManagement.Application.Contracts.Product;
 
 namespace InventoryManagement.Application.Contract.Inventory
 {
     public class CreateInventory
     {
+        [Range(1,100000 , ErrorMessage = ValidationMessages.IsRequired)]
         public int ProductId { get; set; }
 
+        [Range(1, double.MaxValue, ErrorMessage = ValidationMessages.IsRequired)]
         public double UnitPrice { get; set; }
 
         public List<ProductViewModel> Products { get; set; }
