b78c1d96931f91f1e6b11386e7d8a6d520ae4a5e
callme-devil <pransermi@gmail.com>
2022-08-31 11:53:12 +0430

    feat: InventoryOperationViewModel Implemented

---
+++ InventoryManagement.Application.Contract/Inventory/InventoryOperationViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace InventoryManagement.Application.Contract.Inventory
+{
+    public class InventoryOperationViewModel
+    {
+        public int Id { get; set; }
+
+        public bool Operation { get; set; }
+
+        public int Count { get; set; }
+
+        public int OperationId { get; set; }
+
+        public string Operator { get; set; }
+
+        public DateTime OperationDate { get; set; }
+
+        public int CurrentCount { get; set; }
+
+        public string Description { get; set; }
+
+        public int OrderId { get; set; }
+
+    }
+}
