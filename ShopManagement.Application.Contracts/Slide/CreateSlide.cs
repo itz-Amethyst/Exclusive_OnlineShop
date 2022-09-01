da1f1e0bfd1be167168ef4c36118954fa5cbcde3
callme-devil <pransermi@gmail.com>
2022-09-01 17:59:49 +0430

    feat: CreateSlide  Server-Side Validations Completed

--- ShopManagement.Application.Contracts/Slide/CreateSlide.cs
+++ ShopManagement.Application.Contracts/Slide/CreateSlide.cs
@@ -1,23 +1,32 @@
-﻿namespace ShopManagement.Application.Contracts.Slide
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Slide
 {
     public class CreateSlide
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Heading { get; set; }
-
+        
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Title { get; set; }
 
         public string Text { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Link { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string BtnText { get; set; }
 
-        public bool IsRemoved { get; set; }
     }
 }
