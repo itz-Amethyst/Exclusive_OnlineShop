c6728e064cc34028e420a39f84b6c944dea878cb
callme-devil <pransermi@gmail.com>
2022-09-01 19:49:21 +0430

    feat: SlideViewComponent Implement

---
+++ ServiceHost/ViewComponents/SlideViewComponents.cs
@@ -0,0 +1,22 @@
+﻿using _01_ExclusiveQuery.Contracts.Slide;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class SlideViewComponents : ViewComponent
+    {
+        private readonly ISlideQuery _slidQuery;
+
+        public SlideViewComponents(ISlideQuery slideQuery)
+        {
+            _slidQuery = slideQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var slides = _slidQuery.GetSlides();
+
+            return View("/Pages/Shared/Components/Slide/Default.cshtml", slides);
+        }
+    }
+}
