6e32647e090b587b0a077409a570f2e48bb5ddd2
callme-devil <pransermi@gmail.com>
2022-09-01 19:57:18 +0430

    feat: Slide Link Migration Added

---
+++ ShopManagement.Infrastructure.EFCore/Migrations/20220901152138_LinkAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class LinkAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Slides",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Slides");
+        }
+    }
+}
