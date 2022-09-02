2d411f18281c6419d3ddd69dfba5500cf1f2846f
callme-devil <pransermi@gmail.com>
2022-09-02 20:56:15 +0430

    feat: ProductCategoryWIthProductViewComponent Added

---
+++ ServiceHost/ViewComponents/ProductCategoryWIthProductViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using _01_ExclusiveQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ServiceHost.ViewComponents
+{
+    public class ProductCategoryWIthProductViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategoryQuery;
+
+        public ProductCategoryWIthProductViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var productCategory = _productCategoryQuery.GetProductCategoriesWithProducts();
+            return View(productCategory);
+        }
+    }
+}
