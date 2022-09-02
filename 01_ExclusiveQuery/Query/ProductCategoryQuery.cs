c4bdd4a01f14fd42908cfa693a7bea240b57d967
callme-devil <pransermi@gmail.com>
2022-09-02 20:55:49 +0430

    feat: ProductCategoryQuery GetProductCategoriesWithProducts Method Filled

--- 01_ExclusiveQuery/Query/ProductCategoryQuery.cs
+++ 01_ExclusiveQuery/Query/ProductCategoryQuery.cs
@@ -1,11 +1,14 @@
-﻿using _01_ExclusiveQuery.Contracts.ProductCategory;
+﻿using _01_ExclusiveQuery.Contracts.Product;
+using _01_ExclusiveQuery.Contracts.ProductCategory;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductAgg;
 using ShopManagement.Infrastructure.EFCore.Context;
 
 namespace _01_ExclusiveQuery.Query
 {
     public class ProductCategoryQuery : IProductCategoryQuery
     {
-        private readonly  ShopContext _shopContext;
+        private readonly ShopContext _shopContext;
 
         public ProductCategoryQuery(ShopContext shopContext)
         {
@@ -27,7 +30,38 @@ namespace _01_ExclusiveQuery.Query
 
         public List<ProductCategoryQueryModel> GetProductCategoriesWithProducts()
         {
-            throw new NotImplementedException();
+            return _shopContext.ProductCategories.Include(x => x.Products)
+                .ThenInclude(x => x.Category)
+                .Select(x => new ProductCategoryQueryModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Products = MapProducts(x.Products)
+                }).ToList();
+        }
+
+        private List<ProductQueryModel> MapProducts(List<Product> products)
+        {
+            var result = new List<ProductQueryModel>();
+            
+            foreach (var product in products)
+            {
+                var item = new ProductQueryModel
+                {
+                    Id = product.Id,
+                    ProductCategory = product.Category.Name,
+                    Name = product.Name,
+                    Picture = product.Picture,
+                    PictureAlt = product.PictureAlt,
+                    PictureTitle = product.PictureTitle,
+                    Slug = product.Slug
+                };
+
+                result.Add(item);
+            }
+
+            return result;
+
         }
     }
 }
