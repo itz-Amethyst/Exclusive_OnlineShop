e894ff443f14136e9f2e153795f847064c12d74c
callme-devil <pransermi@gmail.com>
2022-09-05 15:55:39 +0430

    feat: IsDeleted Product Migration Added

---
+++ ShopManagement.Infrastructure.EFCore/Migrations/20220905070541_IsDeletedFeautureadded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class IsDeletedFeautureadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Products");
+        }
+    }
+}
