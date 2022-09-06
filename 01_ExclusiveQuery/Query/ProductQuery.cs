8868ceb4248bd4e83b65c8e98a2821726b1cab85
callme-devil <pransermi@gmail.com>
2022-09-06 21:30:29 +0430

    feat: GetOnly 6 last Products filter Added to ProductQuery

--- 01_ExclusiveQuery/Query/ProductQuery.cs
+++ 01_ExclusiveQuery/Query/ProductQuery.cs
@@ -44,7 +44,7 @@ namespace _01_ExclusiveQuery.Query
                     PictureTitle = product.PictureTitle,
                     Slug = product.Slug,
                     IsDeleted = product.IsDeleted
-                }).Where(x => x.IsDeleted == false).ToList();
+                }).Where(x => x.IsDeleted == false).OrderByDescending(x=>x.Id).Take(6).ToList();
 
 
             foreach (var product in products)
