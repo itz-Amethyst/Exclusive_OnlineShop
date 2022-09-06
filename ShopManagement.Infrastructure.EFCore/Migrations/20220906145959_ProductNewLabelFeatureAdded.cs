4548f3dfe0ae8e4e11ddfe44e6d3df6152ab683f
callme-devil <pransermi@gmail.com>
2022-09-06 20:22:38 +0430

    feat: Product new Prop migration added (Part of finally did it 🙃 )

---
+++ ShopManagement.Infrastructure.EFCore/Migrations/20220906145959_ProductNewLabelFeatureAdded.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class ProductNewLabelFeatureAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDateNewLabel",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDateNewLabel",
+                table: "Products");
+        }
+    }
+}
