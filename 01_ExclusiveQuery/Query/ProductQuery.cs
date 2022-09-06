bd0e6ecd4a4796c7563ff20199cf5360832f1034
callme-devil <pransermi@gmail.com>
2022-09-06 21:27:02 +0430

    feat: GetLatestArrivals ProductQuery Implemeneted

---
+++ 01_ExclusiveQuery/Query/ProductQuery.cs
@@ -0,0 +1,87 @@
+﻿using _0_Framework.Application;
+using _01_ExclusiveQuery.Contracts.Product;
+using DiscountManagement.Infrastructure.EFCore.Context;
+using InventoryManagement.Domain.InventoryAgg;
+using InventoryManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.ProductAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace _01_ExclusiveQuery.Query
+{
+    public class ProductQuery : IProductQuery
+    {
+        private readonly ShopContext _shopContext;
+        private readonly InventoryContext _inventoryContext;
+        private readonly DiscountContext _discountContext;
+
+        public ProductQuery(ShopContext shopContext, InventoryContext inventoryContext, DiscountContext discountContext)
+        {
+            _shopContext = shopContext;
+            _inventoryContext = inventoryContext;
+            _discountContext = discountContext;
+        }
+
+        public List<ProductQueryModel> GetLatestArrivals()
+        {
+            var inventory = _inventoryContext.Inventories.Select(x => new { x.ProductId, x.UnitPrice }).ToList();
+
+            var date = _shopContext.Products.Select(x => new { x.CreationDateNewLabel, x.Id }).ToList();
+
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(x => x.StartDate <= DateTime.Now && x.EndDate >= DateTime.Now)
+                .Select(x => new { x.ProductId, x.DiscountRate }).ToList();
+
+            var products = _shopContext.Products
+                .Include(x => x.Category)
+                .Select(product => new ProductQueryModel
+                {
+                    Id = product.Id,
+                    ProductCategory = product.Category.Name,
+                    Name = product.Name,
+                    Picture = product.Picture,
+                    PictureAlt = product.PictureAlt,
+                    PictureTitle = product.PictureTitle,
+                    Slug = product.Slug,
+                    IsDeleted = product.IsDeleted
+                }).Where(x => x.IsDeleted == false).ToList();
+
+
+            foreach (var product in products)
+            {
+
+                var productInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
+
+                if (productInventory != null)
+                {
+                    var price = productInventory.UnitPrice;
+                    product.Price = price.ToMoney();
+                    var discount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+
+                    var productDate = date.FirstOrDefault(x => x.Id == product.Id);
+
+                    var productLabelDate = product.LabelDate = productDate.CreationDateNewLabel;
+
+                    var currentDate = DateTime.Now;
+
+                    product.IsNew = currentDate <= productLabelDate;
+
+                    if (discount != null)
+                    {
+                        int discountRate = discount.DiscountRate;
+
+                        product.DiscountRate = discountRate;
+                        product.HasDiscount = discountRate > 0;
+
+                        var discountAmount = Math.Round((price * discountRate) / 100);
+                        product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                    }
+                }
+
+            }
+
+
+            return products;
+        }
+    }
+}
