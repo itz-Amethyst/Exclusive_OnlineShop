4b8d435721fcab647b2d8f1e3ee471e0c3798b3a
callme-devil <pransermi@gmail.com>
2022-09-07 20:14:08 +0430

    feat: OnGet ProductCategory IndexModel Filled

--- ServiceHost/Pages/ProductCategory.cshtml.cs
+++ ServiceHost/Pages/ProductCategory.cshtml.cs
@@ -1,3 +1,4 @@
+using _01_ExclusiveQuery.Contracts.ProductCategory;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,17 @@ namespace ServiceHost.Pages
 {
     public class ProductCategoryModel : PageModel
     {
+        public ProductCategoryQueryModel ProductCategory;
+        private readonly IProductCategoryQuery _productcategoryQuery;
+
+        public ProductCategoryModel(IProductCategoryQuery productcategoryQuery)
+        {
+            _productcategoryQuery = productcategoryQuery;
+        }
+
         public void OnGet(string id)
         {
+            ProductCategory = _productcategoryQuery.GetProductCategoryWithProductsBy(id);
         }
     }
 }
