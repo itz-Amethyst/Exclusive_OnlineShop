548d7d2081957fc52ee531f53de26a58d99f50da
callme-devil <pransermi@gmail.com>
2022-09-09 18:13:45 +0430

    feat: ProductQuery Search Method Filled

--- 01_ExclusiveQuery/Query/ProductQuery.cs
+++ 01_ExclusiveQuery/Query/ProductQuery.cs
@@ -1,5 +1,6 @@
 ﻿using _0_Framework.Application;
 using _01_ExclusiveQuery.Contracts.Product;
+using _01_ExclusiveQuery.Contracts.ProductCategory;
 using DiscountManagement.Infrastructure.EFCore.Context;
 using InventoryManagement.Domain.InventoryAgg;
 using InventoryManagement.Infrastructure.EFCore.Context;
@@ -86,7 +87,70 @@ namespace _01_ExclusiveQuery.Query
 
         public List<ProductQueryModel> Search(string value)
         {
-            throw new NotImplementedException();
+            var inventory = _inventoryContext.Inventories.Select(x => new { x.ProductId, x.UnitPrice }).ToList();
+
+            var date = _shopContext.Products.Select(x => new { x.CreationDateNewLabel, x.Id }).ToList();
+
+            var discounts = _discountContext.CustomerDiscounts
+                .Where(x => x.StartDate <= DateTime.Now && x.EndDate >= DateTime.Now)
+                .Select(x => new { x.ProductId, x.DiscountRate, x.EndDate }).ToList();
+
+            var query = _shopContext.Products
+                .Include(x => x.Category)
+                .Select(product => new ProductQueryModel()
+                {
+                    Id = product.Id,
+                    ProductCategory = product.Category.Name,
+                    Name = product.Name,
+                    Picture = product.Picture,
+                    PictureAlt = product.PictureAlt,
+                    PictureTitle = product.PictureTitle,
+                    Slug = product.Slug,
+                    IsDeleted = product.IsDeleted
+                }).Where(x=>x.IsDeleted == false).AsNoTracking();
+
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                query = query.Where(x => x.Name.Contains(value) || x.ShortDescription.Contains(value));
+            }
+
+            var products = query.OrderByDescending(x => x.Id).ToList();
+
+            foreach (var product in products)
+            {
+                var productInventory = inventory.FirstOrDefault(x => x.ProductId == product.Id);
+
+                if (productInventory != null)
+                {
+                    var price = productInventory.UnitPrice;
+                    product.Price = price.ToMoney();
+                    var discount = discounts.FirstOrDefault(x => x.ProductId == product.Id);
+
+                    var productDate = date.FirstOrDefault(x => x.Id == product.Id);
+
+                    var productLabelDate = product.LabelDate = productDate.CreationDateNewLabel;
+
+                    var currentDate = DateTime.Now;
+
+                    product.IsNew = currentDate <= productLabelDate;
+
+                    if (discount != null)
+                    {
+                        int discountRate = discount.DiscountRate;
+
+                        product.DiscountRate = discountRate;
+
+                        product.DiscountEndDate = discount.EndDate.ToDiscountFormat();
+
+                        product.HasDiscount = discountRate > 0;
+
+                        var discountAmount = Math.Round((price * discountRate) / 100);
+                        product.PriceWithDiscount = (price - discountAmount).ToMoney();
+                    }
+                }
+            }
+
+            return products;
         }
     }
 }
