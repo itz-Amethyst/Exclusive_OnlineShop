620ad8158605c664435d32030b36d6a9fb853de1
callme-devil <pransermi@gmail.com>
2022-09-09 13:14:08 +0430

    feat: Moved Layout Header Section to ViewComponent

---
+++ ServiceHost/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using _01_ExclusiveQuery.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly IProductCategoryQuery _productCategoryQuery;
+
+        public HeaderViewComponent(IProductCategoryQuery productCategoryQuery)
+        {
+            _productCategoryQuery = productCategoryQuery;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
