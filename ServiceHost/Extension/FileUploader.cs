2aa0d222bf655b1ca7f6b7358a359c7e118ede81
callme-devil <pransermi@gmail.com>
2022-09-10 17:06:24 +0430

    feat: Empty File Uploader Extension Added

---
+++ ServiceHost/Extension/FileUploader.cs
@@ -0,0 +1,12 @@
+﻿using _0_Framework.Application;
+
+namespace ServiceHost.Extension
+{
+    public class FileUploader : IFileUploader
+    {
+        public string Upload(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
