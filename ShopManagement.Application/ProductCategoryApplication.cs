7feda978813378a36de2448e75bc7b49a987b9ce
callme-devil <pransermi@gmail.com>
2022-09-10 19:14:14 +0430

    feat: Edit ProductCategory Image File Upload Completed

--- ShopManagement.Application/ProductCategoryApplication.cs
+++ ShopManagement.Application/ProductCategoryApplication.cs
@@ -1,4 +1,5 @@
-﻿using _0_Framework.Application;
+﻿using System.Runtime.CompilerServices;
+using _0_Framework.Application;
 using ShopManagement.Application.Contracts.ProductCategory;
 using ShopManagement.Domain.ProductCategoryAgg;
 
@@ -6,11 +7,13 @@ namespace ShopManagement.Application
 {
     public class ProductCategoryApplication : IProductCategoryApplication
     {
+        private readonly IFileUploader _fileUploader;
         private readonly IProductCategoryRepository _productCategoryRepository;
 
-        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository, IFileUploader fileUploader)
         {
             _productCategoryRepository = productCategoryRepository;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateProductCategory command)
@@ -47,7 +50,9 @@ namespace ShopManagement.Application
             }
 
             var slug = command.Slug.Slugify();
-            productCategory.Edit(command.Name, command.Description, command.Picture,
+            var picturePath = $"{command.Slug}//";
+            var fileName = _fileUploader.Upload(command.Picture , picturePath);
+            productCategory.Edit(command.Name, command.Description, fileName,
                 command.PictureAlt, command.PictureTitle, command.Keywords, command.MetaDescription, slug);
 
             _productCategoryRepository.SaveChanges();
