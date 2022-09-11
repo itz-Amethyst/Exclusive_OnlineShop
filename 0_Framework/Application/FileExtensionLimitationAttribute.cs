6b488ef44949c5a33c569ab88865e8b181e2248b
callme-devil <pransermi@gmail.com>
2022-09-11 15:13:52 +0430

    feat: Empty FileExtensionLimitationAttribute Added

---
+++ 0_Framework/Application/FileExtensionLimitationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace _0_Framework.Application
+{
+    public class FileExtensionLimitationAttribute :ValidationAttribute , IClientModelValidator
+    {
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
