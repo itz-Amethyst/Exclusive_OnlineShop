39145cf0458b6006274e65b20dad77837d970dd9
callme-devil <pransermi@gmail.com>
2022-09-11 19:51:14 +0430

    feat: InvalidFileFormat Validation Message added

--- 0_Framework/Application/ValidationMessages.cs
+++ 0_Framework/Application/ValidationMessages.cs
@@ -5,5 +5,6 @@
         public const string IsRequired = "این مقدار نمیتواند خالی باشد";
         public const string NotValid = "مقدار وارد شده صحیح نمیباشد";
         public const string MaxFileSize = "فایل حجیم تر ازحد مجاز است";
+        public const string InvalidFileFormat = "فرمت فایل مجاز نیست لطفا فقط (jpg , jpeg , png) انتخاب کنید";
     }
 }
