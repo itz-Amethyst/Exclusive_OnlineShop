341605320b50174ecabf4439c0744d10ca941536
callme-devil <pransermi@gmail.com>
2022-09-11 14:47:29 +0430

    feat: MaxFileSizeAttribute Validation Void Added

--- 0_Framework/Application/MaxFileSizeAttribute.cs
+++ 0_Framework/Application/MaxFileSizeAttribute.cs
@@ -1,9 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace _0_Framework.Application
 {
-    public class MaxFileSizeAttribute : ValidationAttribute
+    public class MaxFileSizeAttribute : ValidationAttribute , IClientModelValidator
     {
         private readonly int _maxFileSize;
 
@@ -19,6 +20,12 @@ namespace _0_Framework.Application
             if (file == null) return true;
 
             return file.Length <= _maxFileSize;
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            context.Attributes.Add("data-value" , "true");
+            context.Attributes.Add("data-val-maxFileSize" , ErrorMessage);
         }
     }
 }
