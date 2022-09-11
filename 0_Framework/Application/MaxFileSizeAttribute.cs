407a299092da51136befdf93bde4e3b8a41adaf4
callme-devil <pransermi@gmail.com>
2022-09-11 14:01:09 +0430

    feat: MaxFileSize Class for pictures Added

---
+++ 0_Framework/Application/MaxFileSizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace _0_Framework.Application
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var file = value as IFormFile;
+
+            if (file == null) return true;
+
+            return file.Length <= _maxFileSize;
+        }
+    }
+}
