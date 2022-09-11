22c78b9eef52dea8c3b3fb70e959622239f2a448
callme-devil <pransermi@gmail.com>
2022-09-11 19:51:53 +0430

    feat: FileExtensionLimitationAttribute Filled

--- 0_Framework/Application/FileExtensionLimitationAttribute.cs
+++ 0_Framework/Application/FileExtensionLimitationAttribute.cs
@@ -1,13 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace _0_Framework.Application
 {
     public class FileExtensionLimitationAttribute :ValidationAttribute , IClientModelValidator
     {
+        private readonly string[] _validExtensions;
+
+        public FileExtensionLimitationAttribute(string[] validExtensions)
+        {
+            _validExtensions = validExtensions;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var file = value as IFormFile;
+            if (file == null) return true;
+
+            var fileExtension = Path.GetExtension(file.FileName);
+
+            return _validExtensions.Contains(fileExtension);
+        }
+
         public void AddValidation(ClientModelValidationContext context)
         {
-            throw new NotImplementedException();
+            //context.Attributes.Add("data-value", "true");
+            context.Attributes.Add("data-val-fileExtensionLimit", ErrorMessage);
         }
     }
 }
