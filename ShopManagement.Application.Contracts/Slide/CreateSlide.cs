697ac5d9699dfd8fb97533d2c50a086060041ac6
callme-devil <pransermi@gmail.com>
2022-09-12 15:44:32 +0430

    feat: Changed CreateSlide Picture type from string to IFormFile

--- ShopManagement.Application.Contracts/Slide/CreateSlide.cs
+++ ShopManagement.Application.Contracts/Slide/CreateSlide.cs
@@ -1,12 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
 
 namespace ShopManagement.Application.Contracts.Slide
 {
     public class CreateSlide
     {
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
-        public string Picture { get; set; }
+        [MaxFileSize(5 * 1024 * 1024 , ErrorMessage = ValidationMessages.MaxFileSize)]
+        public IFormFile Picture { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
