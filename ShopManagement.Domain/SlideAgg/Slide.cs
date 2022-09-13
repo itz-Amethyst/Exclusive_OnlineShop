b9217fc081ea805a00e434841dd6216747e58a02
callme-devil <pransermi@gmail.com>
2022-09-13 13:03:24 +0430

    feat: Business Logic Added in Slide Edit Method

--- ShopManagement.Domain/SlideAgg/Slide.cs
+++ ShopManagement.Domain/SlideAgg/Slide.cs
@@ -38,7 +38,10 @@ namespace ShopManagement.Domain.SlideAgg
 
         public void Edit(string picture, string pictureAlt, string pictureTitle, string heading, string title, string text, string btnText , string link)
         {
-            Picture = picture;
+            if (!string.IsNullOrWhiteSpace(picture))
+            {
+                Picture = picture;
+            }
             PictureAlt = pictureAlt;
             PictureTitle = pictureTitle;
             Heading = heading;
