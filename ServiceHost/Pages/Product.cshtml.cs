26d7d878b4852dd1baecd466bd4d9a7dd8ed2db6
callme-devil <pransermi@gmail.com>
2022-09-14 12:18:58 +0430

    feat: Product GetDetails Method Injected

--- ServiceHost/Pages/Product.cshtml.cs
+++ ServiceHost/Pages/Product.cshtml.cs
@@ -1,12 +1,21 @@
-using Microsoft.AspNetCore.Mvc;
+using _01_ExclusiveQuery.Contracts.Product;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
 {
     public class ProductModel : PageModel
     {
-        public void OnGet()
+        private readonly IProductQuery _productQuery;
+        public ProductQueryModel Product;
+
+        public ProductModel(IProductQuery productQuery)
         {
+            _productQuery = productQuery;
+        }
+
+        public void OnGet(string id)
+        {
+            Product = _productQuery.GetDetails(id);
         }
     }
 }
