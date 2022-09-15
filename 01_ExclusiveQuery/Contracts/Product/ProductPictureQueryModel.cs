e8506a145d35e1734d73d4975cf72144be75c8d6
callme-devil <pransermi@gmail.com>
2022-09-15 14:21:56 +0430

    feat: ProductPictureQueryModel Implemented

---
+++ 01_ExclusiveQuery/Contracts/Product/ProductPictureQueryModel.cs
@@ -0,0 +1,16 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Product
+{
+    public class ProductPictureQueryModel
+    {
+        public int ProductId { get; set; }
+
+        public string Picture { get; set; }
+
+        public string PictureAlt { get; set; }
+
+        public string PictureTitle { get; set; }
+
+        public bool IsRemoved { get; set; }
+
+    }
+}
