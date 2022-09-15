ac4364f68297fae02ee79c50d18c0bb4844be27f
callme-devil <pransermi@gmail.com>
2022-09-15 16:10:03 +0430

    feat: CommentSearchModel Implemented

---
+++ ShopManagement.Application.Contracts/Comment/CommentSearchModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopManagement.Application.Contracts.Comment
+{
+    public class CommentSearchModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
