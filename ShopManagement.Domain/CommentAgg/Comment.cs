0c5a3022b0e9cba69100b10c71f42c456ee1243c
callme-devil <pransermi@gmail.com>
2022-09-15 15:59:53 +0430

    feat: Comment Domain Agg Created & Filled

---
+++ ShopManagement.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,30 @@
+﻿using _0_Framework.Domain;
+using ShopManagement.Domain.ProductAgg;
+
+namespace ShopManagement.Domain.CommentAgg
+{
+    public class Comment : EntityBase
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Message { get; private set; }
+
+        public int ProductId { get; private set; }
+
+        public Product Product { get; private set; }
+
+        public bool IsConfirmed { get; private set; }
+
+        public bool IsCanceled { get; private set; }
+
+        public Comment(string name, string email, string message, int productId)
+        {
+            Name = name;
+            Email = email;
+            Message = message;
+            ProductId = productId;
+        }
+    }
+}
