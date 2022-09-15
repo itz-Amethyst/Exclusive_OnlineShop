120f1ed435005961c5984cd8f590f05c0a1ddf6b
callme-devil <pransermi@gmail.com>
2022-09-15 16:22:51 +0430

    feat: Comment Mapping Referenced Inside Product Mapping

--- ShopManagement.Infrastructure.EFCore/Mappings/ProductMapping.cs
+++ ShopManagement.Infrastructure.EFCore/Mappings/ProductMapping.cs
@@ -29,6 +29,10 @@ namespace ShopManagement.Infrastructure.EFCore.Mappings
             builder.HasMany(x=>x.ProductPictures)
                 .WithOne(x=>x.Product)
                 .HasForeignKey(x => x.ProductId);
+
+            builder.HasMany(x => x.Comments)
+                .WithOne(x => x.Product)
+                .HasForeignKey(x => x.ProductId);
         }
     }
 }
