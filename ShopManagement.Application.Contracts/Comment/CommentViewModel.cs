62af49cafd22b7e0fb0c56e939f299aa7faf7bfd
callme-devil <pransermi@gmail.com>
2022-09-15 16:09:46 +0430

    feat: CommentViewModel Implemented

---
+++ ShopManagement.Application.Contracts/Comment/CommentViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ShopManagement.Application.Contracts.Comment
+{
+    public class CommentViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public bool IsCanceled { get; set; }
+    }
+}
