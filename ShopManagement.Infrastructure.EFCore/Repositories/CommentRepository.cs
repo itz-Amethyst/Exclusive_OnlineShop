5316ca0574d3194cfb575ff61f7bbf948f44f171
callme-devil <pransermi@gmail.com>
2022-09-15 16:32:24 +0430

    feat: CommentRepository Search Method Filled

--- ShopManagement.Infrastructure.EFCore/Repositories/CommentRepository.cs
+++ ShopManagement.Infrastructure.EFCore/Repositories/CommentRepository.cs
@@ -1,4 +1,5 @@
-﻿using _0_Framework.Infrastructure;
+﻿using _0_Framework.Application;
+using _0_Framework.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using ShopManagement.Application.Contracts.Comment;
 using ShopManagement.Domain.CommentAgg;
@@ -6,7 +7,7 @@ using ShopManagement.Infrastructure.EFCore.Context;
 
 namespace ShopManagement.Infrastructure.EFCore.Repositories
 {
-    public class CommentRepository :RepositoryBase<int , Comment>, ICommentRepository
+    public class CommentRepository : RepositoryBase<int, Comment>, ICommentRepository
     {
         private readonly ShopContext _context;
 
@@ -17,7 +18,32 @@ namespace ShopManagement.Infrastructure.EFCore.Repositories
 
         public List<CommentViewModel> Search(CommentSearchModel searchModel)
         {
-            throw new NotImplementedException();
+            var query = _context.Comments
+                .Include(x => x.Product)
+                .Select(x => new CommentViewModel
+                {
+                    Id = x.Id,
+                    Email = x.Email,
+                    IsConfirmed = x.IsConfirmed,
+                    IsCanceled = x.IsCanceled,
+                    Message = x.Message,
+                    Name = x.Name,
+                    ProductId = x.ProductId,
+                    ProductName = x.Product.Name,
+                    CommentCreatedDate = x.CreationDate.ToFarsi()
+                });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+            {
+                query = query.Where(x => x.Name.Contains(searchModel.Name));
+            }
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Email))
+            {
+                query = query.Where(x => x.Email.Contains(searchModel.Email));
+            }
+
+            return query.OrderByDescending(x => x.Id).ToList();
         }
     }
 }
