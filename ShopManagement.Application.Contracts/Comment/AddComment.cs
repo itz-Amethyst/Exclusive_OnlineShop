1c1aee8a514eab3c473171aa54731ae3557ebf9d
callme-devil <pransermi@gmail.com>
2022-09-15 16:04:25 +0430

    feat: AddComment Implemented

---
+++ ShopManagement.Application.Contracts/Comment/AddComment.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopManagement.Application.Contracts.Comment
+{
+    public class AddComment
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+        public int ProductId { get; set; }
+        
+    }
+}
