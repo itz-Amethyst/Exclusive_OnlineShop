38f3f2214c2aff31e7a3350f2d8af2de4bceb1bb
callme-devil <pransermi@gmail.com>
2022-09-15 16:19:33 +0430

    feat: ICommentRepository Filled With Search Method

--- ShopManagement.Domain/CommentAgg/ICommentRepository.cs
+++ ShopManagement.Domain/CommentAgg/ICommentRepository.cs
@@ -1,9 +1,10 @@
 ﻿using _0_Framework.Domain;
+using ShopManagement.Application.Contracts.Comment;
 
 namespace ShopManagement.Domain.CommentAgg
 {
     public interface ICommentRepository :IRepository<int , Comment>
     {
-        
+        List<CommentViewModel> Search(CommentSearchModel searchModel);
     }
 }
