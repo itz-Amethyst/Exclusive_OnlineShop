34c3fb105dfcfeeae98959cfb4a76577e19e1682
callme-devil <pransermi@gmail.com>
2022-09-15 16:21:49 +0430

    feat: Comment Mapping Implemented

---
+++ ShopManagement.Infrastructure.EFCore/Mappings/CommentMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopManagement.Domain.CommentAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Mappings
+{
+    public class CommentMapping : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.Message).HasMaxLength(100).IsRequired();
+
+            builder.HasOne(x => x.Product)
+                .WithMany(x => x.Comments)
+                .HasForeignKey(x => x.ProductId);
+        }
+    }
+}
