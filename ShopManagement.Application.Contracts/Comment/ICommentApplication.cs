990893fa48f5531077af3c2847dc688b0e2b0193
callme-devil <pransermi@gmail.com>
2022-09-15 16:13:02 +0430

    feat: ICommentApplication Created & Filled

---
+++ ShopManagement.Application.Contracts/Comment/ICommentApplication.cs
@@ -0,0 +1,16 @@
+﻿using _0_Framework.Application;
+
+namespace ShopManagement.Application.Contracts.Comment
+{
+    public interface ICommentApplication
+    {
+        OperationResult Add(AddComment command);
+
+        OperationResult Confirm(int id);
+
+        OperationResult Cancel(int id);
+
+        List<CommentViewModel> Search(CommentSearchModel searchModel);
+
+    }
+}
