28d1b87b1d741f660822d658a818e842591395f5
callme-devil <pransermi@gmail.com>
2022-09-15 16:23:09 +0430

    feat: Base CommentRepository Created

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/CommentRepository.cs
@@ -0,0 +1,23 @@
+﻿using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Application.Contracts.Comment;
+using ShopManagement.Domain.CommentAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class CommentRepository :RepositoryBase<int , Comment>, ICommentRepository
+    {
+        private readonly ShopContext _context;
+
+        public CommentRepository(ShopContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<CommentViewModel> Search(CommentSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
