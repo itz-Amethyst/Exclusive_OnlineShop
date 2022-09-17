1189a942f1a0ebbd7625d63cff101ec1adcc131c
callme-devil <pransermi@gmail.com>
2022-09-17 15:19:01 +0430

    feat: Product Add Comment Section Index Model Page Completed

--- ServiceHost/Pages/Product.cshtml.cs
+++ ServiceHost/Pages/Product.cshtml.cs
@@ -1,21 +1,31 @@
 using _01_ExclusiveQuery.Contracts.Product;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.Comment;
 
 namespace ServiceHost.Pages
 {
     public class ProductModel : PageModel
     {
         private readonly IProductQuery _productQuery;
+        private readonly ICommentApplication _commentApplication;
         public ProductQueryModel Product;
 
-        public ProductModel(IProductQuery productQuery)
+        public ProductModel(IProductQuery productQuery, ICommentApplication commentApplication)
         {
             _productQuery = productQuery;
+            _commentApplication = commentApplication;
         }
 
         public void OnGet(string id)
         {
             Product = _productQuery.GetDetails(id);
+        }
+
+        public IActionResult OnPost(AddComment command)
+        {
+            var result = _commentApplication.Add(command);
+            return RedirectToPage("/Product" , new {Id = command.ProductId});
         }
     }
 }
