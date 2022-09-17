ef261fd311c278438730e69d43ee7502e389ca1c
callme-devil <pransermi@gmail.com>
2022-09-17 16:34:42 +0430

    feat: CommentQueryModel Implemented

---
+++ 01_ExclusiveQuery/Contracts/Comment/CommentQueryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Comment
+{
+    public class CommentQueryModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
