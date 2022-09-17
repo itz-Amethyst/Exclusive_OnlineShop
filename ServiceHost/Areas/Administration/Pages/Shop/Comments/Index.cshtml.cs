d21b4a15df1e188650e19ae28554aa78acf2ba6c
callme-devil <pransermi@gmail.com>
2022-09-17 12:32:04 +0430

    feat: Comment IndexModel Page Implemented

---
+++ ServiceHost/Areas/Administration/Pages/Shop/Comments/Index.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.Comment;
+
+namespace ServiceHost.Areas.Administration.Pages.Shop.Comments
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ICommentApplication _commentApplication;
+      
+
+        public List<CommentViewModel> Comments;
+        public CommentSearchModel SearchModel;
+
+        public IndexModel(ICommentApplication commentApplication)
+        {
+            _commentApplication = commentApplication;
+        }
+
+        public void OnGet(CommentSearchModel searchModel, bool confirmed = false , bool canceled = false)
+        {
+            ViewData["Confirmed"] = confirmed;
+            ViewData["Canceled"] = canceled;
+            Comments = _commentApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCancel(int id)
+        {
+            var result = _commentApplication.Cancel(id);
+           
+            return RedirectToPage("./Index" , new {Canceled="True"});
+            
+        }
+
+        public IActionResult OnGetConfirm(int id)
+        {
+            var result = _commentApplication.Confirm(id);
+
+            return RedirectToPage("./Index", new { Confirmed = "True" });
+        }
+    }
+}
