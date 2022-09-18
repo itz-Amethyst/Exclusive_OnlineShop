e27259e2214152bc6d2e97a331586b0f26c8c239
callme-devil <pransermi@gmail.com>
2022-09-18 11:08:50 +0430

    feat: ArticleCategoryApplication Basic Implemented

---
+++ BlogManagement.Application/ArticleCategoryApplication.cs
@@ -0,0 +1,31 @@
+﻿using _0_Framework.Application;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategoryAgg;
+
+namespace BlogManagement.Application
+{
+    public class ArticleCategoryApplication : IArticleCategoryApplication
+    {
+        private readonly IArticleCategoryRepository _articleCategoryRepository;
+
+        public ArticleCategoryApplication(IArticleCategoryRepository articleCategoryRepository)
+        {
+            _articleCategoryRepository = articleCategoryRepository;
+        }
+
+        public OperationResult Create(CreateArticleCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditArticleCategory command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
