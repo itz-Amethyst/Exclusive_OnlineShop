8ce2e2e6465fb3795e01f8e0207bf77a14f3fd53
callme-devil <pransermi@gmail.com>
2022-09-18 11:04:23 +0430

    feat: IArticleCategoryApplication Created & Filled

---
+++ BlogManagement.Application.Contracts/ArticleCategory/IArticleCategoryApplication.cs
@@ -0,0 +1,13 @@
+﻿using _0_Framework.Application;
+
+namespace BlogManagement.Application.Contracts.ArticleCategory
+{
+    public interface IArticleCategoryApplication
+    {
+        OperationResult Create(CreateArticleCategory command);
+
+        OperationResult Edit(EditArticleCategory command);
+
+        List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
+    }
+}
