9cfac357e4bafacb3918993ae60d3f153e840538
callme-devil <pransermi@gmail.com>
2022-09-18 10:58:19 +0430

    feat: CreateArticleCategory Implemented

---
+++ BlogManagement.Application.Contracts/ArticleCategory/CreateArticleCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BlogManagement.Application.Contracts.ArticleCategory
+{
+    public class CreateArticleCategory
+    {
+        public string Name { get; set; }
+
+        public IFormFile Picture { get; set; }
+
+        public string Description { get; set; }
+
+        public int ShowOrder { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Keywords { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public string CannoicalAddress { get; set; }
+    }
+}
