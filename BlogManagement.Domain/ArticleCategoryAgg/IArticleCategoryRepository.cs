2feaac65348e0a22a1dbee9c794de41cc9d99518
callme-devil <pransermi@gmail.com>
2022-09-18 10:55:52 +0430

    feat: IArticleCategoryRepository Empty Implemented

---
+++ BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using _0_Framework.Domain;
+
+namespace BlogManagement.Domain.ArticleCategoryAgg
+{
+    public interface IArticleCategoryRepository : IRepository<int , ArticleCategory>
+    {
+
+    }
+}
