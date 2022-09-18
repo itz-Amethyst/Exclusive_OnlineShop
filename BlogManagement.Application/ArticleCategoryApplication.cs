39d7238a2b9efc91bcf87c01373ac588bd194f84
callme-devil <pransermi@gmail.com>
2022-09-18 11:18:18 +0430

    feat: ArticleCategoryApplication Create Method Filled

--- BlogManagement.Application/ArticleCategoryApplication.cs
+++ BlogManagement.Application/ArticleCategoryApplication.cs
@@ -6,16 +6,34 @@ namespace BlogManagement.Application
 {
     public class ArticleCategoryApplication : IArticleCategoryApplication
     {
+        private readonly IFileUploader _fileUploader;
         private readonly IArticleCategoryRepository _articleCategoryRepository;
 
-        public ArticleCategoryApplication(IArticleCategoryRepository articleCategoryRepository)
+        public ArticleCategoryApplication(IArticleCategoryRepository articleCategoryRepository, IFileUploader fileUploader)
         {
             _articleCategoryRepository = articleCategoryRepository;
+            _fileUploader = fileUploader;
         }
 
         public OperationResult Create(CreateArticleCategory command)
         {
-            throw new NotImplementedException();
+            var operation = new OperationResult();
+
+            if (_articleCategoryRepository.Exists(x=> x.Name == command.Name))
+            {
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+            }
+
+            var slug = command.Slug.Slugify();
+            var picturePath =_fileUploader.Upload(command.Picture, slug);
+
+            var articleCategory = new ArticleCategory(command.Name, picturePath, command.Description, command.ShowOrder,
+                slug, command.Keywords, command.MetaDescription, command.CannoicalAddress);
+
+            _articleCategoryRepository.Create(articleCategory);
+            _articleCategoryRepository.SaveChanges();
+
+            return operation.Succeeded();
         }
 
         public OperationResult Edit(EditArticleCategory command)
