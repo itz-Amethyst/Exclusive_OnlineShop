827f63d2ce32931d78e633cd138473de70e48880
callme-devil <pransermi@gmail.com>
2022-09-18 11:00:23 +0430

    feat: ArticleCategoryViewModel Implemented

---
+++ BlogManagement.Application.Contracts/ArticleCategory/ArticleCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BlogManagement.Application.Contracts.ArticleCategory
+{
+    public class ArticleCategoryViewModel
+    {
+        public string Name { get; set; }
+
+        public IFormFile Picture { get; set; }
+
+        public string Description { get; set; }
+
+        public int ShowOrder { get; set; }
+
+    }
+}
