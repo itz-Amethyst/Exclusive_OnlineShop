17c7d3f7dad511811496fcda9da30dcf83cfb77d
callme-devil <pransermi@gmail.com>
2022-09-19 22:49:24 +0430

    feat: BlogManagementBootstrapper Configure Method Filled

--- BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
+++ BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BlogManagement.Application;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategoryAgg;
+using BlogManagement.Infrastructure.EFCore.Context;
+using BlogManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlogManagement.Infrastructure.Configuration
 {
@@ -6,6 +12,14 @@ namespace BlogManagement.Infrastructure.Configuration
     {
         public static void Configure(IServiceCollection services, string connectionString)
         {
+            #region ArticleCategory
+
+            services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
+            services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
+
+            #endregion
+
+            services.AddDbContext<BlogContext>(x => x.UseSqlServer(connectionString));
 
         }
     }
