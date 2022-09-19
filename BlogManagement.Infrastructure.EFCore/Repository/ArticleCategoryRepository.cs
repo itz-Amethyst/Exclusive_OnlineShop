6fbdafe46fa53b86cbcca2a46dd6d3adf09432be
callme-devil <pransermi@gmail.com>
2022-09-19 22:23:57 +0430

    feat: ArticleCategoryRepository Base Created

---
+++ BlogManagement.Infrastructure.EFCore/Repository/ArticleCategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using _0_Framework.Infrastructure;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using BlogManagement.Domain.ArticleCategoryAgg;
+using BlogManagement.Infrastructure.EFCore.Context;
+
+namespace BlogManagement.Infrastructure.EFCore.Repository
+{
+    public class ArticleCategoryRepository : RepositoryBase<int , ArticleCategory> , IArticleCategoryRepository
+    {
+        private readonly BlogContext _context;
+        
+        public ArticleCategoryRepository(BlogContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditArticleCategory GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
