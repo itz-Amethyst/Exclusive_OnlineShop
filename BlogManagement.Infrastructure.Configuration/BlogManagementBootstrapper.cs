1980548e607861339a8a29ea9e8726204fc48383
callme-devil <pransermi@gmail.com>
2022-09-19 22:07:51 +0430

    feat: BlogManagementBootstraper Empty Added

---
+++ BlogManagement.Infrastructure.Configuration/BlogManagementBootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogManagement.Infrastructure.Configuration
+{
+    public class BlogManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+
+        }
+    }
+}
