a1d4dc0b9bf77cb35a4950002e8b6eaa702af02c
callme-devil <pransermi@gmail.com>
2022-09-19 22:14:19 +0430

    feat: BlogContext Added And their references

---
+++ BlogManagement.Infrastructure.EFCore/Context/BlogContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogManagement.Domain.ArticleCategoryAgg;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogManagement.Infrastructure.EFCore.Context
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<ArticleCategory> ArticleCategories { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            
+        }
+    }
+}
