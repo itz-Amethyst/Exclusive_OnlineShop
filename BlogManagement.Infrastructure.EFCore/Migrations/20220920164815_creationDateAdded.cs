52369813f7eae464f20cae7a1be1a6054033727d
callme-devil <pransermi@gmail.com>
2022-09-20 21:41:34 +0430

    feat: CreationDateAdded migration Completed

---
+++ BlogManagement.Infrastructure.EFCore/Migrations/20220920164815_creationDateAdded.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class creationDateAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArticleCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Picture = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    PictureAlt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PictureTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    ShowOrder = table.Column<int>(type: "int", nullable: false),
+                    Slug = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Keywords = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    MetaDescription = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CanonicalAddress = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleCategories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleCategories");
+        }
+    }
+}
