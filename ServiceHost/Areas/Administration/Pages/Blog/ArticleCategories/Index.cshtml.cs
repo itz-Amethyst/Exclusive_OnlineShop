7419cc6a11103000568145791657273898498886
callme-devil <pransermi@gmail.com>
2022-09-20 20:00:30 +0430

    feat: Index Model CreateArticleCategory Implemented

---
+++ ServiceHost/Areas/Administration/Pages/Blog/ArticleCategories/Index.cshtml.cs
@@ -0,0 +1,49 @@
+using BlogManagement.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages.Blog.ArticleCategories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+
+        public ArticleCategorySearchModel SearchModel;
+        public List<ArticleCategoryViewModel> ArticleCategories;
+
+        public IndexModel(IArticleCategoryApplication articleCategoryApplication)
+        {
+            _articleCategoryApplication = articleCategoryApplication;
+        }
+
+        public void OnGet(ArticleCategorySearchModel searchModel)
+        {
+            ArticleCategories = _articleCategoryApplication.Search(searchModel);
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            return Partial("./Create", new CreateArticleCategory());
+        }
+
+        public JsonResult OnPostCreate(CreateArticleCategory command)
+        {
+            var result = _articleCategoryApplication.Create(command);
+            return new JsonResult(result);
+        }
+
+        public IActionResult OnGetEdit(int id)
+        {
+            var articleCategory = _articleCategoryApplication.GetDetails(id);
+
+            return Partial("Edit", articleCategory);
+        }
+
+        public JsonResult OnPostEdit(EditArticleCategory command)
+        {
+            var result = _articleCategoryApplication.Edit(command);
+
+            return new JsonResult(result);
+        }
+    }
+}
