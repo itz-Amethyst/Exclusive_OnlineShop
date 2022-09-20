f81cb854a66af12617c4d9f127d138cd711e7438
callme-devil <pransermi@gmail.com>
2022-09-20 22:23:43 +0430

    feat: IsRequired CanonicalAddress is set to false

---
+++ BlogManagement.Infrastructure.EFCore/Migrations/20220920171219_canonicalAddressIsRequiredfalse.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class canonicalAddressIsRequiredfalse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CanonicalAddress",
+                table: "ArticleCategories",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CanonicalAddress",
+                table: "ArticleCategories",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
