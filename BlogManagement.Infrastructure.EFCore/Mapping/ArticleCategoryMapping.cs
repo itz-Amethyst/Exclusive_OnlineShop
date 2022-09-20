6ab2c60e45828fa2a192d274c156924c8e862cef
callme-devil <pransermi@gmail.com>
2022-09-20 21:40:48 +0430

    refactor: ArticleCategoryMapping Refactored

--- BlogManagement.Infrastructure.EFCore/Mapping/ArticleCategoryMapping.cs
+++ BlogManagement.Infrastructure.EFCore/Mapping/ArticleCategoryMapping.cs
@@ -12,12 +12,12 @@ namespace BlogManagement.Infrastructure.EFCore.Mapping
             builder.HasKey(x => x.Id);
 
             builder.Property(x => x.Name).HasMaxLength(500).IsRequired();
-            builder.Property(x => x.Description).HasMaxLength(2000);
-            builder.Property(x => x.Picture).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(2000).IsRequired();
+            builder.Property(x => x.Picture).HasMaxLength(500);
             builder.Property(x => x.Slug).HasMaxLength(500).IsRequired();
             builder.Property(x => x.Keywords).HasMaxLength(500).IsRequired();
             builder.Property(x => x.MetaDescription).HasMaxLength(500).IsRequired();
-            builder.Property(x => x.CanonicalAddress).HasMaxLength(500);
+            builder.Property(x => x.CanonicalAddress).HasMaxLength(500).IsRequired(false);
 
         }
     }
