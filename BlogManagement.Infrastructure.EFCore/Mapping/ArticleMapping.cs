03e520326b42c5d336f946fd9e9f58f18d8cc6c9
callme-devil <pransermi@gmail.com>
2022-09-22 18:25:16 +0330

    feat: ArticleMapping Added

---
+++ BlogManagement.Infrastructure.EFCore/Mapping/ArticleMapping.cs
@@ -0,0 +1,29 @@
+﻿using BlogManagement.Domain.ArticleAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogManagement.Infrastructure.EFCore.Mapping
+{
+    public class ArticleMapping : IEntityTypeConfiguration<Article>
+    {
+        public void Configure(EntityTypeBuilder<Article> builder)
+        {
+            builder.ToTable("Articles");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.ShortDescription).HasMaxLength(1000).IsRequired();
+            builder.Property(x => x.Picture).HasMaxLength(500);
+            builder.Property(x => x.PictureAlt).HasMaxLength(500);
+            builder.Property(x => x.PictureTitle).HasMaxLength(500);
+            builder.Property(x => x.Slug).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.Keywords).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.MetaDescription).HasMaxLength(500).IsRequired();
+            builder.Property(x => x.CanonicalAddress).HasMaxLength(500).IsRequired(false);
+
+            builder.HasOne(x => x.Category)
+                .WithMany(x => x.Articles)
+                .HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
