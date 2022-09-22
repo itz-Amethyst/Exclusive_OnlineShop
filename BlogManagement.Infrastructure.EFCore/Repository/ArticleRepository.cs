a2765ebf5d91f6ae873fe44d85039251b6ec2cbc
callme-devil <pransermi@gmail.com>
2022-09-22 18:26:18 +0330

    feat: Base ArticleRepository Created

---
+++ BlogManagement.Infrastructure.EFCore/Repository/ArticleRepository.cs
@@ -0,0 +1,27 @@
+﻿using _0_Framework.Infrastructure;
+using BlogManagement.Application.Contracts.Article;
+using BlogManagement.Domain.ArticleAgg;
+using BlogManagement.Infrastructure.EFCore.Context;
+
+namespace BlogManagement.Infrastructure.EFCore.Repository
+{
+    public class ArticleRepository : RepositoryBase<int , Article>, IArticleRepository
+    {
+        private readonly BlogContext _context;
+
+        public ArticleRepository(BlogContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditArticle GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleViewModel> Search(ArticleSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
