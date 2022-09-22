77fc13c8363f00c8c43520433c80b60f7ed3b348
callme-devil <pransermi@gmail.com>
2022-09-22 18:22:07 +0330

    feat: IArticleApplication Interface implemented

---
+++ BlogManagement.Application.Contracts/Article/IArticleApplication.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Application;
+
+namespace BlogManagement.Application.Contracts.Article
+{
+    public interface IArticleApplication
+    {
+        OperationResult Create(CreateArticle command);
+
+        OperationResult Edit(EditArticle command);
+
+        EditArticle GetDetails(int id);
+
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
