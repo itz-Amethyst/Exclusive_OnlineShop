d1683396c33796b2878a40d60115c1598e797d4a
callme-devil <pransermi@gmail.com>
2022-09-22 18:23:50 +0330

    feat: IArticleRepository Created

---
+++ BlogManagement.Domain/ArticleAgg/IArticleRepository.cs
@@ -0,0 +1,12 @@
+﻿using _0_Framework.Domain;
+using BlogManagement.Application.Contracts.Article;
+
+namespace BlogManagement.Domain.ArticleAgg
+{
+    public interface IArticleRepository : IRepository<int , Article>
+    {
+        EditArticle GetDetails(int id);
+
+        List<ArticleViewModel> Search(ArticleSearchModel searchModel);
+    }
+}
