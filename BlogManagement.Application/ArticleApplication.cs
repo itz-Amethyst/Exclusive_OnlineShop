d65e801c6918d6c0ebdd5de415c14f6929fb3d45
callme-devil <pransermi@gmail.com>
2022-09-24 17:33:58 +0330

    feat: ArticleApplication Base File Created

--- BlogManagement.Application/ArticleApplication.cs
+++ BlogManagement.Application/ArticleApplication.cs
@@ -1,7 +1,38 @@
-﻿namespace BlogManagement.Application
+﻿using _0_Framework.Application;
+using BlogManagement.Application.Contracts.Article;
+using BlogManagement.Domain.ArticleAgg;
+
+namespace BlogManagement.Application
 {
-    public class ArticleApplication
+    public class ArticleApplication : IArticleApplication
     {
-        
+        private readonly IArticleRepository _articleRepository;
+        private readonly IFileUploader _fileUploader;
+
+        public ArticleApplication(IArticleRepository articleRepository, IFileUploader fileUploader)
+        {
+            _articleRepository = articleRepository;
+            _fileUploader = fileUploader;
+        }
+
+        public OperationResult Create(CreateArticle command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditArticle command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditArticle GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ArticleViewModel> Search(ArticleSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
