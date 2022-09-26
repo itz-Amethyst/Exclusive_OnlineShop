ef552a6eef82885e56a356a5d5554dbd8db8af6e
callme-devil <pransermi@gmail.com>
2022-09-26 21:46:53 +0330

    feat: IndexModelPage Blog Implemented

--- ServiceHost/Areas/Administration/Pages/Blog/Articles/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Blog/Articles/Index.cshtml.cs
@@ -1,22 +1,29 @@
 using BlogManagement.Application.Contracts.Article;
+using BlogManagement.Application.Contracts.ArticleCategory;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ServiceHost.Areas.Administration.Pages.Blog.Articles
 {
     public class IndexModel : PageModel
     {
         private readonly IArticleApplication _articleApplication;
-
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+        
         public ArticleSearchModel SearchModel;
         public List<ArticleViewModel> Articles;
+        public SelectList ArticleCategories;
 
-        public IndexModel(IArticleApplication articleApplication)
+        public IndexModel(IArticleApplication articleApplication, IArticleCategoryApplication articleCategoryApplication)
         {
             _articleApplication = articleApplication;
+            _articleCategoryApplication = articleCategoryApplication;
         }
 
         public void OnGet(ArticleSearchModel searchModel)
         {
+            ArticleCategories = new SelectList(_articleCategoryApplication.GetArticleCategories(), "Id", "Name");
+
             Articles = _articleApplication.Search(searchModel);
         }
     }
