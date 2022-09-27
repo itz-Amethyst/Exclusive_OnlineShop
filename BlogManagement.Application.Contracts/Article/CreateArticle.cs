b3aeb6f80d6d068dcf1f43a8ca64a423363b07ca
callme-devil <pransermi@gmail.com>
2022-09-27 18:32:38 +0330

    feat: Validations Added to CreateArticle Server-Side

--- BlogManagement.Application.Contracts/Article/CreateArticle.cs
+++ BlogManagement.Application.Contracts/Article/CreateArticle.cs
@@ -1,31 +1,45 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using _0_Framework.Application;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogManagement.Application.Contracts.Article
 {
     public class CreateArticle
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+
         public string Title { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string ShortDescription { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public IFormFile Picture { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureAlt { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PictureTitle { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Slug { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string MetaDescription { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Keywords { get; set; }
 
         public string CanonicalAddress { get; set; }
 
+        [Range(1,int.MaxValue , ErrorMessage = ValidationMessages.IsRequired)]
         public int CategoryId { get; set; }
-
+        
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string PublishDate { get; set; }
     }
 }
