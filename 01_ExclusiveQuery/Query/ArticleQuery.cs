27bcba3b92a866bd259f71fa4689fe78c113e9ac
callme-devil <pransermi@gmail.com>
2022-10-02 15:10:50 +0330

    feat: Base ArticleQuery Added

---
+++ 01_ExclusiveQuery/Query/ArticleQuery.cs
@@ -0,0 +1,20 @@
+﻿using _01_ExclusiveQuery.Contracts.Article;
+using BlogManagement.Infrastructure.EFCore.Context;
+
+namespace _01_ExclusiveQuery.Query
+{
+    public class ArticleQuery : IArticleQuery
+    {
+        private readonly BlogContext _context;
+
+        public ArticleQuery(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleQueryModel> GetLatestArticles()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
