db0da8de018fa336f6b8855bb696898784ce8e9a
callme-devil <pransermi@gmail.com>
2022-10-02 14:02:10 +0330

    feat: Edit Article IndexModelPage Implemented

---
+++ ServiceHost/Areas/Administration/Pages/Blog/Articles/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+using BlogManagement.Application.Contracts.Article;
+using BlogManagement.Application.Contracts.ArticleCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ServiceHost.Areas.Administration.Pages.Blog.Articles
+{
+    public class EditModel : PageModel
+    {
+        private readonly IArticleCategoryApplication _articleCategoryApplication;
+        private readonly IArticleApplication _articleApplication;
+
+        public SelectList ArticleCategories;
+        public EditArticle Command;
+
+        public EditModel(IArticleCategoryApplication articleCategoryApplication, IArticleApplication articleApplication)
+        {
+            _articleCategoryApplication = articleCategoryApplication;
+            _articleApplication = articleApplication;
+          
+        }
+
+        public void OnGet(int id)
+        {
+            Command = _articleApplication.GetDetails(id);
+
+            ArticleCategories = new SelectList(_articleCategoryApplication.GetArticleCategories(), "Id", "Name");
+        }
+
+        public IActionResult OnPost(EditArticle command)
+        {
+            _articleApplication.Edit(command);
+            return RedirectToPage("./Index", new { Edited = "True" });
+        }
+    }
+}
