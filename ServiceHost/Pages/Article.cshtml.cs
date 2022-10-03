5f5f5aa56e4ae0eb60440dbb4a7938f3faf48fc1
callme-devil <pransermi@gmail.com>
2022-10-03 14:21:37 +0330

    feat: Empty Article Page Created

---
+++ ServiceHost/Pages/Article.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Pages
+{
+    public class ArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
