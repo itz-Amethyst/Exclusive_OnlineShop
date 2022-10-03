5834568cede10df8fdbd1d8c053d38446d91a42a
callme-devil <pransermi@gmail.com>
2022-10-03 15:18:36 +0330

    feat: ArticleCategoryQueries Implemented

---
+++ 01_ExclusiveQuery/Contracts/ArticleCategory/IArticleCategoryQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace _01_ExclusiveQuery.Contracts.ArticleCategory
+{
+    public interface IArticleCategoryQuery
+    {
+        List<ArticleCategoryQueryModel> GetArticleCategories();
+    }
+}
---
+++ 01_ExclusiveQuery/Query/ArticleCategoryQuery.cs
@@ -0,0 +1,20 @@
+﻿using _01_ExclusiveQuery.Contracts.ArticleCategory;
+using BlogManagement.Infrastructure.EFCore.Context;
+
+namespace _01_ExclusiveQuery.Query
+{
+    public class ArticleCategoryQuery : IArticleCategoryQuery
+    {
+        private readonly BlogContext _context;
+
+        public ArticleCategoryQuery(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public List<ArticleCategoryQueryModel> GetArticleCategories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
