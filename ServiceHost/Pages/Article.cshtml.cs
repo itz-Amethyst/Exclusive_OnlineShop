6369b09975c24ad2df3282884e9cab2000eb30aa
callme-devil <pransermi@gmail.com>
2022-10-03 15:35:56 +0330

    feat: ArticleCategories Feature Added in ArticleModel Page

--- ServiceHost/Pages/Article.cshtml.cs
+++ ServiceHost/Pages/Article.cshtml.cs
@@ -1,4 +1,5 @@
 using _01_ExclusiveQuery.Contracts.Article;
+using _01_ExclusiveQuery.Contracts.ArticleCategory;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
@@ -6,19 +7,23 @@ namespace ServiceHost.Pages
     public class ArticleModel : PageModel
     {
         private readonly IArticleQuery _articleQuery;
+        private readonly IArticleCategoryQuery _articleCategoryQuery;
 
         public ArticleQueryModel Article;
         public List<ArticleQueryModel> LatestArticles;
+        public List<ArticleCategoryQueryModel> ArticleCategories;
 
-        public ArticleModel(IArticleQuery articleQuery)
+        public ArticleModel(IArticleQuery articleQuery, IArticleCategoryQuery articleCategoryQuery)
         {
             _articleQuery = articleQuery;
+            _articleCategoryQuery = articleCategoryQuery;
         }
 
-        public void OnGet(string slug)
+        public void OnGet(string id)
         {
-            Article = _articleQuery.GetArticleDetails(slug);
+            Article = _articleQuery.GetArticleDetails(id);
             LatestArticles = _articleQuery.GetLatestArticles();
+            ArticleCategories = _articleCategoryQuery.GetArticleCategories();
         }
     }
 }
