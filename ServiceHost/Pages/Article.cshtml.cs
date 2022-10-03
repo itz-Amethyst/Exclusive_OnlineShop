cf2b21cf33397d23f6100fcf1d919819e58301dc
callme-devil <pransermi@gmail.com>
2022-10-03 14:29:38 +0330

    feat: Moved Base Article Markup To Page

--- ServiceHost/Pages/Article.cshtml.cs
+++ ServiceHost/Pages/Article.cshtml.cs
@@ -1,11 +1,22 @@
+using _01_ExclusiveQuery.Contracts.Article;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Pages
 {
     public class ArticleModel : PageModel
     {
-        public void OnGet()
+        private readonly IArticleQuery _articleQuery;
+
+        public ArticleQueryModel Article;
+
+        public ArticleModel(IArticleQuery articleQuery)
         {
+            _articleQuery = articleQuery;
+        }
+
+        public void OnGet(string slug)
+        {
+            Article = _articleQuery.GetArticleDetails(slug);
         }
     }
 }
