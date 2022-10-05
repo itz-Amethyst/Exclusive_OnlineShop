0e6ef854df561fcdb02ce2117b8b5c953b777703
callme-devil <pransermi@gmail.com>
2022-10-05 12:29:39 +0330

    feat: MenuModel Created

---
+++ 01_ExclusiveQuery/MenuModel/MenuModel.cs
@@ -0,0 +1,12 @@
+﻿using _01_ExclusiveQuery.Contracts.ArticleCategory;
+using _01_ExclusiveQuery.Contracts.ProductCategory;
+
+namespace _01_ExclusiveQuery.MenuModel
+{
+    public class MenuModel
+    {
+        public List<ArticleCategoryQueryModel> ArticleCategories { get; set; }
+
+        public List<ProductCategoryQueryModel> ProductCategories { get; set; }
+    }
+}
