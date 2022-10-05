1d9691e15366114a97c0ce387bc52e25980e4d01
callme-devil <pransermi@gmail.com>
2022-10-05 10:45:04 +0330

    feat: ArticleCategory Articles Count Configured

--- BlogManagement.Infrastructure.EFCore/Repository/ArticleCategoryRepository.cs
+++ BlogManagement.Infrastructure.EFCore/Repository/ArticleCategoryRepository.cs
@@ -3,6 +3,7 @@ using _0_Framework.Infrastructure;
 using BlogManagement.Application.Contracts.ArticleCategory;
 using BlogManagement.Domain.ArticleCategoryAgg;
 using BlogManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogManagement.Infrastructure.EFCore.Repository
 {
@@ -35,14 +36,17 @@ namespace BlogManagement.Infrastructure.EFCore.Repository
 
         public List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel)
         {
-            var query = _context.ArticleCategories.Select(x => new ArticleCategoryViewModel
+            var query = _context.ArticleCategories
+                .Include(x=>x.Articles)
+                .Select(x => new ArticleCategoryViewModel
             {
                 Id = x.Id,
                 Name = x.Name,
                 Description = x.Name,
                 Picture = x.Picture,
                 ShowOrder = x.ShowOrder,
-                CreationDate = x.CreationDate.ToFarsi()
+                CreationDate = x.CreationDate.ToFarsi(),
+                ArticlesCount = x.Articles.Count
             });
 
             if (!string.IsNullOrWhiteSpace(searchModel.Name))
