bbfad52ff768c6920da7a42c471ed92f07d66695
callme-devil <pransermi@gmail.com>
2022-10-05 12:30:15 +0330

    feat: HeaderViewComponent Dynamiced

--- ServiceHost/ViewComponents/HeaderViewComponent.cs
+++ ServiceHost/ViewComponents/HeaderViewComponent.cs
@@ -1,4 +1,6 @@
-﻿using _01_ExclusiveQuery.Contracts.ProductCategory;
+﻿using _01_ExclusiveQuery.Contracts.ArticleCategory;
+using _01_ExclusiveQuery.Contracts.ProductCategory;
+using _01_ExclusiveQuery.MenuModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ServiceHost.ViewComponents
@@ -6,15 +8,23 @@ namespace ServiceHost.ViewComponents
     public class HeaderViewComponent : ViewComponent
     {
         private readonly IProductCategoryQuery _productCategoryQuery;
+        private readonly IArticleCategoryQuery _articleCategoryQuery;
 
-        public HeaderViewComponent(IProductCategoryQuery productCategoryQuery)
+        public HeaderViewComponent(IProductCategoryQuery productCategoryQuery, IArticleCategoryQuery articleCategoryQuery)
         {
             _productCategoryQuery = productCategoryQuery;
+            _articleCategoryQuery = articleCategoryQuery;
         }
 
         public IViewComponentResult Invoke()
         {
-            return View();
+            var result = new MenuModel
+            {
+                ArticleCategories = _articleCategoryQuery.GetArticleCategories(),
+                ProductCategories = _productCategoryQuery.GetProductCategories()
+            };
+
+            return View(result);
         }
     }
 }
