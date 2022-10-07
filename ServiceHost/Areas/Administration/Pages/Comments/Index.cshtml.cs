530f933a49e581e0ac487b33d1922d50ed0ae6df
callme-devil <pransermi@gmail.com>
2022-10-07 16:10:35 +0330

    refactor: Moved Comment Outside Product

--- ServiceHost/Areas/Administration/Pages/Comments/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Comments/Index.cshtml.cs
@@ -1,8 +1,9 @@
+using CommentManagement.Application.Contract.Comment;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ShopManagement.Application.Contracts.Comment;
 
-namespace ServiceHost.Areas.Administration.Pages.Shop.Comments
+namespace ServiceHost.Areas.Administration.Pages.Comments
 {
     public class IndexModel : PageModel
     {
