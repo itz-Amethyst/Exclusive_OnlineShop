1af6027a85fd54f17cc97a98f3e2ed318cf04401
callme-devil <pransermi@gmail.com>
2022-10-07 16:15:32 +0330

    feat: CommentManagementBootstrapper added

---
+++ CommentManagement.Configuration/CommentManagementBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using CommentManagement.Application;
+using CommentManagement.Domain.CommentAgg;
+using CommentManagement.Infrastructure.EFCore.Context;
+using CommentManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application.Contracts.Comment;
+
+namespace CommentManagement.Infrastructure.Configuration
+{
+    public class CommentManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            #region Comment
+            
+            services.AddTransient<ICommentRepository, CommentRepository>();
+            services.AddTransient<ICommentApplication, CommentApplication>();
+
+            #endregion
+
+            services.AddDbContext<CommentContext>(x => x.UseSqlServer(connectionString));
+        }
+        
+    }
+}
