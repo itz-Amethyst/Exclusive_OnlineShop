73a961ad0a6f019f0fec128a1a95f5d416ab1007
callme-devil <pransermi@gmail.com>
2022-10-09 16:01:16 +0330

    feat: ProductId Comment Remove Migration Added

--- CommentManagement.Infrastructure.EFCore/Migrations/20221009114311_ProductIdRemovedFromComment.Designer.cs
+++ CommentManagement.Infrastructure.EFCore/Migrations/20221009114311_ProductIdRemovedFromComment.Designer.cs
@@ -4,6 +4,7 @@ using CommentManagement.Infrastructure.EFCore.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CommentManagement.Infrastructure.EFCore.Migrations
 {
     [DbContext(typeof(CommentContext))]
-    partial class CommentContextModelSnapshot : ModelSnapshot
+    [Migration("20221009114311_ProductIdRemovedFromComment")]
+    partial class ProductIdRemovedFromComment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CommentManagement.Infrastructure.EFCore/Migrations/20221009114311_ProductIdRemovedFromComment.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommentManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class ProductIdRemovedFromComment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Comments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
