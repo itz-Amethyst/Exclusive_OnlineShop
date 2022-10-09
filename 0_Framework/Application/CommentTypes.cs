a9c19d2b27eea2e10a761d886dae3ee3875e7f87
callme-devil <pransermi@gmail.com>
2022-10-09 12:35:31 +0330

    feat: CommentTypes Added For NewComment System

---
+++ 0_Framework/Application/CommentTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace _0_Framework.Application
+{
+    public static class CommentTypes
+    {
+        public const int Product = 1;
+        
+        public const int Article = 1;
+        
+    }
+}
