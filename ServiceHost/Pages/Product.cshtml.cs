fa4c8ec2679235173a52e9531ba77f0e496628e9
callme-devil <pransermi@gmail.com>
2022-10-10 22:00:19 +0330

    feat: Google Recaptcha Server Side Validation Added in Product

--- ServiceHost/Pages/Product.cshtml.cs
+++ ServiceHost/Pages/Product.cshtml.cs
@@ -3,7 +3,9 @@ using _01_ExclusiveQuery.Contracts.Product;
 using CommentManagement.Application.Contract.Comment;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
 using ShopManagement.Application.Contracts.Comment;
+using System.Net;
 
 namespace ServiceHost.Pages
 {
@@ -19,17 +21,34 @@ namespace ServiceHost.Pages
             _commentApplication = commentApplication;
         }
 
-        public void OnGet(string id)
+        public void OnGet(string id , bool created = false)
         {
+            ViewData["Created"] = created;
+
             Product = _productQuery.GetProductDetails(id);
         }
 
         public IActionResult OnPost(AddComment command , string productSlug)
         {
-            command.Type = CommentTypes.Product;
+            var response = Request.Form["g-recaptcha-response"];
+            string secretKey = "6Lf-bm0iAAAAALfFjSkAOJkGmsT8LFS1jYmy6rw4";
+            var client = new WebClient();
+            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            var data =TempData["Created"] = false;
 
-            var result = _commentApplication.Add(command);
-            return RedirectToPage("/Product" , new {Id = productSlug});
+            if (status)
+            {
+                command.Type = CommentTypes.Product;
+                _commentApplication.Add(command);
+                return RedirectToPage("/Product", new { Id = productSlug , Created = true });
+            }
+
+            TempData["Message"] = "Google reCaptcha validation failed";
+
+            return RedirectToPage("/Product");
+
         }
     }
 }
