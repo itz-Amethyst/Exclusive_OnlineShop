115751a8fab1ab621b3cf811ee4489844d80cad1
callme-devil <pransermi@gmail.com>
2022-10-12 13:15:55 +0330

    feat:  Show Comment Owner Name Feature Added (Part of Finally did it 🙃 )

--- CommentManagement.Infrastructure.EFCore/Repository/CommentRepository.cs
+++ CommentManagement.Infrastructure.EFCore/Repository/CommentRepository.cs
@@ -1,22 +1,33 @@
 ﻿using _0_Framework.Application;
 using _0_Framework.Infrastructure;
+using BlogManagement.Infrastructure.EFCore.Context;
 using CommentManagement.Application.Contract.Comment;
 using CommentManagement.Domain.CommentAgg;
 using CommentManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Infrastructure.EFCore.Context;
 
 namespace CommentManagement.Infrastructure.EFCore.Repository
 {
     public class CommentRepository : RepositoryBase<int, Comment>, ICommentRepository
     {
         private readonly CommentContext _context;
+        private readonly ShopContext _shopContext;
+        private readonly BlogContext _blogContext;
 
-        public CommentRepository(CommentContext context) : base(context)
+        public CommentRepository(CommentContext context, ShopContext shopContext, BlogContext blogContext) : base(context)
         {
             _context = context;
+            _shopContext = shopContext;
+            _blogContext = blogContext;
         }
 
         public List<CommentViewModel> Search(CommentSearchModel searchModel)
         {
+            var products = _shopContext.Products.Select(x => new { x.Id, x.Name }).ToList();
+
+            var articles = _blogContext.Articles.Select(x => new { x.Id, x.Title }).ToList();
+
             var query = _context.Comments
                 .Select(x => new CommentViewModel
                 {
@@ -31,6 +42,14 @@ namespace CommentManagement.Infrastructure.EFCore.Repository
                     CommentCreatedDate = x.CreationDate.ToFarsi()
                 });
 
+            var comments = query.OrderByDescending(x => x.Id).ToList();
+
+            comments.ForEach(item =>
+            {
+                item.OwnerNameProduct = products.FirstOrDefault(x => x.Id == item.OwnerRecordId)?.Name;
+                item.OwnerNameArticle = articles.FirstOrDefault(x => x.Id == item.OwnerRecordId)?.Title;
+            });
+
             if (!string.IsNullOrWhiteSpace(searchModel.Name))
             {
                 query = query.Where(x => x.Name.Contains(searchModel.Name));
@@ -41,7 +60,7 @@ namespace CommentManagement.Infrastructure.EFCore.Repository
                 query = query.Where(x => x.Email.Contains(searchModel.Email));
             }
 
-            return query.OrderByDescending(x => x.Id).ToList();
+            return comments;
         }
     }
 }
