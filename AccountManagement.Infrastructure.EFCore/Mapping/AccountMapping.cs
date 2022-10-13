1b35b6c6b0cea2ba2df259908f5dc02771345fee
callme-devil <pransermi@gmail.com>
2022-10-13 14:50:35 +0330

    feat: AccountMapping Implemented

---
+++ AccountManagement.Infrastructure.EFCore/Mapping/AccountMapping.cs
@@ -0,0 +1,22 @@
+﻿using AccountManagement.Domain.AccountAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountManagement.Infrastructure.EFCore.Mapping
+{
+    public class AccountMapping :IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("Accounts");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Fullname).HasMaxLength(100);
+            builder.Property(x => x.Username).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Password).HasMaxLength(1000).IsRequired();
+            builder.Property(x => x.Mobile).HasMaxLength(20).IsRequired();
+            builder.Property(x => x.ProfilePhoto).HasMaxLength(500);
+
+        }
+    }
+}
