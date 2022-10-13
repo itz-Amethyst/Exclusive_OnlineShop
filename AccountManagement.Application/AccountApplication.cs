f7a161e1c5fa5987beef78b268712616826a927a
callme-devil <pransermi@gmail.com>
2022-10-13 15:20:39 +0330

    feat: AccountApplication Created

---
+++ AccountManagement.Application/AccountApplication.cs
@@ -0,0 +1,41 @@
+﻿using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Domain.AccountAgg;
+
+namespace AccountManagement.Application
+{
+    public class AccountApplication :IAccountApplication
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountApplication(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public OperationResult Create(CreateAccount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditAccount command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult ChangePassword(ChangePassword command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditAccount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountViewModel> Search(AccountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
