2823ae27a98ff2478c93b4773f879fbe2f21be22
callme-devil <pransermi@gmail.com>
2022-10-13 14:52:56 +0330

    feat: AccountRepository Created

---
+++ AccountManagement.Infrastructure.EFCore/Repository/AccountRepository.cs
@@ -0,0 +1,28 @@
+﻿using _0_Framework.Infrastructure;
+using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Domain.AccountAgg;
+using AccountManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountManagement.Infrastructure.EFCore.Repository
+{
+    public class AccountRepository : RepositoryBase<int , Account> , IAccountRepository
+    {
+        private readonly AccountContext _context;
+
+        public AccountRepository(AccountContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public EditAccount GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountViewModel> Search(AccountSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
