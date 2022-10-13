b94d0b98ce3a24b88480237e3feb1234766a9ee4
callme-devil <pransermi@gmail.com>
2022-10-13 14:42:38 +0330

    feat: IAccountRepository Filled

--- AccountManagement.Domain/AccountAgg/IAccountRepository.cs
+++ AccountManagement.Domain/AccountAgg/IAccountRepository.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Domain;
+using AccountManagement.Application.Contracts.Account;
 
 namespace AccountManagement.Domain.AccountAgg
 {
     public interface IAccountRepository : IRepository<int , Account>
     {
-        
+        EditAccount GetDetails(int id);
+
+        List<AccountViewModel> Search(AccountSearchModel searchModel);
     }
 }
