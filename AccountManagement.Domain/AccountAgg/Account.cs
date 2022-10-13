1510deff1254a4baa5c78e278fc5141ebb7feebe
callme-devil <pransermi@gmail.com>
2022-10-13 14:18:51 +0330

    feat: DomainManagement Domain Class Added

---
+++ AccountManagement.Domain/AccountAgg/Account.cs
@@ -0,0 +1,59 @@
+﻿using _0_Framework.Domain;
+
+namespace AccountManagement.Domain.AccountAgg
+{
+    public class Account : EntityBase
+    {
+        public string Username { get; private set; }
+        
+        public string Password { get; private set; }
+        
+        public string Fullname { get; private set; }
+        
+        public string Mobile { get; private set; }
+        
+        public int RoleId { get; private set; }
+        
+        public bool IsRemoved { get; private set; }
+
+        public string ProfilePhoto { get; private set; }
+
+        public Account(string username, string password, string fullname, string mobile, int roleId, string profilePhoto)
+        {
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Mobile = mobile;
+            RoleId = roleId;
+            IsRemoved = false;
+            ProfilePhoto = profilePhoto;
+        }
+
+        public void Edit(string username, string fullname, string mobile, int roleId, string profilePhoto)
+        {
+            Username = username;
+            Fullname = fullname;
+            Mobile = mobile;
+            RoleId = roleId;
+            if (!string.IsNullOrWhiteSpace(profilePhoto))
+            {
+                ProfilePhoto = profilePhoto;
+            }
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+        }
+
+        public void Remove()
+        {
+            IsRemoved = true;
+        }
+
+        public void Restore()
+        {
+            IsRemoved = false;
+        }
+    }
+}
