b317e033c3b937aac99540f886cadcaadcdd7a26
callme-devil <pransermi@gmail.com>
2022-10-13 14:22:55 +0330

    feat: AccountManagement Application Contract Create Account Implemented

---
+++ AccountManagement.Application.Contracts/Account/CreateAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class CreateAccount
+    {
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+        
+        public string Fullname { get; set; }
+        
+        public string Mobile { get; set; }
+        
+        public int RoleId { get; set; }
+        
+        public string ProfilePhoto { get; set; }
+    }
+}
