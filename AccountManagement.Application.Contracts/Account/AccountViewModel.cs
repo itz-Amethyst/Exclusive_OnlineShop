82baac0d1733284b9a2fbea99f25f29e074560f7
callme-devil <pransermi@gmail.com>
2022-10-13 14:36:28 +0330

    feat: AccountViewModel Implemented

---
+++ AccountManagement.Application.Contracts/Account/AccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class AccountViewModel
+    {
+        public string Username { get; set; }
+
+        public string Fullname { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Role  { get; set; }
+
+        public string ProfilePhoto { get; set; }
+    }
+}
