3adfa00d473435ce76eb20ee13ee0037769d0f4b
callme-devil <pransermi@gmail.com>
2022-10-13 14:40:45 +0330

    feat: ChangePassword Added

---
+++ AccountManagement.Application.Contracts/Account/ChangePassword.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class ChangePassword
+    {
+        public int Id { get; set; }
+
+        public string Password { get; set; }
+
+        public string RePassword { get; set; }
+    }
+}
