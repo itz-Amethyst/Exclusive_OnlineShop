5b084158a7bf0cc33a53408ee3614bac01a69d90
callme-devil <pransermi@gmail.com>
2022-10-13 14:54:55 +0330

    feat: AccountContext Filled 2/2

--- AccountManagement.Infrastructure.EFCore/Context/AccountContext.cs
+++ AccountManagement.Infrastructure.EFCore/Context/AccountContext.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AccountManagement.Domain.AccountAgg;
+using AccountManagement.Infrastructure.EFCore.Mapping;
+using Microsoft.EntityFrameworkCore;
 
 namespace AccountManagement.Infrastructure.EFCore.Context
 {
     public class AccountContext : DbContext
     {
-        
+        public DbSet<Account> Accounts { get; set; }
+
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var assembly = typeof(AccountMapping).Assembly;
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
