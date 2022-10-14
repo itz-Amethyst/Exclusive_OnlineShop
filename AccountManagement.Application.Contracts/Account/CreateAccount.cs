bda423164b15eb9f365b2fa0cd53f2a759ba5074
callme-devil <pransermi@gmail.com>
2022-10-14 14:51:16 +0330

    feat: RoleViewModel Referenced in CreateAccount

--- AccountManagement.Application.Contracts/Account/CreateAccount.cs
+++ AccountManagement.Application.Contracts/Account/CreateAccount.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AccountManagement.Application.Contracts.Role;
+using Microsoft.AspNetCore.Http;
 
 namespace AccountManagement.Application.Contracts.Account
 {
@@ -15,5 +16,7 @@ namespace AccountManagement.Application.Contracts.Account
         public int RoleId { get; set; }
         
         public IFormFile ProfilePhoto { get; set; }
+
+        public List<RoleViewModel> Roles { get; set; }
     }
 }
