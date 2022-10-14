82394d89da607a052d7bf5a53f2302170870305d
callme-devil <pransermi@gmail.com>
2022-10-14 14:50:55 +0330

    feat: RoleViewModel Created

---
+++ AccountManagement.Application.Contracts/Role/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountManagement.Application.Contracts.Role
+{
+    public class RoleViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CreationDate { get; set; }
+    }
+}
