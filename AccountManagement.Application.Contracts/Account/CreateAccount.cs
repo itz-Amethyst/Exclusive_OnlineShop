9ccb62cce0c4ef4b5afd9f833d783526ba96483b
callme-devil <pransermi@gmail.com>
2022-10-14 15:20:49 +0330

    feat: Validations Added for CreateAccount

--- AccountManagement.Application.Contracts/Account/CreateAccount.cs
+++ AccountManagement.Application.Contracts/Account/CreateAccount.cs
@@ -1,18 +1,24 @@
-﻿using AccountManagement.Application.Contracts.Role;
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Role;
 using Microsoft.AspNetCore.Http;
 
 namespace AccountManagement.Application.Contracts.Account
 {
     public class CreateAccount
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Username { get; set; }
-        
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Password { get; set; }
         
         public string Fullname { get; set; }
         
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Mobile { get; set; }
         
+        [Range(1, int.MaxValue , ErrorMessage = ValidationMessages.IsRequired)]
         public int RoleId { get; set; }
         
         public IFormFile ProfilePhoto { get; set; }
