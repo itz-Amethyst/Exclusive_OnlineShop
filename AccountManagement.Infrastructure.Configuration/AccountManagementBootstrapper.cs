dbd068feb97846aea9c1488165697816b85c8b6d
callme-devil <pransermi@gmail.com>
2022-10-14 13:08:04 +0330

    feat: AccountManagement Bootstrapper Added

---
+++ AccountManagement.Infrastructure.Configuration/AccountManagementBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using AccountManagement.Application;
+using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Domain.AccountAgg;
+using AccountManagement.Infrastructure.EFCore.Context;
+using AccountManagement.Infrastructure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccountManagement.Infrastructure.Configuration
+{
+    public class AccountManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            #region Account
+
+            services.AddTransient<IAccountApplication, AccountApplication>();
+
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            
+            #endregion
+
+            services.AddDbContext<AccountContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
