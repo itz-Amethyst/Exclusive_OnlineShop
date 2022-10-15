2bf68e87246b7489837236beb40bdb89381cf75b
callme-devil <pransermi@gmail.com>
2022-10-15 15:55:41 +0330

    feat: Validation Messages Added for CreateRole

--- AccountManagement.Application.Contracts/Role/CreateRole.cs
+++ AccountManagement.Application.Contracts/Role/CreateRole.cs
@@ -1,7 +1,11 @@
-﻿namespace AccountManagement.Application.Contracts.Role
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace AccountManagement.Application.Contracts.Role
 {
     public class CreateRole
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Name { get; set; }
     }
 }
