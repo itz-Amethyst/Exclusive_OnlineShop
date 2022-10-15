547e2913eb5b8542b24bf58775de5bc11405ffdd
callme-devil <pransermi@gmail.com>
2022-10-15 14:31:36 +0330

    feat: Base RoleRepository Created

---
+++ AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
@@ -0,0 +1,27 @@
+﻿using _0_Framework.Infrastructure;
+using AccountManagement.Application.Contracts.Role;
+using AccountManagement.Domain.RoleAgg;
+using AccountManagement.Infrastructure.EFCore.Context;
+
+namespace AccountManagement.Infrastructure.EFCore.Repository
+{
+    public class RoleRepository : RepositoryBase<int , Role>, IRoleRepository
+    {
+        private readonly AccountContext _accountContext;
+
+        public RoleRepository(AccountContext accountContext) : base(accountContext)
+        {
+            _accountContext = accountContext;
+        }
+
+        public EditRole GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RoleViewModel> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
