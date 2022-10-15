7addb88e2e825e31fc587ff898423e5e36621b92
callme-devil <pransermi@gmail.com>
2022-10-15 14:43:52 +0330

    feat: Base RoleApplication Created

---
+++ AccountManagement.Application/RoleApplication.cs
@@ -0,0 +1,36 @@
+﻿using _0_Framework.Application;
+using AccountManagement.Application.Contracts.Role;
+using AccountManagement.Domain.RoleAgg;
+
+namespace AccountManagement.Application
+{
+    public class RoleApplication : IRoleApplication
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleApplication(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public OperationResult Create(CreateRole command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult Edit(EditRole command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EditRole GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RoleViewModel> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
