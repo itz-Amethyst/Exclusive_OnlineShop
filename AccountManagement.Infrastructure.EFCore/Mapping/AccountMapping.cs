1d21aa8c27d54682381d33b0780c81a6a93e75c3
callme-devil <pransermi@gmail.com>
2022-10-15 14:28:25 +0330

    feat: AccountMapping Updated

--- AccountManagement.Infrastructure.EFCore/Mapping/AccountMapping.cs
+++ AccountManagement.Infrastructure.EFCore/Mapping/AccountMapping.cs
@@ -11,12 +11,15 @@ namespace AccountManagement.Infrastructure.EFCore.Mapping
             builder.ToTable("Accounts");
             builder.HasKey(x => x.Id);
 
-            builder.Property(x => x.Fullname).HasMaxLength(100);
+            builder.Property(x => x.Fullname).HasMaxLength(100).IsRequired();
             builder.Property(x => x.Username).HasMaxLength(100).IsRequired();
             builder.Property(x => x.Password).HasMaxLength(1000).IsRequired();
             builder.Property(x => x.Mobile).HasMaxLength(20).IsRequired();
-            builder.Property(x => x.ProfilePhoto).HasMaxLength(500);
+            builder.Property(x => x.ProfilePhoto).HasMaxLength(500).IsRequired();
 
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.Accounts)
+                .HasForeignKey(x => x.RoleId);
         }
     }
 }
