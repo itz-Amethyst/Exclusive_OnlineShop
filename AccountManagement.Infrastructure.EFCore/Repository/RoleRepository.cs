391d816d3b9c73c1ce24b8d0edd1ac1704f6caaf
callme-devil <pransermi@gmail.com>
2022-10-15 15:56:24 +0330

    feat: CreationDate Added to RoleRepository

--- AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
+++ AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
@@ -1,4 +1,5 @@
-﻿using _0_Framework.Infrastructure;
+﻿using _0_Framework.Application;
+using _0_Framework.Infrastructure;
 using AccountManagement.Application.Contracts.Role;
 using AccountManagement.Domain.RoleAgg;
 using AccountManagement.Infrastructure.EFCore.Context;
@@ -28,7 +29,8 @@ namespace AccountManagement.Infrastructure.EFCore.Repository
             return _accountContext.Roles.Select(x => new RoleViewModel()
             {
                 Id = x.Id,
-                Name = x.Name
+                Name = x.Name,
+                CreationDate = x.CreationDate.ToFarsi()
             }).ToList();
         }
     }
