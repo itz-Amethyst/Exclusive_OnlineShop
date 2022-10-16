538122731bcf0cf8a69501bf213db5f67978c7cb
callme-devil <pransermi@gmail.com>
2022-10-16 15:36:25 +0330

    feat: AuthViewModel Added

---
+++ 0_Framework/Application/AuthViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace _0_Framework.Application;
+
+public class AuthViewModel
+{
+    public int Id { get; set; }
+
+    public int RoleId { get; set; }
+        
+    public string FullName { get; set; }
+        
+    public string Username { get; set; }
+        
+    //public string Email { get; set; }
+        
+    //public string Picture { get; set; }
+        
+    //public List<long> Roles { get; set; }
+}
