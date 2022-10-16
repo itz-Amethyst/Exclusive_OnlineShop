a5d5abd0ccf7852aa6e4f64ed45397bf4a956089
callme-devil <pransermi@gmail.com>
2022-10-16 18:26:36 +0330

    feat: Added Method Login to Login Page

--- ServiceHost/Pages/Login.cshtml.cs
+++ ServiceHost/Pages/Login.cshtml.cs
@@ -1,3 +1,4 @@
+using AccountManagement.Application.Contracts.Account;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,33 @@ namespace ServiceHost.Pages
 {
     public class AccountModel : PageModel
     {
+        [TempData]
+        public string Message { get; set; }
+
+        private readonly IAccountApplication _accountApplication;
+
+        public AccountModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost(Login command)
+        {
+            var result = _accountApplication.Login(command);
+
+            if (result.IsSuccessful)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            Message = result.Message;
+            
+            return RedirectToPage("/Login");
+
         }
     }
 }
