9b4b29a15e8d44840a22afcd58a7ea9f1c028167
callme-devil <pransermi@gmail.com>
2022-10-17 20:36:08 +0330

    feat: Validation Message Added for Login

--- AccountManagement.Application.Contracts/Account/Login.cs
+++ AccountManagement.Application.Contracts/Account/Login.cs
@@ -1,9 +1,14 @@
-﻿namespace AccountManagement.Application.Contracts.Account
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace AccountManagement.Application.Contracts.Account
 {
     public class Login
     {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string UserName { get; set; }
 
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Password { get; set; }
     }
 }
