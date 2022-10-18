64c8a023a245867a722962cd090d3c93d5861153
callme-devil <pransermi@gmail.com>
2022-10-18 18:12:55 +0330

    feat: RegisterAccountByUser Class Added

---
+++ AccountManagement.Application.Contracts/Account/RegisterAccountByUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application;
+
+namespace AccountManagement.Application.Contracts.Account
+{
+    public class RegisterAccountByUser : RegisterAccount
+    {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string RePassword { get; set; }
+
+    }
+}
