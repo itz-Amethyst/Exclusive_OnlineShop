a0f99deaf05270b9f1cb39c5b41d448dcd15bec7
callme-devil <pransermi@gmail.com>
2022-10-18 21:26:37 +0330

    feat: Created Roles Class for Better Quality

---
+++ 0_Framework/Infrastructure/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace _0_Framework.Infrastructure
+{
+    public static class Roles
+    {
+        public const string Administrator = "2";
+        public const string User = "3";
+        public const string Manager = "4";
+
+    }
+}
