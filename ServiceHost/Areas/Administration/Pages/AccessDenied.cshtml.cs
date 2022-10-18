bceb54d0cb9a85f2af16d370c9ea00b9a84a86f4
callme-devil <pransermi@gmail.com>
2022-10-18 21:26:53 +0330

    feat: AccessDenied Page Added

---
+++ ServiceHost/Areas/Administration/Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
