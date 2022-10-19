2728ee6a82555b6c6c7b1ff686d2dfabb8b6d259
callme-devil <pransermi@gmail.com>
2022-10-19 18:48:30 +0330

    feat: Account Policy Added to Program

--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -60,6 +60,7 @@ builder.Services.AddAuthorization(options =>
 
     options.AddPolicy("Discounts", policy => policy.RequireRole(new List<string> { Roles.Administrator }));
     
+    options.AddPolicy("Account", policy => policy.RequireRole(new List<string> { Roles.Administrator }));
 
 });
 
@@ -71,6 +72,8 @@ builder.Services.AddRazorPages()
         options.Conventions.AuthorizeAreaFolder("Administration", "/Shop", "Shop");
         
         options.Conventions.AuthorizeAreaFolder("Administration", "/Discounts", "Discounts");
+
+        options.Conventions.AuthorizeAreaFolder("Administration", "/Accounts", "Account");
 
     });
 
