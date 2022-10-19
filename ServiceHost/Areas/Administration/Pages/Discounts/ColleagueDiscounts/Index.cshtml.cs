af3369539bd423a35301099f4335a9987f1c6f7b
callme-devil <pransermi@gmail.com>
2022-10-19 17:48:34 +0330

    feat: ColleagueDiscount Permission Added

--- ServiceHost/Areas/Administration/Pages/Discounts/ColleagueDiscounts/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Discounts/ColleagueDiscounts/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using _0_Framework.Infrastructure;
 using DiscountManagement.Application.Contract.ColleagueDiscount;
 using DiscountManagement.Application.Contract.CustomerDiscount;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -7,6 +9,7 @@ using ShopManagement.Application.Contracts.Product;
 
 namespace ServiceHost.Areas.Administration.Pages.Discounts.ColleagueDiscounts
 {
+    [Authorize(Roles = Roles.Administrator)]
     public class IndexModel : PageModel
     {
         private readonly IProductApplication _productApplication;
