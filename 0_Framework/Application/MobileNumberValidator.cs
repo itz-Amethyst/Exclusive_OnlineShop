21351a057578ec58099817ec0698104dfd6c43e6
callme-devil <pransermi@gmail.com>
2022-10-20 19:14:15 +0330

    feat: Mobile Number Validator Method Added

---
+++ 0_Framework/Application/MobileNumberValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _0_Framework.Application
+{
+    public class MobileNumberValidator
+    {
+        public static bool IsMobileNumberValid(string input)
+        {
+            const string pattern = @"^09[0|1|2|3][0-9]{8}$";
+            Regex reg = new Regex(pattern);
+            return reg.IsMatch(input);
+        }
+        
+    }
+}
