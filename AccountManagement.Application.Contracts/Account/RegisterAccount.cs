4d2f61cd46e1303ba9204bd6699caf41d79dbf35
callme-devil <pransermi@gmail.com>
2022-10-20 21:20:14 +0330

    feat: Added Email to RegisterAccount

--- AccountManagement.Application.Contracts/Account/RegisterAccount.cs
+++ AccountManagement.Application.Contracts/Account/RegisterAccount.cs
@@ -8,21 +8,27 @@ namespace AccountManagement.Application.Contracts.Account
     public class RegisterAccount
     {
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
-        public string Fullname { get; set; }
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        [EmailAddress(ErrorMessage = ValidationMessages.EmailIsNotValid)]
+        public string Email { get; set; }
         
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
         public string Mobile { get; set; }
         
         [Range(1, int.MaxValue , ErrorMessage = ValidationMessages.IsRequired)]
         public int RoleId { get; set; }
 
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
         public IFormFile ProfilePhoto { get; set; }
 
         public List<RoleViewModel> Roles { get; set; }
