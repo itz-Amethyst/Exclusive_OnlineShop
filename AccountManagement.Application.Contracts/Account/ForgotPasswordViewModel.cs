dd712e7d8f8bcb8023ad98dcf0314a7e5d8a69b7
callme-devil <pransermi@gmail.com>
2022-10-22 21:13:31 +0330

    feat: ForgotPasswordViewModel Added

---
+++ AccountManagement.Application.Contracts/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AccountManagement.Application.Contracts.Account
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        [EmailAddress(ErrorMessage = "ایمل وارد شده معتبر نمیباشد")]
+        public string Email { get; set; }
+    }
+}
