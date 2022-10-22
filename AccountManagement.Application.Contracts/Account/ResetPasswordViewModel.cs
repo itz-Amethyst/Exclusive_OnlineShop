c18475fb0430dda0e2e61966d6a486b9d9428966
callme-devil <pransermi@gmail.com>
2022-10-22 21:13:15 +0330

    feat: ResetPasswordViewModel Added

---
+++ AccountManagement.Application.Contracts/Account/ResetPasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountManagement.Application.Contracts.Account
+{
+    public class ResetPasswordViewModel
+    {
+        public string ActiveCode { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید")]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        [Compare(nameof(Password), ErrorMessage = "رمز عبور و تکرار آن یکسان نمیباشد")]
+        public string RePassword { get; set; }
+    }
+}
