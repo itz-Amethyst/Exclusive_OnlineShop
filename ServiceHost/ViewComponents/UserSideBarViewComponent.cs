5def3dbbf8645c5011dd0171e63e62584abe8283
callme-devil <pransermi@gmail.com>
2022-10-23 21:42:06 +0330

    feat: UserSideBarViewComponent Added

---
+++ ServiceHost/ViewComponents/UserSideBarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class UserSideBarViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
