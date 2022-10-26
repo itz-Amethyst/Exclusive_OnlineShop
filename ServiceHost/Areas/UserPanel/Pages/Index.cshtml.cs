69c8515088edb9a96d3fa9c5f96c399e89e8152e
callme-devil <pransermi@gmail.com>
2022-10-26 19:27:04 +0330

    feat: Filled AccountInformationViewModel in UserPanel IndexModelPage

--- ServiceHost/Areas/UserPanel/Pages/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/Index.cshtml.cs
@@ -1,11 +1,23 @@
+using AccountManagement.Application.Contracts.Account;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Areas.UserPanel.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IAccountApplication _accountApplication;
+
+        public AccountInformationViewModel Information;
+
+        public IndexModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+        }
+
         public void OnGet()
         {
+            Information = _accountApplication.GetUserInformation(User.Identity.Name);
         }
     }
 }
