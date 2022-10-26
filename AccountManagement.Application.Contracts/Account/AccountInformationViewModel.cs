a1ce28f00d6bc1ba4a760606f723483e1dbb44be
callme-devil <pransermi@gmail.com>
2022-10-26 19:25:40 +0330

    feat: Added AccountInformationViewModel

---
+++ AccountManagement.Application.Contracts/Account/AccountInformationViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class AccountInformationViewModel
+    {
+        public string Username { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Mobile { get; set; }
+        
+        public string ProfilePhoto { get; set; }
+
+        public string RegisterDate { get; set; }
+
+        //public int Wallet { get; set; }
+    }
+}
