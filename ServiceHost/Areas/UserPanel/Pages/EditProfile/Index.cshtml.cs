40dc2d0182b15d0e4b4773a97d44388ee98a9c29
callme-devil <pransermi@gmail.com>
2022-10-26 19:25:22 +0330

    refactor: Added EditProfile ViewData Identity & ..

--- ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
@@ -1,10 +1,21 @@
+using AccountManagement.Application.Contracts.Account;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Areas.UserPanel.Pages.EditProfile
 {
-    
+    [Authorize]
     public class IndexModel : PageModel
     {
-       
+        private readonly IAccountApplication _accountApplication;
+
+        public IndexModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
