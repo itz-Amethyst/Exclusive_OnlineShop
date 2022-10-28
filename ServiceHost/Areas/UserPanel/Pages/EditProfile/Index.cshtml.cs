059b0624995ac8329e7aa999f0b1135c6e87cb97
callme-devil <pransermi@gmail.com>
2022-10-28 21:54:26 +0330

    feat: UserPanel EditPage IndexModelPage Filled

--- ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
@@ -2,13 +2,18 @@ using AccountManagement.Application.Contracts.Account;
 using AccountManagement.Application.Contracts.Account.User;
 using AccountManagement.Domain.AccountAgg;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Common;
 
 namespace ServiceHost.Areas.UserPanel.Pages.EditProfile
 {
     [Authorize]
     public class IndexModel : PageModel
     {
+        [TempData] 
+        public string EditMessage { get; set; }
+
         private readonly IAccountApplication _accountApplication;
         private readonly IAccountRepository _accountRepository;
 
@@ -23,6 +28,25 @@ namespace ServiceHost.Areas.UserPanel.Pages.EditProfile
         public void OnGet()
         {
             Command = _accountRepository.GetProfileDetails(User.Identity.Name);
+        }
+
+        public IActionResult OnPost(EditUserProfile command)
+        {
+            var result = _accountApplication.EditUserProfile(command);
+
+            if (result.IsSuccessful)
+            {
+                return RedirectToPage("/EditProfile/Index");
+            }
+
+            if (result.IsNeedToLoginAgain)
+            {
+                return RedirectToPage("/Login");
+            }
+
+            EditMessage = result.Message;
+
+            return RedirectToPage("/EditProfile/Index");
         }
     }
 }
