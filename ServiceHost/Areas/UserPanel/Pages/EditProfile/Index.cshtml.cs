31b631ce17c8845f7bbfe185397e95647cdea111
callme-devil <pransermi@gmail.com>
2022-10-28 17:02:39 +0330

    feat: Filled EditProfileUser View Model in EditProfile IndexModelPage

--- ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/EditProfile/Index.cshtml.cs
@@ -1,4 +1,6 @@
 using AccountManagement.Application.Contracts.Account;
+using AccountManagement.Application.Contracts.Account.User;
+using AccountManagement.Domain.AccountAgg;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -8,14 +10,19 @@ namespace ServiceHost.Areas.UserPanel.Pages.EditProfile
     public class IndexModel : PageModel
     {
         private readonly IAccountApplication _accountApplication;
+        private readonly IAccountRepository _accountRepository;
 
-        public IndexModel(IAccountApplication accountApplication)
+        public EditUserProfile Command;
+
+        public IndexModel(IAccountApplication accountApplication, IAccountRepository accountRepository)
         {
             _accountApplication = accountApplication;
+            _accountRepository = accountRepository;
         }
 
         public void OnGet()
         {
+            Command = _accountRepository.GetProfileDetails(User.Identity.Name);
         }
     }
 }
