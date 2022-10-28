e8390bb776139a764714a7f8efab432e17108ef1
callme-devil <pransermi@gmail.com>
2022-10-28 16:30:38 +0330

    feat: EditUserProfile Added

---
+++ AccountManagement.Application.Contracts/Account/User/EditUserProfile.cs
@@ -0,0 +1,26 @@
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountManagement.Application.Contracts.Account.User
+{
+    public class EditUserProfile
+    {
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(11, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        public string Mobile { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        [EmailAddress(ErrorMessage = ValidationMessages.EmailIsNotValid)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        [MaxLength(200, ErrorMessage = " {0} نمیتواند بیشتر از {1} کاراکترباشد")]
+        public string ProfilePicture { get; set; }
+    }
+}
