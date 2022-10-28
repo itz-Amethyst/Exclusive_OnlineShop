e0bf1308c14f216ce675025d7d5c6905d5abfef9
callme-devil <pransermi@gmail.com>
2022-10-28 15:17:10 +0330

    feat: Added UserPanelSideBarViewModel

---
+++ AccountManagement.Application.Contracts/Account/UserPanelSideBarViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountManagement.Application.Contracts.Account
+{
+    public class UserPanelSideBarViewModel
+    {
+        public string UserName { get; set; }
+
+        public string RegisterDate { get; set; }
+
+        public string UserProfilePicture { get; set; }
+    }
+}
