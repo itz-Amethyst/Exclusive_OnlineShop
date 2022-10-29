cf4050bb9a35321254add1feedfd9056fcbf21b2
callme-devil <pransermi@gmail.com>
2022-10-29 22:46:26 +0330

    feat: Configured ResetPassword UserPanel IndexModelPage

--- ServiceHost/Areas/UserPanel/Pages/ChangePassword/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/ChangePassword/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using AccountManagement.Application.Contracts.Account;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ServiceHost.Areas.UserPanel.Pages.ChangePassword
@@ -5,6 +7,30 @@ namespace ServiceHost.Areas.UserPanel.Pages.ChangePassword
     
     public class IndexModel : PageModel
     {
-       
+        [TempData]
+        public string ChangePasswordMessage { get; set; }
+
+        private readonly IAccountApplication _accountApplication;
+
+        public ChangePasswordViewModel Command;
+
+        public IndexModel(IAccountApplication accountApplication)
+        {
+            _accountApplication = accountApplication;
+        }
+
+        public IActionResult OnPost(ChangePasswordViewModel command)
+        {
+            var result = _accountApplication.ChangePasswordByUser(command);
+
+            if (result.IsSuccessful)
+            {
+                return RedirectToPage("./Login?ChangePassword=true");
+            }
+
+            ChangePasswordMessage = result.Message;
+
+            return RedirectToPage("/ChangePassword/Index");
+        }
     }
 }
