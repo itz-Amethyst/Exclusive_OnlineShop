e8ec692f395a0e4326a7f8848cb591d0c330a6c6
callme-devil <pransermi@gmail.com>
2022-11-02 20:55:11 +0330

    feat: Added Permission RolePermission Tables Migration

--- AccountManagement.Infrastructure.EFCore/Migrations/20221102165703_AddedPermissionAndRolePermissionTables.Designer.cs
+++ AccountManagement.Infrastructure.EFCore/Migrations/20221102165703_AddedPermissionAndRolePermissionTables.Designer.cs
@@ -4,6 +4,7 @@ using AccountManagement.Infrastructure.EFCore.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AccountManagement.Infrastructure.EFCore.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20221102165703_AddedPermissionAndRolePermissionTables")]
+    partial class AddedPermissionAndRolePermissionTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
