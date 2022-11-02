3153826868dee27bceb27579d0d61cb68c79d87e
callme-devil <pransermi@gmail.com>
2022-11-02 20:25:56 +0330

    feat: Added PermissionMapping

---
+++ AccountManagement.Infrastructure.EFCore/Mapping/PermissionMapping.cs
@@ -0,0 +1,19 @@
+﻿using AccountManagement.Domain.RoleAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountManagement.Infrastructure.EFCore.Mapping
+{
+    public class PermissionMapping : IEntityTypeConfiguration<Permission>
+    {
+        public void Configure(EntityTypeBuilder<Permission> builder)
+        {
+            builder.ToTable("Permissions");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.PermissionTitle).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.ParentId).IsRequired(false);
+
+        }
+    }
+}
