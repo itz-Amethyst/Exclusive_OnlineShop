fbd03acfc919f7556b72bac8e68bd095a330ed1c
callme-devil <pransermi@gmail.com>
2022-11-02 18:56:38 +0330

    feat: Added Create Role Page

---
+++ ServiceHost/Areas/Administration/Pages/Accounts/Role/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using AccountManagement.Application.Contracts.Role;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages.Accounts.Role
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IRoleApplication _roleApplication;
+
+        public CreateRole command;
+
+        public CreateModel(IRoleApplication roleApplication)
+        {
+            _roleApplication = roleApplication;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public IActionResult OnPost(CreateRole command)
+        {
+            var result = _roleApplication.Create(command);
+            return RedirectToPage("./Index", new { Created = "True" });
+        }
+    }
+}
