a836569d936d590a8ae1ce1def583bde341f5527
callme-devil <pransermi@gmail.com>
2022-11-02 20:26:07 +0330

    feat: Added RolePermissionMapping

---
+++ AccountManagement.Infrastructure.EFCore/Mapping/RolePermissionsMapping.cs
@@ -0,0 +1,26 @@
+﻿using AccountManagement.Domain.RoleAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountManagement.Infrastructure.EFCore.Mapping
+{
+    public class RolePermissionsMapping : IEntityTypeConfiguration<RolePermissions>
+    {
+        public void Configure(EntityTypeBuilder<RolePermissions> builder)
+        {
+            builder.ToTable("RolePermissions");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.RoleId).IsRequired();
+            builder.Property(x => x.PermissionId).IsRequired();
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x=>x.RolePermissions)
+                .HasForeignKey(x => x.RoleId);
+
+            builder.HasOne(x => x.Permission)
+                .WithMany(x => x.RolePermissions)
+                .HasForeignKey(x => x.PermissionId);
+        }
+    }
+}
