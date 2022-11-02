d9bc10a1b953d365c36eabf40efa0b1d7dae0561
callme-devil <pransermi@gmail.com>
2022-11-02 20:24:37 +0330

    feat: Added RolePermission

---
+++ AccountManagement.Domain/RoleAgg/RolePermissions.cs
@@ -0,0 +1,22 @@
+﻿namespace AccountManagement.Domain.RoleAgg
+{
+    public class RolePermissions
+    {
+        public int Id { get; private set; }
+
+        public int RoleId { get; private set; }
+
+        public int PermissionId { get; private set; }
+
+        public RolePermissions(int roleId, int permissionId)
+        {
+            RoleId = roleId;
+            PermissionId = permissionId;
+        }
+
+
+        public Role Role { get; private set; }
+
+        public Permission Permission { get; private set; }
+    }
+}
