1288f70ade826ed218dab1e9564c9b80182d2e7c
callme-devil <pransermi@gmail.com>
2022-11-02 20:24:22 +0330

    feat: Added Permission Class

---
+++ AccountManagement.Domain/RoleAgg/Permission.cs
@@ -0,0 +1,28 @@
+﻿using AccountManagement.Domain.AccountAgg;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace AccountManagement.Domain.RoleAgg
+{
+    public class Permission
+    {
+        public int Id { get; private set; }
+
+        public string PermissionTitle { get; private set; }
+
+        public int? ParentId { get; private set; }
+
+        public Permission(string permissionTitle, int? parentId)
+        {
+            PermissionTitle = permissionTitle;
+            ParentId = parentId;
+        }
+
+        [ForeignKey("ParentId")]
+        public List<Permission> Permissions { get; private set; }
+
+        public List<RolePermissions> RolePermissions { get; private set; }
+        
+    }
+
+}
