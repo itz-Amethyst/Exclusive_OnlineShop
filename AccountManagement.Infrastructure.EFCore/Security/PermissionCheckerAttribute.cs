dd8269800224f2c6185dc3cf9a342dcaa518e080
callme-devil <pransermi@gmail.com>
2022-11-04 20:47:32 +0330

    feat: Created PermissionCheckerAttribute Class

---
+++ AccountManagement.Infrastructure.EFCore/Security/PermissionCheckerAttribute.cs
@@ -0,0 +1,39 @@
+﻿using AccountManagement.Domain.RoleAgg;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AccountManagement.Infrastructure.EFCore.Security
+{
+    public class PermissionCheckerAttribute : AuthorizeAttribute , IAuthorizationFilter
+    {
+        private IRoleRepository _roleRepository;
+
+        private int _permissionId = 0;
+
+        public PermissionCheckerAttribute(int permissionId)
+        {
+            _permissionId = permissionId;
+        }
+        
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            _roleRepository =
+                (IRoleRepository)context.HttpContext.RequestServices.GetService(typeof(IRoleRepository));
+
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                string UserName = context.HttpContext.User.Identity.Name;
+
+                if (!_roleRepository.CheckPermission(_permissionId, UserName))
+                {
+                    context.Result = new RedirectResult("/AccessDenied");
+                }
+            }
+            else
+            {
+                context.Result = new RedirectResult("/Login");
+            }
+        }
+    }
+}
