e4b67b47e7e4ad415abcccc7c34d9ce7d53168b5
callme-devil <pransermi@gmail.com>
2022-11-04 20:47:51 +0330

    feat: Added Permissions in Roles Class

--- 0_Framework/Infrastructure/Roles.cs
+++ 0_Framework/Infrastructure/Roles.cs
@@ -2,24 +2,69 @@
 {
     public static class Roles
     {
-        public const string Administrator = "2";
-        public const string User = "3";
-        public const string Manager = "4";
+        public const int Administrator = 1;
+        public const int ManageUsers = 2;
+        public const int CreateUser = 3;
+        public const int EditUser = 4;
+        public const int DeleteUser = 5;
+        public const int ManageRoles = 6;
+        public const int CreateRole = 7;
+        public const int EditRole = 8;
+        public const int DeleteRole = 9;
+        public const int ManageShop = 10;
+        public const int CreateProduct = 11;
+        public const int EditProduct = 12;
+        public const int DeleteProduct = 13;
+        public const int ManageProductCategory = 14;
+        public const int CreateProductCategory = 15;
+        public const int EditProductCategory = 16;
+        public const int ManageProductPicture = 17;
+        public const int AddProductPicture = 18;
+        public const int EditProductPicture = 19;
+        public const int DeleteProductPicture = 20;
+        public const int ManageSlider = 21;
+        public const int CreateSlider = 22;
+        public const int EditSlider = 23;
+        public const int DeleteSlider = 24;
+        public const int ManageColleagueDiscount = 25;
+        public const int CreateColleagueDiscount = 26;
+        public const int EditColleagueDiscount = 27;
+        public const int DeleteColleagueDiscount = 28;
+        public const int ManageCustomerDiscount = 29;
+        public const int CreateCustomerDiscount = 30;
+        public const int EditCustomerDiscount = 31;
+        public const int DeleteCustomerDiscount = 32;
+        public const int ManageComment = 33;
+        public const int AccessToCommentOperations = 34;
+        public const int ManageArticleCategory = 35;
+        public const int CreateArticleCategory = 36;
+        public const int EditArticleCategory = 37;
+        public const int ManageArticle = 38;
+        public const int CreateArticle = 39;
+        public const int EditArticle = 40;
+        public const int DeleteArticle = 41;
+        public const int ManageInventory = 42;
+        public const int AddInventory = 43;
+        public const int EditInventory = 44;
+        public const int IncreaseItemInventory = 45;
+        public const int DecreaseItemInventory = 46;
+        public const int ShowOperationHistory = 47;
+        public const int ChangeUserPassword = 48;
+        
 
-
-        public static string GetRoleBy(int id)
-        {
-            switch (id)
-            {
-                case 2:
-                    return "مدیر سیستم";
-                case 3:
-                    return "کاربر";
-                case 4:
-                    return "منیجر";
-                default:
-                     return "";
-            }
-        }
+        //public static string GetRoleBy(int id)
+        //{
+        //    switch (id)
+        //    {
+        //        case 2:
+        //            return "مدیر سیستم";
+        //        case 3:
+        //            return "کاربر";
+        //        case 4:
+        //            return "منیجر";
+        //        default:
+        //             return "";
+        //    }
+        //}
     }
 }
