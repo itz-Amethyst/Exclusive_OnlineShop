269e65fe41efda042b16c043910c9115c6f84743
callme-devil <pransermi@gmail.com>
2022-11-06 20:48:40 +0330

    feat: Moved CheckPermission From RoleRepository 2/2

--- AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
+++ AccountManagement.Infrastructure.EFCore/Repository/RoleRepository.cs
@@ -95,24 +95,5 @@ namespace AccountManagement.Infrastructure.EFCore.Repository
             return true;
 
         }
-
-        public bool CheckPermission(int permissionId, string username)
-        {
-            int userId = _accountContext.Accounts.Single(x => x.Username == username).Id;
-            
-           
-            List<int> UserRoles = _accountContext.Accounts
-                .Where(r => r.Id == userId)
-                .Select(u => u.RoleId).ToList();
-
-            if (!UserRoles.Any())
-                return false;
-
-            List<int> RolesPermission = _accountContext.RolePermissions
-                .Where(p => p.PermissionId == permissionId)
-                .Select(p => p.RoleId).ToList();
-
-            return RolesPermission.Any(p => UserRoles.Contains(p));
-        }
     }
 }
