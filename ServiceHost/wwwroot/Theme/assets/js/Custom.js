d3a4bb9e24ff9df73ff1fa96b2a3c92ec46509e8
callme-devil <pransermi@gmail.com>
2022-11-09 19:52:16 +0330

    feat: Added CustomJs & Filled AddToCart func

---
+++ ServiceHost/wwwroot/Theme/assets/js/Custom.js
@@ -0,0 +1,30 @@
+﻿const cookieName = "cart-items";
+const cookieExpireDay = 7;
+
+function addToCart(id, name, price, picture) {
+    let products = $.cookie(cookieName);
+
+    if (products === undefined) {
+        products = [];
+    }
+    else {
+        products = JSON.parse(products); 
+    }
+
+    const count = $("#productCount").val();
+
+    const currentProduct = products.find(p => p.id === id);
+
+    if (currentProduct != undefined) {
+        currentProduct.count = parseInt(currentProduct.count) + parseInt(count);
+    }
+    else {
+        const product = {
+            id , name , price , picture , count
+        }
+
+        products.push(product);
+    }
+
+    $.cookie(cookieName, JSON.stringify(products), { expires: cookieExpireDay, path: "/" });
+}
