d850dbb237009dd5f73673dff49ebbb32f58042d
callme-devil <pransermi@gmail.com>
2022-11-09 21:12:33 +0330

    feat: Func UpdateCart Filled

--- ServiceHost/wwwroot/Theme/assets/js/Custom.js
+++ ServiceHost/wwwroot/Theme/assets/js/Custom.js
@@ -1,7 +1,7 @@
 ﻿const cookieName = "cart-items";
 const cookieExpireDay = 7;
 
-function addToCart(id, name, price, picture) {
+function addToCart(id, name, price, picture , slug) {
     let products = $.cookie(cookieName);
 
     if (products === undefined) {
@@ -20,7 +20,7 @@ function addToCart(id, name, price, picture) {
     }
     else {
         const product = {
-            id , name , price , picture , count
+            id , name , price , picture , count , slug
         }
 
         products.push(product);
@@ -35,4 +35,42 @@ function updateCart() {
     products = JSON.parse(products);
 
     $("#cart_items_count").text(products.length);
+
+    const cartItemsWrapper = $("#cart_items_wrapper");
+    cartItemsWrapper.empty();
+
+    products.forEach(p => {
+        // Work On This
+        // <p class="count">
+        //    <span>قیمت کل: ${totalPrice} </span>
+        //</p>
+        //totalPrice = parseFloat(p.price.replace(/,/g, '')) * parseInt(p.count);
+        let product =
+            ` <div class="single-cart-item">
+                    <a href="javascript:void(0)" class="remove-icon">
+                        <i class="ion-android-close"></i>
+                    </a>
+                    <div class="image">
+                        <a href="single-product.html">
+                            <img src="/UploadedFiles/${p.picture}"
+                                 class="img-fluid" alt="">
+                        </a>
+                    </div>
+                    <div class="content">
+                        <p class="product-title">
+                            <a href="https://localhost:7018/Product/${p.slug}">محصول: ${p.name}</a>
+                        </p>
+                        <p class="count">
+                            <span>تعداد:‌ ${p.count} </span>
+                        </p>
+                        <p class="count">
+                            <span>قیمت واحد: ${p.price} </span>
+                        </p>
+                      
+                    </div>
+                </div>
+            `;
+
+        cartItemsWrapper.append(product);
+    });
 }
