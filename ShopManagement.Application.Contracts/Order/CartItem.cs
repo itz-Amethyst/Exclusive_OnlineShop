8449d90f9652f836f15ba9a8579aa8bdf7a94691
callme-devil <pransermi@gmail.com>
2022-11-10 17:53:52 +0330

    feat: Added CartItem Class

---
+++ ShopManagement.Application.Contracts/Order/CartItem.cs
@@ -0,0 +1,24 @@
+﻿namespace ShopManagement.Application.Contracts.Order
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public string Picture { get; set; }
+
+        public int Count { get; set; }
+
+        public string Slug { get; set; }
+
+        public double TotalItemPrice { get; set; }
+
+        public CartItem()
+        {
+            TotalItemPrice = UnitPrice * Count;
+        }
+    }
+}
