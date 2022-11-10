92a1d4f9af33b20a66af5459241ec03b98038f8d
callme-devil <pransermi@gmail.com>
2022-11-10 17:42:41 +0330

    feat: Created Cart Page & Injected Default Design

---
+++ ServiceHost/Pages/Shared/Cart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Pages.Shared
+{
+    public class CartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
