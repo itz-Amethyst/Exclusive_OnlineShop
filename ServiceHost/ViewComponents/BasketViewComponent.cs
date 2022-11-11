9a200252b80e0374eeb2256125de3195df5ae682
callme-devil <pransermi@gmail.com>
2022-11-11 22:43:45 +0330

    feat: configured basketviewcomponent

--- ServiceHost/ViewComponents/BasketViewComponent.cs
+++ ServiceHost/ViewComponents/BasketViewComponent.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _01_ExclusiveQuery.Contracts.Order;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
 
 namespace ServiceHost.ViewComponents
 {
     public class BasketViewComponent : ViewComponent
     {
+        private readonly IOrderQuery _orderQuery;
+        public List<CartQueryModel> CartItems;
+
+        public BasketViewComponent(IOrderQuery orderQuery)
+        {
+            _orderQuery = orderQuery;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            if (Request.Cookies["cart-items"] != null)
+            {
+                var serializer = new JavaScriptSerializer();
+                var value = Request.Cookies["cart-items"];
+                CartItems = serializer.Deserialize<List<CartQueryModel>>(value);
+                var basket = _orderQuery.GetCartItemsBy(CartItems);
+                return View(basket);
+
+            }
+
+            return View(CartItems);
         }
     }
 }
