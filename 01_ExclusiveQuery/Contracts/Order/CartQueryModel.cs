d2b1d05c5584dd4c989a479d8e6e4dcfbba57bed
callme-devil <pransermi@gmail.com>
2022-11-11 22:21:50 +0330

    feat: CartQueryModel Added

---
+++ 01_ExclusiveQuery/Contracts/Order/CartQueryModel.cs
@@ -0,0 +1,26 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Order
+{
+    public class CartQueryModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Picture { get; set; }
+
+        public int Count { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public double TotalItemPrice { get; set; }
+
+        public bool HasDiscount { get; set; }
+
+        public int DiscountRate { get; set; }
+
+        public double PriceWithDiscount { get; set; }
+        
+    }
+}
