444b07be2a838aeee60e7ae6511e9e71c5e1e5c8
callme-devil <pransermi@gmail.com>
2022-11-11 22:42:15 +0330

    feat: Created IOrderQuery

---
+++ 01_ExclusiveQuery/Contracts/Order/IOrderQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Order
+{
+    public interface IOrderQuery
+    {
+        public List<CartQueryModel> GetCartItemsBy(List<CartQueryModel> cartItems);
+    }
+}
