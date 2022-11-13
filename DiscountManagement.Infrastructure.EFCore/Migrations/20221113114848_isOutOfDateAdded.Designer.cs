ea80e9e789ead9cb916248e26ec80a0d303033e1
callme-devil <pransermi@gmail.com>
2022-11-13 16:48:27 +0330

    feat: IsOutOfDate Migration Added

--- DiscountManagement.Infrastructure.EFCore/Migrations/20221113114848_isOutOfDateAdded.Designer.cs
+++ DiscountManagement.Infrastructure.EFCore/Migrations/20221113114848_isOutOfDateAdded.Designer.cs
@@ -4,6 +4,7 @@ using DiscountManagement.Infrastructure.EFCore.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiscountManagement.Infrastructure.EFCore.Migrations
 {
     [DbContext(typeof(DiscountContext))]
-    partial class DiscountContextModelSnapshot : ModelSnapshot
+    [Migration("20221113114848_isOutOfDateAdded")]
+    partial class isOutOfDateAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DiscountManagement.Infrastructure.EFCore/Migrations/20221113114848_isOutOfDateAdded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscountManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class isOutOfDateAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOutOfDate",
+                table: "CustomerDiscounts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOutOfDate",
+                table: "CustomerDiscounts");
+        }
+    }
+}
