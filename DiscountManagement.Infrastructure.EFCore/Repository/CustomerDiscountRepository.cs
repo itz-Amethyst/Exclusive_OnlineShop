d85f50209ae18bf3f1d7354952fed0c2091325f2
callme-devil <pransermi@gmail.com>
2022-11-13 16:54:07 +0330

    feat: Added new OutOfDate Feature to CustomerDiscount

--- DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
+++ DiscountManagement.Infrastructure.EFCore/Repository/CustomerDiscountRepository.cs
@@ -9,7 +9,7 @@ using ShopManagement.Infrastructure.EFCore.Context;
 
 namespace DiscountManagement.Infrastructure.EFCore.Repository
 {
-    public class CustomerDiscountRepository : RepositoryBase<int , CustomerDiscount>, ICustomerDiscountRepository
+    public class CustomerDiscountRepository : RepositoryBase<int, CustomerDiscount>, ICustomerDiscountRepository
     {
         private readonly DiscountContext _context;
         private readonly ShopContext _shopContext;
@@ -30,13 +30,16 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
                 StartDate = x.StartDate.ToString(),
                 EndDate = x.EndDate.ToString(),
                 Reason = x.Reason
-            }).First(x=>x.Id == id);
+            }).First(x => x.Id == id);
         }
 
         public List<CustomerDiscountViewModel> Search(CustomerDiscountSearchModel searchModel)
         {
             //? Give Me Only Id & Name Better Query
             var products = _shopContext.Products.Select(x => new { x.Id, x.Name }).ToList();
+
+            //var discountOutOfDate = _context.CustomerDiscounts.Select(x => new { x.Id, x.EndDate }).AsNoTracking().ToList();
+
 
             var query = _context.CustomerDiscounts.Select(x => new CustomerDiscountViewModel
             {
@@ -49,7 +52,8 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
                 ProductId = x.ProductId,
                 Reason = x.Reason,
                 CreationDate = x.CreationDate.ToFarsi(),
-                IsDeleted = x.IsDeleted
+                IsDeleted = x.IsDeleted,
+                IsOutOfDate = x.IsOutOfDate,
             });
 
             if (searchModel.ProductId > 0)
@@ -67,10 +71,48 @@ namespace DiscountManagement.Infrastructure.EFCore.Repository
                 query = query.Where(x => x.EndDateEn < searchModel.EndDate.ToGeorgianDateTime());
             }
 
+            //!:) 
+
+            //query.ForEachAsync(x =>
+            //{
+            //    var discounts = query.FirstOrDefault(d => d.Id == x.Id);
+
+            //    var endDateEn = query.FirstOrDefault(x => x.Id == discounts.Id).EndDateEn;
+
+            //    int result = DateTime.Compare(endDateEn, DateTime.Now);
+
+            //    if(result < 0)
+            //    {
+            //        query.FirstOrDefault(x => x.Id == discounts.Id).IsOutOfDate = true;
+            //    }
+
+            //});
+
+            foreach (var item in query)
+            {
+                var endDateEn = item.EndDateEn;
+
+                int result = DateTime.Compare(endDateEn, DateTime.Now);
+
+                if (result < 0)
+                {
+                    _context.CustomerDiscounts.FirstOrDefault(x => x.Id == item.Id).IsOutOfDate = true;
+                }
+            }
+
             var discount = query.OrderByDescending(x => x.Id).ToList();
 
-            discount
-                .ForEach(discount => discount.ProductName = products.First(x=>x.Id == discount.ProductId)?.Name);
+            discount.ForEach(discount => discount.ProductName = products.First(x => x.Id == discount.ProductId)?.Name);
+            
+            discount.ForEach(discount => discount.IsOutOfDate = _context.CustomerDiscounts.FirstOrDefault(x => x.Id == discount.Id).IsOutOfDate);
+
+
+            //discount = _context.CustomerDiscounts.Select(x => query.Select(x=>x.IsOutOfDate)
+            //{
+            //    IsOutOfDate = x.IsOutOfDate
+            //}).ToList();
+
+            _context.SaveChanges();
 
             return discount;
         }
