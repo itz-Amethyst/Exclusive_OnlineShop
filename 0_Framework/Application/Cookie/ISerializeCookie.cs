52e60132737226519293873dd3379b5d6800c15a
callme-devil <pransermi@gmail.com>
2022-11-14 20:28:57 +0330

    feat: Created ISerializeCookie For Better Code My Idea

---
+++ 0_Framework/Application/Cookie/ISerializeCookie.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Nancy.Json;
+
+namespace _0_Framework.Application.Cookie
+{
+    public interface ISerializeCookie
+    {
+        List<CookieCartModel> Serialize(List<CookieCartModel> cartItems , HttpContext httpContext);
+
+        bool CheckSerialize(List<CookieCartModel> cartItems, HttpContext httpContext);
+
+        JavaScriptSerializer JavaScriptSerializer();
+
+        void DeleteCookie(HttpContext httpContext);
+
+        void RemoveItemFromCookie(List<CookieCartModel> cartItems , int id , HttpContext httpContext);
+    }
+}
