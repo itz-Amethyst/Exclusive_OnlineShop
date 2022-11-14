2b6a38353428c22eb7bac19354b77af05da271e8
callme-devil <pransermi@gmail.com>
2022-11-14 20:27:05 +0330

    feat: Moved CookieQueryModel To Cookie Framework To Working (Dependency Error)

---
+++ 0_Framework/Application/Cookie/CookieCartModel.cs
@@ -0,0 +1,26 @@
+﻿namespace _0_Framework.Application.Cookie
+{
+    public class CookieCartModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Picture { get; set; }
+
+        public int Count { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public double TotalItemPrice { get; set; }
+
+        public bool HasDiscount { get; set; }
+
+        public int DiscountRate { get; set; }
+
+        public double PriceWithDiscount { get; set; }
+        
+    }
+}
