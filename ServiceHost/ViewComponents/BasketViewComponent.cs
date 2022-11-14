b1b97d4fee0b0ce7993005515a2db8ba00940ef1
callme-devil <pransermi@gmail.com>
2022-11-14 21:07:56 +0330

    feat: configured Header ViewComponent With New Methods Cookie

--- ServiceHost/ViewComponents/BasketViewComponent.cs
+++ ServiceHost/ViewComponents/BasketViewComponent.cs
@@ -1,57 +1,70 @@
-﻿using _01_ExclusiveQuery.Contracts.Order;
+﻿using _0_Framework.Application.Cookie;
+using _01_ExclusiveQuery.Contracts.Order;
 using Microsoft.AspNetCore.Mvc;
-using Nancy.Json;
 
 namespace ServiceHost.ViewComponents
 {
     public class BasketViewComponent : ViewComponent
     {
         private readonly IOrderQuery _orderQuery;
-        public List<CartQueryModel> CartItems;
+        private readonly ISerializeCookie _serializeCookie;
+        public List<CookieCartModel> CartItems;
 
-        [TempData] 
-        public string CartMessage { get; set; }
-        
         public const string CookieName = "cart-items";
 
-        public BasketViewComponent(IOrderQuery orderQuery)
+        public BasketViewComponent(IOrderQuery orderQuery, ISerializeCookie serializeCookie)
         {
             _orderQuery = orderQuery;
+            _serializeCookie = serializeCookie;
         }
 
         public IViewComponentResult Invoke()
         {
             if (Request.Cookies["cart-items"] != null)
             {
-                var serializer = new JavaScriptSerializer();
-                var value = Request.Cookies[CookieName];
-                try
+                //var serializer = new JavaScriptSerializer();
+                //var value = Request.Cookies[CookieName];
+                //try
+                //{
+                //    CartItems = serializer.Deserialize<List<CartQueryModel>>(value);
+                //}
+                //catch (Exception e)
+                //{
+                //    ////HttpContext.Response.Cookies.Delete(CookieName);
+                //    ////var mycookie = HttpContext.Request.Cookies[CookieName];
+
+                //    ////Request.Cookies[CookieName] = null;
+                //    ////var cookieeee = HttpContext.Request.Cookies["cart-items"];
+
+                //    ////cookieeee.ex = DateTime.Now.AddDays(-1);
+
+                //    //HttpContext.Request.Cookies cookie = new HttpCookie(key)
+                //    //{
+                //    //    Expires = DateTime.Now.AddDays(-1) // or any other time in the past
+                //    //};
+
+                //    ////f.Delete(CookieName);
+
+                //    ////Console.WriteLine(e);
+                //    Console.WriteLine(e);
+                //    HttpContext.Response.Cookies.Delete(CookieName);
+                //    return View(CartItems);
+                //}
+
+                if (!_serializeCookie.CheckSerialize(CartItems, HttpContext))
                 {
-                    CartItems = serializer.Deserialize<List<CartQueryModel>>(value);
-                }
-                catch (Exception e)
-                {
-                    ////HttpContext.Response.Cookies.Delete(CookieName);
-                    ////var mycookie = HttpContext.Request.Cookies[CookieName];
-
-                    ////Request.Cookies[CookieName] = null;
-                    ////var cookieeee = HttpContext.Request.Cookies["cart-items"];
-
-                    ////cookieeee.ex = DateTime.Now.AddDays(-1);
-
-                    //HttpContext.Request.Cookies cookie = new HttpCookie(key)
-                    //{
-                    //    Expires = DateTime.Now.AddDays(-1) // or any other time in the past
-                    //};
-
-                    ////f.Delete(CookieName);
-
-                    ////Console.WriteLine(e);
-                    CartMessage = "t";
+                    //?Just in Case
+                    HttpContext.Response.Cookies.Delete(CookieName);
                     return View(CartItems);
                 }
 
+                CartItems = _serializeCookie.Serialize(CartItems, HttpContext);
 
+                if (CartItems.Count <= 0)
+                {
+                    _serializeCookie.DeleteCookie(HttpContext);
+                    return View(CartItems);
+                }
 
                 var basket = _orderQuery.GetCartItemsBy(CartItems);
                 return View(basket);
