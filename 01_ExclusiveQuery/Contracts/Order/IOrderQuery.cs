52dddcaada7281660c7b3678a8fa4cbb14af5c7f
callme-devil <pransermi@gmail.com>
2022-11-16 23:13:52 +0330

    feat: Created New Method IOrderQuery

--- 01_ExclusiveQuery/Contracts/Order/IOrderQuery.cs
+++ 01_ExclusiveQuery/Contracts/Order/IOrderQuery.cs
@@ -1,9 +1,12 @@
 ﻿using _0_Framework.Application.Cookie;
+using Microsoft.AspNetCore.Http;
 
 namespace _01_ExclusiveQuery.Contracts.Order
 {
     public interface IOrderQuery
     {
-        public List<CookieCartModel> GetCartItemsBy(List<CookieCartModel> cartItems);
+        List<CookieCartModel> GetCartItemsBy(List<CookieCartModel> cartItems , HttpContext httpContext);
+
+        CartModelWithSummary GetSummary(List<CookieCartModel> cartItems);
     }
 }
