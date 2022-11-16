a80aedde0bbd7fd9a39dbd50734356e1c2c1b2e9
callme-devil <pransermi@gmail.com>
2022-11-16 23:15:46 +0330

    feat: Configured Checkout IndexModelPage

--- ServiceHost/Pages/Checkout.cshtml.cs
+++ ServiceHost/Pages/Checkout.cshtml.cs
@@ -1,3 +1,5 @@
+using _0_Framework.Application.Cookie;
+using _01_ExclusiveQuery.Contracts.Order;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,72 @@ namespace ServiceHost.Pages
 {
     public class CheckoutModel : PageModel
     {
+        private readonly ISerializeCookie _serializeCookie;
+        private readonly IOrderQuery _orderQuery;
+
+        [TempData] public bool EmptyBasket { get; set; }
+
+        public List<CookieCartModel> CartItems;
+
+        public CartModelWithSummary TotalCartSummaryModel;
+
+
+        public const string CookieName = "cart-items";
+        
+
+        public CheckoutModel(ISerializeCookie serializeCookie, IOrderQuery orderQuery)
+        {
+            _serializeCookie = serializeCookie;
+            _orderQuery = orderQuery;
+        }
+
         public void OnGet()
         {
+            if (Request.Cookies["cart-items"] != null)
+            {
+                //var serializer = new JavaScriptSerializer();
+                //var value = Request.Cookies[CookieName];
+                //try
+                //{
+                //    CartItems = serializer.Deserialize<List<CartQueryModel>>(value);
+                //}
+                //catch (Exception e)
+                //{
+                //    flag = true;
+                //    Console.WriteLine(e);
+                //}
+
+                //if (flag)
+                //{
+                //    //CartMessage = "t";
+                //    HttpContext.Response.Cookies.Delete(CookieName);
+                //    return;
+                //}
+
+                if (!_serializeCookie.CheckSerialize(CartItems, HttpContext))
+                {
+                    //?Just in Case
+                    _serializeCookie.DeleteCookie(HttpContext);
+                    return;
+                }
+
+                CartItems = _serializeCookie.Serialize(CartItems, HttpContext);
+
+                if (CartItems.Count <= 0)
+                {
+                    EmptyBasket = true;
+                    _serializeCookie.DeleteCookie(HttpContext);
+                    return;
+                }
+
+                EmptyBasket = false;
+
+
+                CartItems = _orderQuery.GetCartItemsBy(CartItems , HttpContext);
+
+                TotalCartSummaryModel = _orderQuery.GetSummary(CartItems);
+
+            }
         }
     }
 }
