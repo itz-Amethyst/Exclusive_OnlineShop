2520d1a039f738cca231836b096560d4634cddd3
callme-devil <pransermi@gmail.com>
2022-11-16 23:13:03 +0330

    feat: Filled new Method Is User have Colleague role

--- AccountManagement.Infrastructure.EFCore/Security/PermissionChecker.cs
+++ AccountManagement.Infrastructure.EFCore/Security/PermissionChecker.cs
@@ -1,4 +1,6 @@
-﻿using AccountManagement.Infrastructure.EFCore.Context;
+﻿using _0_Framework.Infrastructure;
+using AccountManagement.Domain.RoleAgg;
+using AccountManagement.Infrastructure.EFCore.Context;
 
 namespace AccountManagement.Infrastructure.EFCore.Security
 {
@@ -32,6 +34,32 @@ namespace AccountManagement.Infrastructure.EFCore.Security
 
             List<int> RolesPermission = _accountContext.RolePermissions
                 .Where(p => p.PermissionId == permissionId)
+                .Select(p => p.RoleId).ToList();
+
+            return RolesPermission.Any(p => UserRoles.Contains(p));
+        }
+
+        public bool CheckUserHasColleagueRole(string username)
+        {
+            int userId = _accountContext.Accounts.Single(x => x.Username == username).Id;
+
+            List<int> UserRoles = _accountContext.Accounts
+                .Where(r => r.Id == userId)
+                .Select(u => u.RoleId).ToList();
+
+            //? Do not work on Multiple roles
+            bool checkIsDeleted = _accountContext.Roles.Single(x => x.Id == UserRoles[0]).IsDeleted;
+
+            if (checkIsDeleted)
+            {
+                return false;
+            }
+
+            if (!UserRoles.Any())
+                return false;
+
+            List<int> RolesPermission = _accountContext.RolePermissions
+                .Where(p => p.PermissionId == Roles.UseColleagueDiscount)
                 .Select(p => p.RoleId).ToList();
 
             return RolesPermission.Any(p => UserRoles.Contains(p));
