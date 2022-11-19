5a1f3203d2a72660a5cfecacc816954f11ada7ef
callme-devil <pransermi@gmail.com>
2022-11-19 14:38:50 +0330

    feat: Created OrderApplication

---
+++ ShopManagement.Application/OrderApplication.cs
@@ -0,0 +1,21 @@
+﻿using _0_Framework.Application.Cookie;
+using ShopManagement.Application.Contracts.Order;
+using ShopManagement.Domain.OrderAgg;
+
+namespace ShopManagement.Application
+{
+    public class OrderApplication : IOrderApplication
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderApplication(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public int PlaceOrder(CartModelWithSummary cartModelWithSummary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
