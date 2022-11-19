4139ec1c109ac42de2f880e8df9bfab8fdf36811
callme-devil <pransermi@gmail.com>
2022-11-19 14:38:24 +0330

    feat: Created && Filled OrderMapping

---
+++ ShopManagement.Infrastructure.EFCore/Mappings/OrderMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopManagement.Domain.OrderAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Mappings
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.IssueTrackingNo).HasMaxLength(12).IsRequired();
+
+            builder.OwnsMany(x => x.Items, navigationBuilder =>
+            {
+                navigationBuilder.ToTable("OrderItems");
+                navigationBuilder.HasKey(x => x.Id);
+                navigationBuilder.WithOwner(x => x.Order).HasForeignKey(x => x.OrderId); 
+            });
+        }
+    }
+}
