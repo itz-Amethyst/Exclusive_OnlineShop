26548ecc4fd2e0ee62e5e88103bf9155c2d58825
callme-devil <pransermi@gmail.com>
2022-11-19 14:37:44 +0330

    feat: Created OrderRepository

---
+++ ShopManagement.Infrastructure.EFCore/Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using _0_Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShopManagement.Domain.OrderAgg;
+using ShopManagement.Infrastructure.EFCore.Context;
+
+namespace ShopManagement.Infrastructure.EFCore.Repositories
+{
+    public class OrderRepository : RepositoryBase<int , Order> , IOrderRepository
+    {
+        private readonly ShopContext _context;
+
+        public OrderRepository(DbContext context, ShopContext context1) : base(context)
+        {
+            _context = context1;
+        }
+    }
+}
