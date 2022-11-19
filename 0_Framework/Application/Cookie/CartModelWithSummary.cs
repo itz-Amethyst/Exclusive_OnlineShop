b9f837df200906a83a3b79deae5c7a52b683ecb2
callme-devil <pransermi@gmail.com>
2022-11-19 14:31:32 +0330

    feat: Moved CartModelWithSummary to Another Class

---
+++ 0_Framework/Application/Cookie/CartModelWithSummary.cs
@@ -0,0 +1,29 @@
+﻿namespace _0_Framework.Application.Cookie;
+
+public class CartModelWithSummary
+{
+    //? summary
+    public double TotalAmount { get; set; }
+    public double TotalDiscountAmount { get; set; }
+    public double TotalPayAmount { get; set; }
+    public int PaymentMethod { get; set; }
+    public List<CookieCartModel> Items { get; set; }
+
+    public CartModelWithSummary()
+    {
+        Items = new List<CookieCartModel>();
+    }
+
+    public void Add(CookieCartModel cartItem)
+    {
+        Items.Add(cartItem);
+        TotalAmount += cartItem.TotalItemPrice;
+        TotalDiscountAmount += cartItem.DiscountAmount;
+        TotalPayAmount += cartItem.ItemPayAmount;
+    }
+
+    public void SetPaymentMethod(int methodId)
+    {
+        PaymentMethod = methodId;
+    }
+}
--- 0_Framework/Application/Cookie/CookieCartModel.cs
+++ 0_Framework/Application/Cookie/CookieCartModel.cs
@@ -29,32 +29,4 @@
         public double ItemPayAmount { get; set; }
 
     }
-
-    public class CartModelWithSummary
-    {
-        //? summary
-        public double TotalAmount { get; set; }
-        public double TotalDiscountAmount { get; set; }
-        public double TotalPayAmount { get; set; }
-        public int PaymentMethod { get; set; }
-        public List<CookieCartModel> Items { get; set; }
-
-        public CartModelWithSummary()
-        {
-            Items = new List<CookieCartModel>();
-        }
-
-        public void Add(CookieCartModel cartItem)
-        {
-            Items.Add(cartItem);
-            TotalAmount += cartItem.TotalItemPrice;
-            TotalDiscountAmount += cartItem.DiscountAmount;
-            TotalPayAmount += cartItem.ItemPayAmount;
-        }
-
-        public void SetPaymentMethod(int methodId)
-        {
-            PaymentMethod = methodId;
-        }
-    }
 }
