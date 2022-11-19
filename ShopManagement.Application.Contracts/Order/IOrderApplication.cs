b903d337540ac50604288b382cafb0d96b2689b4
callme-devil <pransermi@gmail.com>
2022-11-19 14:31:42 +0330

    feat: Created IOrderApplication

---
+++ ShopManagement.Application.Contracts/Order/IOrderApplication.cs
@@ -0,0 +1,9 @@
+﻿using _0_Framework.Application.Cookie;
+
+namespace ShopManagement.Application.Contracts.Order
+{
+    public interface IOrderApplication
+    {
+        int PlaceOrder(CartModelWithSummary cartModelWithSummary);
+    }
+}
