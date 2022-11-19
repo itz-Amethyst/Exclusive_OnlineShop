5ac6da47e24fd3056dd0cee0eedd271c21550e45
callme-devil <pransermi@gmail.com>
2022-11-19 15:11:22 +0330

    feat: Created Get UserId Method in IOrderRepository

--- ShopManagement.Domain/OrderAgg/IOrderRepository.cs
+++ ShopManagement.Domain/OrderAgg/IOrderRepository.cs
@@ -1,9 +1,10 @@
 ﻿using _0_Framework.Domain;
+using Microsoft.AspNetCore.Http;
 
 namespace ShopManagement.Domain.OrderAgg
 {
     public interface IOrderRepository : IRepository<int , Order>
     {
-        
+        int UserId(HttpContext httpContext);
     }
 }
