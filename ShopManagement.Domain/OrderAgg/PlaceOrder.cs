b3185db3923a1ef9c6d2695c559674c30350ede0
callme-devil <pransermi@gmail.com>
2022-11-19 14:20:57 +0330

    feat: PlaceOrder Class Created && Filled

---
+++ ShopManagement.Domain/OrderAgg/PlaceOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopManagement.Domain.OrderAgg
+{
+    public class PlaceOrder
+    {
+        public int AccountId { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public double PayAmount { get; set; }
+
+        public List<AddOrderItem> Items { get; set; }
+    }
+}
