ddfe35a374191ac7442a200bc2e0df40dbc4e6fd
callme-devil <pransermi@gmail.com>
2022-11-19 14:09:31 +0330

    feat: Created Basic Order Domain Class

---
+++ ShopManagement.Domain/OrderAgg/Order.cs
@@ -0,0 +1,46 @@
+﻿using _0_Framework.Domain;
+
+namespace ShopManagement.Domain.OrderAgg
+{
+    public class Order : EntityBase
+    {
+        public int AccountId { get; private set; }
+
+        public double TotalAmount { get; private set; }
+
+        public int DiscountRate { get; private set; }
+
+        public double DiscountAmount { get; private set; }
+
+        public double PayAmount { get; private set; }
+
+        public bool IsPayed { get; private set; }
+
+        public bool IsCanceled { get; private set; }
+
+        public string IssueTrackingNo { get; private set; }
+
+        public int RefId { get; private set; }
+
+        public List<OrderItem> Items { get; private set; }
+
+        public Order(int accountId, double totalAmount, double discountAmount, double payAmount, string issueTrackingNo, List<OrderItem> items , int discountRate)
+        {
+            AccountId = accountId;
+            TotalAmount = totalAmount;
+            DiscountAmount = discountAmount;
+            DiscountRate = discountRate;
+            PayAmount = payAmount;
+            IssueTrackingNo = issueTrackingNo;
+            Items = items;
+            IsPayed = false;
+            IsCanceled = false;
+            RefId = 0;
+        }
+    }
+
+    public class OrderItem : EntityBase
+    {
+
+    }
+}
