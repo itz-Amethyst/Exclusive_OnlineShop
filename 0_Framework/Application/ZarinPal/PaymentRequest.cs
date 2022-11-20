5e3d96b33ea01de0fe05d278f7d7fd0202ada34a
callme-devil <pransermi@gmail.com>
2022-11-20 23:37:25 +0330

    feat: Added UserName && Email In PaymentRequest

--- 0_Framework/Application/ZarinPal/PaymentRequest.cs
+++ 0_Framework/Application/ZarinPal/PaymentRequest.cs
@@ -2,6 +2,7 @@
 {
     public class PaymentRequest
     {
+        public string UserName { get; set; }
         public string Mobile { get; set; }
         public string Email { get; set; }
         public string CallbackURL { get; set; }
