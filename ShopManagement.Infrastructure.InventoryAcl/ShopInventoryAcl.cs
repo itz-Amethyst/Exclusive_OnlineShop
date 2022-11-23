7c9d3f62fd961e780ab9686e3557b3c56a3c91ca
callme-devil <pransermi@gmail.com>
2022-11-23 20:34:44 +0330

    feat: Created && Filled ShopInventoryAcl in New Library

---
+++ ShopManagement.Infrastructure.InventoryAcl/ShopInventoryAcl.cs
@@ -0,0 +1,29 @@
+﻿using InventoryManagement.Application.Contract.Inventory;
+using ShopManagement.Domain.OrderAgg;
+using ShopManagement.Domain.Services;
+
+namespace ShopManagement.Infrastructure.InventoryAcl
+{
+    public class ShopInventoryAcl : IShopInventoryAcl
+    {
+        private readonly IInventoryApplication _inventoryApplication;
+
+        public ShopInventoryAcl(IInventoryApplication inventoryApplication)
+        {
+            _inventoryApplication = inventoryApplication;
+        }
+
+        public bool ReduceFromInventory(List<OrderItem> items)
+        {
+            var command = new List<ReduceInventory>();
+
+            foreach (var orderItem in items)
+            {
+                var item = new ReduceInventory(orderItem.ProductId, orderItem.Count, $"خرید مشتری فاکتور شماره : {orderItem.OrderId}‌", orderItem.OrderId);
+                command.Add(item);
+            }
+
+            return _inventoryApplication.Reduce(command).IsSuccessful;
+        }
+    }
+}
