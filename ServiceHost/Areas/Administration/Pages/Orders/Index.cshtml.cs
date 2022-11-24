b4efd5d8761ae32e84ed3569ad28814fe904b014
callme-devil <pransermi@gmail.com>
2022-11-24 21:29:08 +0330

    feat: Added Confirm Cancel Method in IndexModelpage Order

--- ServiceHost/Areas/Administration/Pages/Orders/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Orders/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using _0_Framework.Infrastructure;
 using AccountManagement.Application.Contracts.Account;
 using AccountManagement.Infrastructure.EFCore.Security;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ShopManagement.Application.Contracts.Order;
@@ -23,11 +24,27 @@ namespace ServiceHost.Areas.Administration.Pages.Orders
             _accountApplication = accountApplication;
         }
 
-        public void OnGet(OrderSearchModel searchModel)
+        public void OnGet(OrderSearchModel searchModel , bool confirmed , bool canceled)
         {
+            ViewData["Confirmed"] = confirmed;
+            ViewData["Canceled"] = canceled;
+
             Accounts = new SelectList(_accountApplication.GetAccounts(), "Id", "Username");
             Orders = _orderApplication.Search(searchModel);
         }
+
+        public IActionResult OnGetConfirm(int id)
+        {
+            _orderApplication.PaymentSucceeded(id, 0);
+            return RedirectToPage("./Index", new { Confirmed = "True" });
+        }
+
+        public IActionResult OnGetCancel(int id)
+        {
+            _orderApplication.Cancel(id);
+            return RedirectToPage("./Index", new { Confirmed = "True" });
+        }
+
 
         //[PermissionChecker(Roles.CreateProductCategory)]
         //public IActionResult OnGetCreate()
