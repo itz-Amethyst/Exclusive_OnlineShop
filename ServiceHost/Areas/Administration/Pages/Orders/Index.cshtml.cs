571eaa5e70510bb26078c2780f55e8140262848a
callme-devil <pransermi@gmail.com>
2022-11-24 18:51:32 +0330

    fix: Fixed Dependency Injection Error && Added Permission Checker to Page

--- ServiceHost/Areas/Administration/Pages/Orders/Index.cshtml.cs
+++ ServiceHost/Areas/Administration/Pages/Orders/Index.cshtml.cs
@@ -1,5 +1,5 @@
 using _0_Framework.Infrastructure;
-using AccountManagement.Application;
+using AccountManagement.Application.Contracts.Account;
 using AccountManagement.Infrastructure.EFCore.Security;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -7,17 +7,17 @@ using ShopManagement.Application.Contracts.Order;
 
 namespace ServiceHost.Areas.Administration.Pages.Orders
 {
-    [PermissionChecker(Roles.ManageProductCategory)]
+    [PermissionChecker(Roles.AccessToOrdersSection)]
     public class IndexModel : PageModel
     {
         private readonly IOrderApplication _orderApplication;
-        private readonly AccountApplication _accountApplication;
+        private readonly IAccountApplication _accountApplication;
 
         public OrderSearchModel SearchModel;
         public List<OrderViewModel> Orders;
         public SelectList Accounts;
 
-        public IndexModel(IOrderApplication orderApplication, AccountApplication accountApplication)
+        public IndexModel(IOrderApplication orderApplication, IAccountApplication accountApplication)
         {
             _orderApplication = orderApplication;
             _accountApplication = accountApplication;
