bfcf0b68aeaf213e5b51482a143c26aa94b2df97
callme-devil <pransermi@gmail.com>
2022-11-24 17:13:17 +0330

    feat: OrderViewModel

---
+++ ShopManagement.Application.Contracts/Order/OrderViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace ShopManagement.Application.Contracts.Order;
+
+public class OrderViewModel
+{
+    public int AccountId { get; set; }
+
+    public string AccountName { get; set; }
+
+    public int PaymentMethodId { get; set; }
+
+    public string PaymentMethod { get; set; }
+
+    public double TotalAmount { get; set; }
+
+    public double DiscountAmount { get; set; }
+
+    public double PayAmount { get; set; }
+
+    public bool IsPaid { get; set; }
+
+    public bool IsCanceled { get; set; }
+
+    //? Just need to add this fucking question to make it null and shut the mouth of sql
+    public string? IssueTrackingNo { get; set; }
+
+    public int RefId { get; set; }
+
+}
