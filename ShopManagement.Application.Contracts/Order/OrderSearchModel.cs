8791dddc9be1e9d0749d448f8111e84ae5f29d37
callme-devil <pransermi@gmail.com>
2022-11-24 17:13:09 +0330

    feat: Added OrderSearchModel

---
+++ ShopManagement.Application.Contracts/Order/OrderSearchModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.Order
+{
+    public class OrderSearchModel
+    {
+        public int AccountId { get; set; }
+
+        public bool IsCanceled { get; set; }
+
+    }
+}
