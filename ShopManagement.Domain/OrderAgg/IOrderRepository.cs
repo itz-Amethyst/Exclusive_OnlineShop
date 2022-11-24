c9b7da6f27d116e1c87e2f8a7c720ff30a0d04fd
callme-devil <pransermi@gmail.com>
2022-11-24 17:30:52 +0330

    feat: Created Search Method in OrderRepository && Commented UserId

--- ShopManagement.Domain/OrderAgg/IOrderRepository.cs
+++ ShopManagement.Domain/OrderAgg/IOrderRepository.cs
@@ -1,12 +1,15 @@
 ﻿using _0_Framework.Domain;
 using Microsoft.AspNetCore.Http;
+using ShopManagement.Application.Contracts.Order;
 
 namespace ShopManagement.Domain.OrderAgg
 {
     public interface IOrderRepository : IRepository<int , Order>
     {
-        int UserId(HttpContext httpContext);
+        //int UserId(HttpContext httpContext);
 
         double GetAmountBy(int id);
+
+        List<OrderViewModel> Search(OrderSearchModel searchModel);
     }
 }
