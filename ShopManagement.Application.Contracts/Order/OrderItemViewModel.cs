a06e39b094428099072fc6bc53e6131251fdb0a7
callme-devil <pransermi@gmail.com>
2022-11-25 16:48:32 +0330

    feat: OrderItemViewModel Added

---
+++ ShopManagement.Application.Contracts/Order/OrderItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ShopManagement.Application.Contracts.Order
+{
+    public class OrderItemViewModel
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Count { get; set; }
+
+        public double UnitPrice { get; set; }
+
+        public int DiscountRate { get; set; }
+
+        public double TotalItemPrice { get; set; }
+
+        public bool HasDiscount { get; set; }
+
+        public double UnitPriceWithDiscount { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public double ItemPayAmount { get; set; }
+
+        public int OrderId { get; set; }
+    }
+}
