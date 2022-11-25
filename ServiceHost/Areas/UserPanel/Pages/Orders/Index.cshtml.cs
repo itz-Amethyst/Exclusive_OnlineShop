475474c11e89d856a17d32fb8039e679ae9ecdb7
callme-devil <pransermi@gmail.com>
2022-11-25 19:46:27 +0330

    feat: Added Basic Order UserPanel Page

---
+++ ServiceHost/Areas/UserPanel/Pages/Orders/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.UserPanel.Pages.Orders
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
