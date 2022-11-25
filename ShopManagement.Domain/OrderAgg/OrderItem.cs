e58e8a21862b5c61921e3278b454cbd1a9655cfd
callme-devil <pransermi@gmail.com>
2022-11-25 16:47:35 +0330

    feat: Added new Props to OrderItems for New Feature

--- ShopManagement.Domain/OrderAgg/OrderItem.cs
+++ ShopManagement.Domain/OrderAgg/OrderItem.cs
@@ -12,15 +12,30 @@ public class OrderItem : EntityBase
 
     public int DiscountRate { get; private set; }
 
+    public double TotalItemPrice { get; private set; }
+
+    public bool HasDiscount { get; private set; }
+
+    public double UnitPriceWithDiscount { get; private set; }
+
+    public double DiscountAmount { get; private set; }
+
+    public double ItemPayAmount { get; private set; }
+
     public int OrderId { get; private set; }
 
     public Order Order { get; private set; }
 
-    public OrderItem(int productId, int count, double unitPrice, int discountRate)
+    public OrderItem(int productId, int count, double unitPrice, int discountRate, double totalItemPrice, bool hasDiscount, double unitPriceWithDiscount, double discountAmount, double itemPayAmount)
     {
         ProductId = productId;
         Count = count;
         UnitPrice = unitPrice;
         DiscountRate = discountRate;
+        TotalItemPrice = totalItemPrice;
+        HasDiscount = hasDiscount;
+        UnitPriceWithDiscount = unitPriceWithDiscount;
+        DiscountAmount = discountAmount;
+        ItemPayAmount = itemPayAmount;
     }
 }
