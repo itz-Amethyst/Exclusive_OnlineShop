4c84039378005891e1b8bcd9f0a92aa69686d316
callme-devil <pransermi@gmail.com>
2022-11-26 19:07:40 +0330

    feat: UserOrderViewModel Added

---
+++ ShopManagement.Application.Contracts/Order/UserPanel/UserOrdersViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopManagement.Application.Contracts.Order.UserPanel
+{
+    public class UserOrdersViewModel
+    {
+        public int Id { get; set; }
+
+        public string RegisterDate { get; set; }
+
+        public string IssueTrackingNo { get; set; }
+
+        public double TotalPayAmount { get; set; }
+
+        public bool OrderCondition { get; set; }
+    }
+}
