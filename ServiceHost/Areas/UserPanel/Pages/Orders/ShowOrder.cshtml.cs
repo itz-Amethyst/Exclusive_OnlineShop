f1b1ad82d65cea9068ad6f3022da4cea0a34d167
callme-devil <pransermi@gmail.com>
2022-11-26 18:43:51 +0330

    feat: Renamed Index to ShowOrder

---
+++ ServiceHost/Areas/UserPanel/Pages/Orders/ShowOrder.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.UserPanel.Pages.Orders
+{
+    public class ShowOrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
