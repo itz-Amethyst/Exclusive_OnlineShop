c430fdeeba5aeb93b00e60d564308d73e90bc198
callme-devil <pransermi@gmail.com>
2022-11-26 20:37:34 +0330

    feat: Configured ShowOrder IndexModelPage to Show Details

--- ServiceHost/Areas/UserPanel/Pages/Orders/ShowOrder.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/Orders/ShowOrder.cshtml.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.Order;
+using ShopManagement.Application.Contracts.Order.UserPanel;
 
 namespace ServiceHost.Areas.UserPanel.Pages.Orders
 {
     public class ShowOrderModel : PageModel
     {
-        public void OnGet()
+        private readonly IOrderApplication _orderApplication;
+
+        public List<ShowUserOrderViewModel> OrderDetail;
+        public SummaryOrderSectionViewModel SummaryOrder;
+
+        public ShowOrderModel(IOrderApplication orderApplication)
         {
+            _orderApplication = orderApplication;
+        }
+
+        public void OnGet(int orderId)
+        {
+            OrderDetail = _orderApplication.GetOrderDetails(orderId);
+            SummaryOrder = _orderApplication.GetOrderSummary(orderId);
         }
     }
 }
