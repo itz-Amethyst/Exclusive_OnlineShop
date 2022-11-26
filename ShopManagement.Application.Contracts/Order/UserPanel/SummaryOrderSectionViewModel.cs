d633f9318db307cbb24f9c86e423ff5f93e2913e
callme-devil <pransermi@gmail.com>
2022-11-26 20:09:16 +0330

    feat: Added Static Summary to SummaryOrder

---
+++ ShopManagement.Application.Contracts/Order/UserPanel/SummaryOrderSectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ShopManagement.Application.Contracts.Order.UserPanel
+{
+    public class SummaryOrderSectionViewModel
+    {
+        public string RegisterDate { get; set; }
+
+        public string IssueTrackingNo { get; set; }
+
+        public double TotalPayAmount { get; set; }
+
+        public double TotalDiscountAmount { get; set; }
+
+        public double FinalPayAmount { get; set; }
+
+        public bool OrderCondition { get; set; }
+
+        public int PaymentMethodId { get; set; }
+
+        public string PaymentMethod { get; set; }
+    }
+}
