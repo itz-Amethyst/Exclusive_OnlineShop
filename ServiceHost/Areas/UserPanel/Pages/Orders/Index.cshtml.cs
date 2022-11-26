32a08a57a0465c861bb34990bd42922288f0ad0a
callme-devil <pransermi@gmail.com>
2022-11-26 20:00:27 +0330

    feat: Configured UserOrder in Index Page

--- ServiceHost/Areas/UserPanel/Pages/Orders/Index.cshtml.cs
+++ ServiceHost/Areas/UserPanel/Pages/Orders/Index.cshtml.cs
@@ -1,12 +1,28 @@
-using Microsoft.AspNetCore.Mvc;
+using _0_Framework.Application;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShopManagement.Application.Contracts.Order;
+using ShopManagement.Application.Contracts.Order.UserPanel;
 
 namespace ServiceHost.Areas.UserPanel.Pages.Orders
 {
     public class IndexModel : PageModel
     {
+        private readonly IOrderApplication _orderApplication;
+        private readonly IAuthHelper _authHelper;
+
+        public List<UserOrdersViewModel> Orders;
+
+        public IndexModel(IOrderApplication orderApplication, IAuthHelper authHelper)
+        {
+            _orderApplication = orderApplication;
+            _authHelper = authHelper;
+        }
+
         public void OnGet()
         {
+            var accountId = _authHelper.CurrentAccountId();
+
+            Orders = _orderApplication.GetUserOrders(accountId);
         }
     }
 }
