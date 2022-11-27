da3c82b9f2a5a7ee39a5f68b76c16d3b2bffcb73
callme-devil <pransermi@gmail.com>
2022-11-27 22:22:49 +0330

    feat: Added DiscountUsage

---
+++ DiscountManagement.Domain/CouponDiscountAgg/DiscountUsage.cs
@@ -0,0 +1,22 @@
+﻿using _0_Framework.Domain;
+
+namespace DiscountManagement.Domain.CouponDiscountAgg
+{
+    public class DiscountUsage : EntityBase
+    {
+        public int UserId { get; private set; }
+
+        public int DiscountId { get; private set; }
+
+        public int OrderId { get; private set; }
+
+        public CouponDiscount CouponDiscount { get; private set; }
+
+        public DiscountUsage(int userId, int discountId, int orderId)
+        {
+            UserId = userId;
+            DiscountId = discountId;
+            OrderId = orderId;
+        }
+    }
+}
