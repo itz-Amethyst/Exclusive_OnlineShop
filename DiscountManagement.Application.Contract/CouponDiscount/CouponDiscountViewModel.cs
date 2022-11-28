0ecca2e76b2fa1a0e5314f7ae379b78829895990
callme-devil <pransermi@gmail.com>
2022-11-28 18:37:09 +0330

    feat: CouponDiscountViewModel added

---
+++ DiscountManagement.Application.Contract/CouponDiscount/CouponDiscountViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace DiscountManagement.Application.Contract.CouponDiscount
+{
+    public class CouponDiscountViewModel
+    {
+        public int Id { get; set; }
+
+        public string CouponCode { get; set; }
+
+        public int DiscountRate { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public string Reason { get; set; }
+
+        public DateTime StartDateEn { get; set; }
+
+        public DateTime EndDateEn { get; set; }
+
+        public string CreationDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public bool IsOutOfDate { get; set; }
+    }
+}
