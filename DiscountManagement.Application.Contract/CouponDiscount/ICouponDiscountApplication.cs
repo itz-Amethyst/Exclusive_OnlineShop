80ea60f47a18459f4691596a955e474ee9073b6b
callme-devil <pransermi@gmail.com>
2022-11-28 20:11:16 +0330

    feat: ICouponDiscountApplication Filled

--- DiscountManagement.Application.Contract/CouponDiscount/ICouponDiscountApplication.cs
+++ DiscountManagement.Application.Contract/CouponDiscount/ICouponDiscountApplication.cs
@@ -1,9 +1,20 @@
-﻿using _0_Framework.Domain;
+﻿using _0_Framework.Application;
+using DiscountManagement.Application.Contract.CustomerDiscount;
 
 namespace DiscountManagement.Application.Contract.CouponDiscount
 {
     public interface ICouponDiscountApplication
     {
+        OperationResult Define(DefineCouponDiscount command);
 
+        OperationResult Edit(EditCouponDiscount command);
+
+        OperationResult Remove(int id);
+
+        OperationResult Restore(int id);
+
+        EditCustomerDiscount GetDetails(int id);
+
+        List<CouponDiscountViewModel> Search(CouponDiscountSearchModel searchModel);
     }
 }
