5489a61abaccab021633bb75d08f2aa7e99e2de0
callme-devil <pransermi@gmail.com>
2022-11-28 19:54:49 +0330

    feat: Changed DiscountRate to int migration Added

---
+++ DiscountManagement.Infrastructure.EFCore/Migrations/20221128154222_changedDiscountRateToInt.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscountManagement.Infrastructure.EFCore.Migrations
+{
+    public partial class changedDiscountRateToInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountPercent",
+                table: "Discounts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiscountRate",
+                table: "Discounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountRate",
+                table: "Discounts");
+
+            migrationBuilder.AddColumn<double>(
+                name: "DiscountPercent",
+                table: "Discounts",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
