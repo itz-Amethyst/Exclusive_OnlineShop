8d738867df8df1d8475d283b5bea50bc4f1ba396
callme-devil <pransermi@gmail.com>
2022-11-28 18:58:20 +0330

    feat: CouponDiscountRepositoryAdded

---
+++ DiscountManagement.Infrastructure.EFCore/Repository/CouponDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using _0_Framework.Infrastructure;
+using DiscountManagement.Domain.CouponDiscountAgg;
+using DiscountManagement.Infrastructure.EFCore.Context;
+
+namespace DiscountManagement.Infrastructure.EFCore.Repository
+{
+    public class CouponDiscountRepository : RepositoryBase<int , CouponDiscount> , ICouponDiscountRepository
+    {
+        private readonly DiscountContext _context;
+        public CouponDiscountRepository(DiscountContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
