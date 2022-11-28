b7cc1be36b50b9d2e0510953d83737650b0d4259
callme-devil <pransermi@gmail.com>
2022-11-28 20:09:35 +0330

    feat: ICouponDiscountRepository Filled

--- DiscountManagement.Domain/CouponDiscountAgg/ICouponDiscountRepository.cs
+++ DiscountManagement.Domain/CouponDiscountAgg/ICouponDiscountRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace DiscountManagement.Domain.CouponDiscountAgg
-{
-    public interface ICouponDiscountRepository
-    {
+﻿using _0_Framework.Domain;
+using DiscountManagement.Application.Contract.CouponDiscount;
 
+namespace DiscountManagement.Domain.CouponDiscountAgg
+{
+    public interface ICouponDiscountRepository : IRepository<int , CouponDiscount>
+    {
+        EditCouponDiscount GetDetails(int id);
+
+        List<CouponDiscountViewModel> Search(CouponDiscountSearchModel searchModel);
     }
 }
