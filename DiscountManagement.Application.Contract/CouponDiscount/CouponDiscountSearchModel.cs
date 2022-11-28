71203b2c9f34cf6e9bc387b8957cd26b25aa814f
callme-devil <pransermi@gmail.com>
2022-11-28 18:36:08 +0330

    feat: Added CouponDiscount SearchModel

---
+++ DiscountManagement.Application.Contract/CouponDiscount/CouponDiscountSearchModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DiscountManagement.Application.Contract.CouponDiscount
+{
+    public class CouponDiscountSearchModel
+    {
+        public string CouponCode { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+    }
+}
