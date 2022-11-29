9e9331901118fc499873b44c00da96f30178af06
callme-devil <pransermi@gmail.com>
2022-11-29 18:40:00 +0330

    feat: Added Program References

--- BlogManagement.Presentation.Api/Program.cs
+++ BlogManagement.Presentation.Api/Program.cs
@@ -1,25 +1,52 @@
+using _0_Framework.Application;
+using _0_Framework.Application.ZarinPal;
+using AccountManagement.Infrastructure.Configuration;
+using BlogManagement.Infrastructure.Configuration;
+using CommentManagement.Infrastructure.Configuration;
+using DiscountManagement.Configuration;
+using InventoryManagement.Infrastructure.Configuration;
+using ServiceHost.Extension;
+using ShopManagement.Infrastructure.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Exclusive_OnlineShopDb");
+ShopManagementBootstrapper.Configure(builder.Services, connectionString);
+DiscountManagementBootstrapper.Configure(builder.Services, connectionString);
+InventoryManagementBootstrapper.Configure(builder.Services, connectionString);
+BlogManagementBootstrapper.Configure(builder.Services, connectionString);
+CommentManagementBootstrapper.Configure(builder.Services, connectionString);
+AccountManagementBootstrapper.Configure(builder.Services, connectionString);
+
+builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+builder.Services.AddTransient<IFileUploader, FileUploader>();
+builder.Services.AddTransient<IAuthHelper, AuthHelper>();
+builder.Services.AddTransient<IViewRenderService, RenderViewToString>();
+builder.Services.AddTransient<IZarinPalFactory, ZarinPalFactory>();
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
