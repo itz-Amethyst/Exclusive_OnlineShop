eb5d571e4d393909e4c63c8ca3ee9e78d07c87cb
callme-devil <pransermi@gmail.com>
2022-11-29 17:51:56 +0330

    feat: added Custom Operation Api for Blog

---
+++ BlogManagement.Presentation.Api/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+﻿using _01_ExclusiveQuery.Contracts.Article;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogManagement.Presentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly IArticleQuery _articleQuery;
+
+        public ArticleController(IArticleQuery articleQuery)
+        {
+            _articleQuery = articleQuery;
+        }
+    }
+}
