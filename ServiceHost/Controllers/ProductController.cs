6fa461078930f45582ab4ccc8e19cbf1c367a880
callme-devil <pransermi@gmail.com>
2022-11-29 17:22:43 +0330

    feat: Added First ApiController

---
+++ ServiceHost/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using _01_ExclusiveQuery.Contracts.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductQuery _productQuery;
+
+        public ProductController(IProductQuery productQuery)
+        {
+            _productQuery = productQuery;
+        }
+
+        [HttpGet]
+        public List<ProductQueryModel> GetLatestArrivals()
+        {
+            return _productQuery.GetLatestArrivals();
+        }
+    }
+}
