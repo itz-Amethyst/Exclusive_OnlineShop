ead96797b3bc155d42ef1d5e7fcb2bc5e0e044b3
callme-devil <pransermi@gmail.com>
2022-11-29 17:51:20 +0330

    refactor: Moved InventoryController in Presentation Layer

---
+++ InventoryManagement.Presentation.Api/InventoryController.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagement.Application.Contract.Inventory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Presentation.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryApplication _inventoryApplication;
+
+        public InventoryController(IInventoryApplication inventoryApplication)
+        {
+            _inventoryApplication = inventoryApplication;
+        }
+
+        [HttpGet("{id}")]
+        public List<InventoryOperationViewModel> GetOperationsBy(int id)
+        {
+            return _inventoryApplication.GetOperationLog(id);
+        }
+    }
+}
