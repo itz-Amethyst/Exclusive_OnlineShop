5e560dd8a5056570797591ac20747a66c21271be
callme-devil <pransermi@gmail.com>
2022-11-30 17:53:51 +0330

    feat: Added StockStatus

---
+++ 01_ExclusiveQuery/Contracts/Inventory/StockStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Inventory
+{
+    public class StockStatus
+    {
+        public bool IsInStock { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
