5a7142483da6f68fc75f286595ae4072a5442449
callme-devil <pransermi@gmail.com>
2022-11-30 17:54:22 +0330

    feat: Added InventoryQuery

---
+++ 01_ExclusiveQuery/Query/InventoryQuery.cs
@@ -0,0 +1,12 @@
+﻿using _01_ExclusiveQuery.Contracts.Inventory;
+
+namespace _01_ExclusiveQuery.Query
+{
+    public class InventoryQuery : IInventoryQuery
+    {
+        public StockStatus CheckStock(IsInStock command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
