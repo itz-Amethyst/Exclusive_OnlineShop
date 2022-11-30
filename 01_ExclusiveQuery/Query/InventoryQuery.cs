09348ca809d8625b6eafe7be6cd1d2e0c3824ded
callme-devil <pransermi@gmail.com>
2022-11-30 18:10:48 +0330

    feat: InventoryQuery CheckStock Method Filled

--- 01_ExclusiveQuery/Query/InventoryQuery.cs
+++ 01_ExclusiveQuery/Query/InventoryQuery.cs
@@ -1,12 +1,42 @@
 ﻿using _01_ExclusiveQuery.Contracts.Inventory;
+using InventoryManagement.Infrastructure.EFCore.Context;
+using ShopManagement.Infrastructure.EFCore.Context;
 
 namespace _01_ExclusiveQuery.Query
 {
     public class InventoryQuery : IInventoryQuery
     {
+        private readonly InventoryContext _inventoryContext;
+
+        private readonly ShopContext _shopContext;
+
+
+        public InventoryQuery(InventoryContext inventoryContext, ShopContext shopContext)
+        {
+            _inventoryContext = inventoryContext;
+            _shopContext = shopContext;
+        }
+
+
         public StockStatus CheckStock(IsInStock command)
         {
-            throw new NotImplementedException();
+            var inventory = _inventoryContext.Inventories.FirstOrDefault(x => x.ProductId == command.ProductId);
+
+            if (inventory == null || inventory.CalculateCurrentCount() < command.Count)
+            {
+                var product = _shopContext.Products.Select(x=> new{x.Id , x.Name}).FirstOrDefault(x => x.Id == command.ProductId);
+
+                return new StockStatus
+                {
+                    IsInStock = false,
+                    ProductName = product.Name
+                };
+            }
+
+            return new StockStatus
+            {
+                IsInStock = true
+            };
         }
     }
 }
