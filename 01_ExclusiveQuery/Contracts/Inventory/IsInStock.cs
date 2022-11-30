7e972a8842bfbc3fbf2b0dcb38c8f94aa5f0d8ac
callme-devil <pransermi@gmail.com>
2022-11-30 17:53:36 +0330

    feat: Added IsInStock

---
+++ 01_ExclusiveQuery/Contracts/Inventory/IsInStock.cs
@@ -0,0 +1,9 @@
+﻿namespace _01_ExclusiveQuery.Contracts.Inventory
+{
+    public class IsInStock
+    {
+        public int ProductId { get; set; }
+
+        public int Count { get; set; }
+    }
+}
