fba36768607efa65b025f5371b72a97e47ab06c9
callme-devil <pransermi@gmail.com>
2022-11-30 18:11:27 +0330

    feat: InventoryController Api Filled

--- InventoryManagement.Presentation.Api/InventoryController.cs
+++ InventoryManagement.Presentation.Api/InventoryController.cs
@@ -1,4 +1,5 @@
-﻿using InventoryManagement.Application.Contract.Inventory;
+﻿using _01_ExclusiveQuery.Contracts.Inventory;
+using InventoryManagement.Application.Contract.Inventory;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InventoryManagement.Presentation.Api
@@ -7,17 +8,26 @@ namespace InventoryManagement.Presentation.Api
     [ApiController]
     public class InventoryController : ControllerBase
     {
+        private readonly IInventoryQuery _inventoryQuery;
+
         private readonly IInventoryApplication _inventoryApplication;
 
-        public InventoryController(IInventoryApplication inventoryApplication)
+        public InventoryController(IInventoryApplication inventoryApplication, IInventoryQuery inventoryQuery)
         {
             _inventoryApplication = inventoryApplication;
+            _inventoryQuery = inventoryQuery;
         }
 
         [HttpGet("{id}")]
         public List<InventoryOperationViewModel> GetOperationsBy(int id)
         {
             return _inventoryApplication.GetOperationLog(id);
+        }
+
+        [HttpPost]
+        public StockStatus CheckStock(IsInStock command)
+        {
+            return _inventoryQuery.CheckStock(command);
         }
     }
 }
