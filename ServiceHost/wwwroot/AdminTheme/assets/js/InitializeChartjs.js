43a22825802a5bee78e6d9284044b0787f1681ae
callme-devil <pransermi@gmail.com>
2022-12-03 19:20:43 +0330

    feat: Initialized PieChart

--- ServiceHost/wwwroot/AdminTheme/assets/js/InitializeChartjs.js
+++ ServiceHost/wwwroot/AdminTheme/assets/js/InitializeChartjs.js
@@ -29,3 +29,30 @@ const barChartElement = new Chart(barChart,
         }
 
     });
+
+const pieChart = document.getElementById("pieChart");
+
+const pieChartElement = new Chart(pieChart,
+    {
+        type: 'pie',
+        data: {
+            labels: ['df', 'dsf', 'f' , "fdfd"],
+            datasets: [
+                {
+                    label: ["Pie Chart"],
+                    data: [100, 200, 300 , 70],
+                    borderColor: "#00BEC5",
+                    backgroundColor: ["#0F1300", "#B4F8C8", "#F24E70", "#07D2BE"],
+                    hoverOffset: 11,
+                }
+            ]
+        },
+        options: {
+            elements: {
+                bar: {
+                    borderWidth: 1
+                }
+            }
+        }
+
+    });
