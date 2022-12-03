16cdb5ba63712c6d12c1976cb51375af2b86e179
callme-devil <pransermi@gmail.com>
2022-12-03 19:03:49 +0330

    refactor: Refactored InitializeChart.js

---
+++ ServiceHost/wwwroot/AdminTheme/assets/js/InitializeChartjs.js
@@ -0,0 +1,25 @@
+﻿const barChart = document.getElementById("barChart");
+
+const barChartElement = new Chart(barChart,
+    {
+        type: 'bar',
+        data: {
+            labels: ['df', 'dsf', 'f'],
+            datasets: [
+                {
+                    label: ["Bar Chart"],
+                    data: [100, 200, 300],
+                    backgroundColor: "#00BEC5",
+                    borderColor: "#050833"
+                }
+            ]
+        },
+        options: {
+            elements: {
+                bar: {
+                    borderWidth: 2
+                }
+            }
+        }
+
+    });
