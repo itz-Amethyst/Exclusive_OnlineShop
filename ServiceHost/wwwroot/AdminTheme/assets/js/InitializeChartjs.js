d7a9120f8cfe8cb67cb147fe427fda16602ef299
callme-devil <pransermi@gmail.com>
2022-12-03 19:08:01 +0330

    feat: Added New DataSet in Chart.js

--- ServiceHost/wwwroot/AdminTheme/assets/js/InitializeChartjs.js
+++ ServiceHost/wwwroot/AdminTheme/assets/js/InitializeChartjs.js
@@ -11,6 +11,12 @@ const barChartElement = new Chart(barChart,
                     data: [100, 200, 300],
                     backgroundColor: "#00BEC5",
                     borderColor: "#050833"
+                },
+                {
+                    label: ["Bar Chart 2"],
+                    data: [50, 20, 350],
+                    backgroundColor: "#1C3F60",
+                    borderColor: "#0B1320"
                 }
             ]
         },
